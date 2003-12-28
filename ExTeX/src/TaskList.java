4bc525261b40a11fb138193cc4978471edf2a3b9
gene <gene>
2003-12-28 00:03:17 +0000

    *** empty log message ***

---
+++ ExTeX/src/TaskList.java
@@ -0,0 +1,11 @@
+public interface TaskList {
+
+//TODO: TeX error handling; interact with the user (via callbacks! not hardcoded!)
+
+//TODO: Eliminate global logger and use observer pattern instead???
+
+//TODO: Clean up the Exceptions. Maybe some of them can be eliminated
+//        in the face of GeneralDetailedException
+
+//todo: assumption: node lists can not be dumped
+}
