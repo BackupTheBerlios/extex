4c99738f145360876621aabaec77c33990a171fe
gene <gene>
2004-01-20 09:25:56 +0000

    ExpandableCode extracted

--- ExTeX/src/de/dante/extex/interpreter/Code.java
+++ ExTeX/src/de/dante/extex/interpreter/Code.java
@@ -35,7 +35,7 @@ import de.dante.util.GeneralException;
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  */
 public interface Code {
 
@@ -77,28 +77,6 @@ public interface Code {
      * @throws GeneralException in case of an error
      */
     void execute(Flags prefix, Context context,
-            TokenSource source, Typesetter typesetter) throws GeneralException;
-
-    /**
-     * This method takes the first token and expands it. The result is placed
-     * on the stack. To expand a token it might be necessary to consume further
-     * tokens. In this case <code>true</code> is returned.
-     * <p>
-     * This means that expandable code does one step of expansion, puts the
-     * result on the stack, and returns <code>true</code>.
-     * Non-expandable code simply return <code>false</code>.
-     * </p>
-     *
-     * @param prefix the prefix flags controlling the expansion
-     * @param context the interpreter context
-     * @param source the token source
-     * @param typesetter the typesetter
-     *
-     * @return <code>true</code> iff the token could be expanded
-     *
-     * @throws GeneralException in case of an error
-     */
-    boolean expand(Flags prefix, Context context,
             TokenSource source, Typesetter typesetter) throws GeneralException;
 
     /**
