8567b4fcf1c8edf4925d48b6d61c8a926ba4b7ed
rolf <rolf>
2004-02-09 07:45:26 +0000

    Initial revision

---
+++ ExTeX/src/de/dante/extex/documentWriter/pdf/pdf/FopTest2.java
@@ -0,0 +1,116 @@
+
+package de.dante.extex.documentWriter.pdf;
+
+// FOP
+
+import org.apache.fop.pdf.PDFDocument;
+import org.apache.fop.pdf.PDFStream;
+import org.apache.fop.pdf.PDFResources;
+import org.apache.fop.pdf.PDFAnnotList;
+import org.apache.fop.pdf.PDFPage;
+import org.apache.fop.pdf.PDFColor;
+import org.apache.fop.layout.Page;
+
+// Java
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.awt.Color;
+
+// ExTeX
+
+/**
+ * Testprogramm für `fop'.
+ * 
+ * @author Rolf Niepraschk
+ * @version $Revision: 1.1 $ $Date: 2004/02/09 07:45:26 $
+ */
+ 
+class FopTest2 {
+
+        private FopTest2() {
+        }
+                
+        private static void println(String s) {
+                System.out.println(s);
+        }
+
+        private static void marker() {
+                println("------------------------------------" + 
+                        "------------------------------------"); // 72
+        }
+        
+        public static void main(String[] args) {
+                
+          marker();
+          println("Test der PDF-Erzeugung mit \"FOP\".");
+          marker();
+
+          PDFDocumentWriter pdw = new PDFDocumentWriter();
+          try {
+            pdw.open(new FileOutputStream("FopTest2" + "." + 
+              pdw.getExtension()));
+
+            pdw.op.lineWidth(5);
+
+            pdw.op.gSave();
+              pdw.op.strokeColor(Color.RED);
+              pdw.op.fillColor(Color.YELLOW);  
+              pdw.op.addRectangle(200,100,300,200); 
+              pdw.op.fillStroke(); 
+            pdw.op.gRestore();
+
+            pdw.op.addRectangle(200,400,300,200); 
+            pdw.op.stroke(); 
+	    
+	    if (false) // 
+	    {
+	      pdw.op.getStream().add( // PDF reference S. 225
+	      "BT                         % Begin text object\n" +
+	      "/F1 1 Tf                   % Set text font and size\n" +
+	      "64 0 0 64 7.1771 2.4414 Tm % Set text matrix\n" +
+	      "0 Tc                       % Set character spacing\n" +
+	      "0 Tw                       % Set word spacing\n" +
+	      "1.0 0.0 0.0 rg             % Set nonstroking color to red\n" +
+	      "(\001) Tj                  % Show spade glyph\n" +
+	      "0.7478 -0.007 TD           % Move text position\n" +
+	      "0.0 1.0 0.0 rg             % Set nonstroking color to green\n" +
+	      "(\002) Tj                  % Show heart glyph\n" +
+	      "-0.7323 0.7813 TD          % Move text position\n" +
+	      "0.0 0.0 1.0 rg             % Set nonstroking color to blue\n" +
+	      "(\003) Tj                  % Show diamond glyph\n" +
+	      "0.6913 0.007 TD            % Move text position\n" +
+	      "0.0 0.0 0.0 rg             % Set nonstroking color to black\n" +
+	      "(\004) Tj                  % Show club glyph\n" +
+	      "ET                         % End text object\n");
+            }
+	    else // Alles noch sehr vage ;-|
+	    {
+	      //pdw.op.moveTo(200,600); 
+	      pdw.op.getStream().add("BT\n");
+	      pdw.op.getStream().add("/F1 1 Tf \n");
+	      pdw.op.getStream().add("64 0 0 -64 7.1771 2.4414 Tm \n");
+	      pdw.op.getStream().add(".5 Tc 0 Tw \n");
+	      pdw.op.getStream().add("2 -12 TD \n");
+	      pdw.op.getStream().add("(HUGO) Tj\n");
+	      pdw.op.getStream().add("ET\n");
+	    }
+            pdw.newPage();
+
+            pdw.op.lineWidth(5);
+
+            pdw.op.addRectangle(200,100,300,200); 
+            pdw.op.stroke();
+            pdw.op.fillColor(Color.GREEN); 
+            pdw.op.moveTo(200,100);
+            pdw.op.curveTo(200,100,200,300,500,300); 
+            pdw.op.curveTo(500,300,500,100,200,100); 
+            pdw.op.fillStroke();
+
+            pdw.close();
+          } 
+          catch (IOException e) {
+            e.printStackTrace();
+          }	  
+        }
+}
+
---
+++ ExTeX/src/java/de/dante/extex/documentWriter/pdf/dump/DumpDocumentWriter.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2003-2004 Gerd Neugebauer
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ */
+package de.dante.extex.documentWriter.dump;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import de.dante.extex.documentWriter.DocumentWriter;
+import de.dante.extex.typesetter.NodeList;
+
+/**
+ * This is a first dummy implementation of a document writer.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class DumpDocumentWriter implements DocumentWriter {
+
+    /**
+     * The field <tt>out</tt> ...
+     */
+    private Writer out = null;
+
+    /**
+     * The field <tt>shippedPages</tt> ...
+     */
+    private int shippedPages = 0;
+
+    /**
+     * Creates a new object.
+     */
+    public DumpDocumentWriter() {
+        super();
+    }
+
+    /**
+     * @see de.dante.extex.documentWriter.DocumentWriter#getPages()
+     */
+    public int getPages() {
+        return shippedPages;
+    }
+
+    /**
+     * @see de.dante.extex.documentWriter.DocumentWriter#getExtension()
+     */
+    public String getExtension() {
+        return "out";
+    }
+
+    /**
+     * @see de.dante.extex.documentWriter.DocumentWriter#setWriter(java.io.Writer)
+     */
+    public void setWriter(final Writer outStream) {
+        out = outStream;
+    }
+
+    /**
+     * @see de.dante.extex.documentWriter.DocumentWriter#shipout(de.dante.extex.typesetter.NodeList)
+     */
+    public void shipout(final NodeList nodes) throws IOException {
+        StringBuffer sb = new StringBuffer();
+        nodes.toString(sb,"\n");
+        out.write(sb.toString());
+        out.write('\n');
+        shippedPages++;
+    }
+
+    /**
+     * @see de.dante.extex.documentWriter.DocumentWriter#close()
+     */
+    public void close() {
+        // nothing to do
+    }
+
+}
