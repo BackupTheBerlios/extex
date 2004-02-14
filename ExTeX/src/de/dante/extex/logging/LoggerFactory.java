4723f1cc4182c81f55984928545ff4eb36994fe7
gene <gene>
2004-02-14 14:21:57 +0000

    *** empty log message ***

--- ExTeX/src/de/dante/extex/logging/LoggerFactory.java
+++ ExTeX/src/de/dante/extex/logging/LoggerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004  Gerd Neugebauer
+ * Copyright (C) 2004 Gerd Neugebauer
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -28,7 +28,7 @@ import de.dante.util.configuration.ConfigurationMissingException;
  * ...
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class LoggerFactory {
 
