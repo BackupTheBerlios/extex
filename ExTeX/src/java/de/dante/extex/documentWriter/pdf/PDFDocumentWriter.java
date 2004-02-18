2a3e0551e6f874aad33291c46337cdfa74962cb5
rolf <rolf>
2004-02-18 06:37:46 +0000

    *** empty log message ***

--- ExTeX/src/java/de/dante/extex/documentWriter/pdf/PDFDocumentWriter.java
+++ ExTeX/src/java/de/dante/extex/documentWriter/pdf/PDFDocumentWriter.java
@@ -42,17 +42,19 @@ import de.dante.extex.documentWriter.DocumentWriter;
 import de.dante.extex.typesetter.Node;
 import de.dante.extex.typesetter.NodeList;
 import de.dante.extex.typesetter.NodeIterator;
+import de.dante.extex.typesetter.NodeVisitor;
 import de.dante.extex.interpreter.type.node.AbstractNodeList;
+import de.dante.util.GeneralException;
 
 /**
  * Implementation of a pdf document writer.
  *
  * @author <a href="mailto:Rolf.Niepraschk@ptb.de">Rolf Niepraschk</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  * @see org.apache.fop.render.pdf.PDFRenderer
  * @see org.apache.fop.svg.PDFGraphics2D
  */
-public class PDFDocumentWriter implements DocumentWriter {
+public class PDFDocumentWriter implements DocumentWriter, NodeVisitor {
 
     /**
      * The field <tt>out</tt> ...
@@ -89,9 +91,23 @@ public class PDFDocumentWriter implements DocumentWriter {
      * @see de.dante.extex.documentWriter.DocumentWriter#setOutputStream(java.io.Writer)
      */
     public void setOutputStream(final OutputStream outStream) {
-        out = outStream;
+        this.out = outStream;
     }
     
+    public void shipout(final NodeList nodes) throws IOException {
+      newPage(); shippedPages++;
+      processNodes(nodes);
+    }
+    
+    /**
+     * @see de.dante.extex.documentWriter.DocumentWriter#close()
+     */
+    public void close() throws IOException {
+      //pdfDoc.getOutlineRoot().setTitle("Erste Schritte");
+      FontSetup.addToResources(this.pdfDoc, fontInfo); // ??? //
+      pdfDoc.outputTrailer(this.out);
+    }
+        
     private PDFDocument pdfDoc = null;
     private PDFStream currentStream = null;
     private PDFResources pdfResources = null;
@@ -108,79 +124,92 @@ public class PDFDocumentWriter implements DocumentWriter {
     private FontInfo fontInfo = null;
     private FontState fontState = null;
     
-   private float sp2bp(long sp) {
-   /*
-       So oder per Transformation? Wie genau rechnet PDF? Einstellbar?
-   */
-        return (float)((sp >> 16) * 72.0 / 72.27);
-   }
-   
-   private float lastX = 0f, lastY = 0f;
-   
-   private void showNode(Node node) {
-   
-        boolean onlyStroke = false, show = true; 
-        
-        float x = 0f, y = 0f;
-        float wd = sp2bp(node.getWidth().getValue());
-        float ht = sp2bp(node.getHeight().getValue());
-        float dp = sp2bp(node.getDepth().getValue());
-        
-        op.gSave();
-          
-        if (node.getType().equals("char")) 
-        {
-          x = lastX; y = lastY; 
-	  lastX = x + wd; lastY = y + ht + dp; 
-	  // Nur damit es weniger eintönig ist ;-)
-          op.fillColor(Color.GREEN);
-        }
-        if (node.getType().equals("horizontallist")) 
-        {
-          show = false;
-        }
-        if (node.getType().equals("verticallist"))  
-        {
-          op.fillColor(Color.LIGHT_GRAY);  //onlyStroke = true;
-        }
-        
-        if (show)
-        {
-          op.addRectangle(x, y+ht, wd, ht+dp); 
-          if (onlyStroke) op.stroke(); else op.fillStroke();
-        }
-        
-        op.gRestore();
-        
-        System.out.println(node.getType() + 
-          " (wd=" + node.getWidth().toPT() + 
-          "\tht=" + node.getHeight().toPT() +
-          "\tdp=" + node.getDepth().toPT() + ")" + "\t" +
-	  ((node.getType().equals("char")) ? node.toString() : ""));
-   }
+    private float sp2bp(long sp) {
+      return (float)((sp >> 16) * 72.0 / 72.27);
+    }
 
-   public void shipout(final NodeList nodes) throws IOException {
-        newPage(); shippedPages++;
-        System.out.println("\n");
-          op.concat(1, 0, 0, 1, 72, 72);
-          // Hier mal testweise zu Pos. (72bp, 72bp) verschieben ...
-          processNodes(nodes);
-        System.out.println("\n");
+    public Object visitAdjust(Object value, Object Value2){return null;}
+    public Object visitAfterMath(Object value, Object Value2){return null;}
+    public Object visitAlignedLeaders(Object value, Object Value2){return null;}
+    public Object visitBeforeMath(Object value, Object Value2){return null;}
+    public Object visitCenteredLeaders(Object value, Object Value2){return null;}
+    public Object visitDiscretionary(Object value, Object Value2){return null;}
+    public Object visitExpandedLeaders(Object value, Object Value2){return null;}
+    public Object visitGlue(Object value, Object Value2){return null;}
+    public Object visitInsertion(Object value, Object Value2){return null;}
+    public Object visitKern(Object value, Object Value2){return null;}
+    public Object visitLigature(Object value, Object Value2){return null;}
+    public Object visitMark(Object value, Object Value2){return null;}
+    public Object visitPenalty(Object value, Object Value2){return null;}
+    public Object visitRule(Object value, Object Value2){return null;}
+    public Object visitSpace(Object value, Object Value2){return null;}
+    public Object visitWhatsIt(Object value, Object Value2){return null;} 
+
+    public Object visitVerticalList(Object value, Object Value2){
+      x = lastX; y = lastY;
+      op.fillColor(Color.LIGHT_GRAY);
+      onlyStroke = false; show = true; 
+      return null;
     }
     
-    /**
-     * @see de.dante.extex.documentWriter.DocumentWriter#close()
-     */
-    public void close() throws IOException {
-      //pdfDoc.getOutlineRoot().setTitle("Erste Schritte");
-      FontSetup.addToResources(this.pdfDoc, fontInfo); // ??? //
-      pdfDoc.outputTrailer(this.out);
+    public Object visitHorizontalList(Object value, Object Value2){
+      show = false; 
+      return null;
+    }
+        
+    public Object visitChar(Object value, Object Value2){
+      x = lastX; y = lastY; 
+      lastX = x + wd; lastY = y + ht + dp; 
+      // Nur damit es weniger eintönig ist ;-)
+      op.fillColor(Color.GREEN);
+      onlyStroke = false; show = true;
+      return null;
+    }   
+ 
+    private float lastX, lastY, x, y, wd, ht, dp;
+    private boolean onlyStroke, show; 
 
-      // this.out.close();
+    private void showNode(Node node) { 
+
+      onlyStroke = false;
+
+      wd = sp2bp(node.getWidth().getValue());
+      ht = sp2bp(node.getHeight().getValue());
+      dp = sp2bp(node.getDepth().getValue());
+
+      op.gSave();
+
+      try {
+       node.visit(this, node, null);
+      }
+      catch (GeneralException  e) {
+       e.printStackTrace();
+      } 
+
+      if (show)
+      {
+       op.addRectangle(x, y-ht, wd, ht+dp); 
+       if (onlyStroke) op.stroke(); else op.fillStroke();
+      } 
+
+      op.gRestore();
+
+      String nodeName = node.getClass().getName();    
+      nodeName = nodeName.substring(nodeName.lastIndexOf('.')+1);
+      nodeName = nodeName.substring(0, nodeName.lastIndexOf("Node"));
+      // Haben wir irgendwo "regular expressions" bei der Hand?
+
+      System.out.println(nodeName + 
+       " (wd=" + node.getWidth().toPT() + 
+       "\tht=" + node.getHeight().toPT() +
+       "\tdp=" + node.getDepth().toPT() + ")" + "\t" +
+       (nodeName.equals("Char") ? node.toString() : ""));     
     }
     
     private void processNodes(final NodeList nodes) {
       NodeIterator it = nodes.iterator(); 
+      
+      lastX = 72; lastY = 72; // Wir beginnen bei 1in Offset.
       
       showNode(nodes);  
       
@@ -197,7 +226,7 @@ public class PDFDocumentWriter implements DocumentWriter {
     /**
      * Opens/setups the document    
      */      
-    public void initDocument() throws IOException {
+    private void initDocument() throws IOException {
       
       pdfDoc = new PDFDocument();
       
@@ -216,7 +245,7 @@ public class PDFDocumentWriter implements DocumentWriter {
       try {
         FontSetup.setup(fontInfo);
         fontState = new FontState(fontInfo, "Helvetica", "normal",
-                                          "normal", 12, 0);
+                                  "normal", 12, 0);
       }
       catch (FOPException e) {
         e.printStackTrace();
@@ -231,7 +260,7 @@ public class PDFDocumentWriter implements DocumentWriter {
     /**
      * Creates/setups a new page 
      */  
-    public void newPage() throws IOException {
+    private void newPage() throws IOException {
     
       if (pdfDoc == null) initDocument();
     
