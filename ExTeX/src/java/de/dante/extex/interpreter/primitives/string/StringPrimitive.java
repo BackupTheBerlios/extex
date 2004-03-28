f71d1a8a718e5034e343f4d99c7dcf8a24e1aa51
gene <gene>
2004-03-28 16:15:56 +0000

    new

---
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/string/StringPrimitive.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ */
+package de.dante.extex.interpreter.primitives.string;
+
+import de.dante.extex.interpreter.AbstractCode;
+import de.dante.extex.interpreter.Flags;
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.interpreter.type.Tokens;
+import de.dante.extex.scanner.Token;
+import de.dante.extex.typesetter.Typesetter;
+import de.dante.util.GeneralException;
+
+/**
+ * This class provides an implementation for the primitive
+ * <code>\string</code>.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class StringPrimitive extends AbstractCode {
+
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for tracing and debugging
+     */
+    public StringPrimitive(final String name) {
+
+        super(name);
+    }
+
+    /**
+     * @see "TeX -- the Program [69]"
+     * @see de.dante.extex.interpreter.Code#execute(de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public void execute(final Flags prefix, final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws GeneralException {
+
+        Token t = source.getToken();
+        source.push(new Tokens(context, t.getValue()));
+    }
+
+}
