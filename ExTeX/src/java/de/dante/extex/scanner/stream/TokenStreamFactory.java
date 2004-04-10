bb756974414f44b0866c921053fc209f1756ddaf
gene <gene>
2004-04-10 06:02:28 +0000

    Reader in TokenStreamFactory made configurable

--- ExTeX/src/java/de/dante/extex/scanner/stream/TokenStreamFactory.java
+++ ExTeX/src/java/de/dante/extex/scanner/stream/TokenStreamFactory.java
@@ -19,10 +19,12 @@
 
 package de.dante.extex.scanner.stream;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
@@ -44,11 +46,78 @@ import de.dante.util.observer.Observer;
 import de.dante.util.observer.ObserverList;
 
 /**
- * This is the factory to provide an instance of a TokenStream.
+ * This is the factory to provide an instance of a
+ * {@link de.dante.extex.scanner.stream.TokenStream TokenStream}.
+ * Like any good factory it is controlled by its configuration.
+ *
+ * <h3>Configuration</h3>
+ *
+ * <p>
+ * Mainly the configuration needs to specify which class to use for the
+ * TokenStream. The name of the class is given as the argument "class" as
+ * shown below.
+ * <pre>
+ *   &lt;Scanner class="the.package.TheClass"/&gt;
+ * </pre>
+ * </p>
+ * <p>
+ * The class given must implement the interface
+ * {@link de.dante.extex.scanner.stream.TokenStream TokenStream}. In addition
+ * an appropriate constructor is required:
+ * <pre>
+ *     public TheClass(Configuration config, Reader reader, Boolean isFile,
+ *          final String theSource) throws IOException
+ * </pre>
+ * </p>
+ *
+ * <p>
+ * In addition to the class for the Token stream the reader class can be
+ * specified for the case that reading from a file is requested. In this case
+ * the mapping from bytes to characters according to an encoding.
+ * The name is given as the argument "reader" as shown below:
+ * <pre>
+ *   &lt;Scanner class="the.package.TheClass"
+ *         reader="another.pack.age.TheReaderClass"/&gt;
+ * </pre>
+ * </p>
+ * <p>
+ * Note that the attribute "reader" is optional. If none is given or the value
+ * is the empty string then <tt>java.io.InputStreamReader</tt> is used instead.
+ * </p>
+ *
+ * <h3>Observable Events</h3>
+ * <p>
+ * {@link de.dante.util.observer.Observer Observer}s can be registered for
+ * several events:
+ * </p>
+ * <dl>
+ *  <dt><tt>file</tt></dt>
+ *  <dd>This event is triggered by the request for a TokenStream fed from a
+ *   file. It is defered until the file has been found and opened.
+ *   The name of the file is passed as argument to the observer.
+ *  </dd>
+ *
+ *  <dt><tt>reader</tt></dt>
+ *  <dd>This event is triggered by the request for a TokenStream fed from a
+ *   file where a custom reader is used. This means that the configuration
+ *   contains a reader class name. The reader is passed as argument to the
+ *   observer.
+ *  </dd>
+ *
+ *  <dt><tt>string</tt></dt>
+ *  <dd>This event is triggered by the request for a TokenStream fed from a
+ *   String. The string is passed as argument to the observer.
+ *  </dd>
+ *
+ *  <dt><tt>stream</tt></dt>
+ *  <dd>This event is triggered by the request for a TokenStream fed from an
+ *   arbitrary Reader. The reader is passed as argument to the observer.
+ *  </dd>
+ * </dl>
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.16 $
+ * @version $Revision: 1.17 $
  */
 public class TokenStreamFactory implements Observable {
 
@@ -57,6 +126,12 @@ public class TokenStreamFactory implements Observable {
      * attribute used to get the class name.
      */
     private static final String CLASS_ATTRIBUTE = "class";
+
+    /**
+     * The constant <tt>READER_ATTRIBUTE</tt> contains the name of the
+     * attribute used to get the class name for the reader.
+     */
+    private static final String READER_ATTRIBUTE = "reader";
 
     /**
      * The field <tt>configuration</tt> contains the configuration for this
@@ -77,6 +152,12 @@ public class TokenStreamFactory implements Observable {
     private ObserverList openStreamObservers = new ObserverList();
 
     /**
+     * The field <tt>openReaderObservers</tt> contains the observers registered
+     * for the "reader" event.
+     */
+    private ObserverList openReaderObservers = new ObserverList();
+
+    /**
      * The field <tt>openStringObservers</tt> contains the observers registered
      * for the "string" event.
      */
@@ -87,6 +168,12 @@ public class TokenStreamFactory implements Observable {
      * to use.
      */
     private Constructor constructor;
+
+    /**
+     * The field <tt>readerConstructor</tt> contains the constructor for the
+     * input stream reader.
+     */
+    private Constructor readerConstructor;
 
     /**
      * The field <tt>fileFinder</tt> contains the file finder used when trying
@@ -123,6 +210,22 @@ public class TokenStreamFactory implements Observable {
             throw new ConfigurationNoSuchMethodException(e);
         } catch (ClassNotFoundException e) {
             throw new ConfigurationClassNotFoundException(classname, config);
+        }
+
+        classname = config.getAttribute(READER_ATTRIBUTE);
+        if (classname == null || classname.equals("")) {
+            readerConstructor = null;
+        } else {
+            try {
+                readerConstructor = Class.forName(classname)
+                .getConstructor(new Class[]{InputStream.class, String.class});
+            } catch (SecurityException e) {
+                throw new ConfigurationInstantiationException(e);
+            } catch (NoSuchMethodException e) {
+                throw new ConfigurationNoSuchMethodException(e);
+            } catch (ClassNotFoundException e) {
+                throw new ConfigurationClassNotFoundException(classname, config);
+            }
         }
     }
 
@@ -174,8 +277,32 @@ public class TokenStreamFactory implements Observable {
             throw new FileNotFoundException(fileName);
         }
 
-        TokenStream stream = makeTokenStream(new InputStreamReader(
-                new FileInputStream(file), encoding), true, fileName);
+        BufferedInputStream inputStream = new BufferedInputStream(
+                new FileInputStream(file));
+
+        Reader reader;
+
+        if (readerConstructor == null) {
+            reader = new InputStreamReader(inputStream, encoding);
+        } else {
+            try {
+                reader = (Reader) readerConstructor.newInstance(new Object[]{
+                        inputStream, encoding});
+                openReaderObservers.update(this, reader);
+            } catch (IllegalArgumentException e) {
+                throw new ConfigurationInstantiationException(e);
+            } catch (InstantiationException e) {
+                throw new ConfigurationInstantiationException(e);
+            } catch (IllegalAccessException e) {
+                throw new ConfigurationInstantiationException(e);
+            } catch (InvocationTargetException e) {
+                throw new ConfigurationInstantiationException(e);
+            } catch (GeneralException e) {
+                throw new ConfigurationWrapperException(e);
+            }
+        }
+
+        TokenStream stream = makeTokenStream(reader, true, fileName);
 
         try {
             openFileObservers.update(this, fileName);
@@ -251,6 +378,8 @@ public class TokenStreamFactory implements Observable {
             openStreamObservers.add(observer);
         } else if ("string".equals(name)) {
             openStringObservers.add(observer);
+        } else if ("reader".equals(name)) {
+            openReaderObservers.add(observer);
         } else {
             throw new NotObservableException(name);
         }
