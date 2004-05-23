07d597b4d5c1a0e91eb96136fb08c59a08467406
gene <gene>
2004-05-23 08:06:45 +0000

    new

---
+++ ExTeX/src/java/de/dante/extex/interpreter/type/count/FixedCount.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.type.count;
+
+import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.interpreter.type.tokens.Tokens;
+import de.dante.util.GeneralException;
+
+/**
+ * This interface describes the features of a
+ * {@link de.dante.extex.interpreter.type.count.Count Count} which do not modify
+ * the value.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public interface FixedCount {
+
+    /**
+     * Getter for the value
+     *
+     * @return the value
+     */
+    long getValue();
+
+    /**
+     * Determine the printable representation of the object.
+     * The value returned is exactely the string which would be produced by
+     * TeX to print the Count.
+     *
+     * @return the printable representation
+     *
+     * @see #toString(StringBuffer)
+     */
+    String toString();
+
+    /**
+     * Determine the printable representation of the object.
+     * The value returned is exactely the string which would be produced by
+     * TeX to print the Count.
+     *
+     * @param sb the target string buffer
+     *
+     * @see #toString()
+     */
+    void toString(final StringBuffer sb);
+
+    /**
+     * Determine the printable representation of the object.
+     * The value returned is exactely the string which would be produced by
+     * TeX to print the Count.
+     *
+     * @param context the interpreter context
+     *
+     * @return the printable respresentation as tokens
+     *
+     * @throws GeneralException in case of an error
+     */
+    Tokens toToks(final Context context)
+            throws GeneralException;
+
+}
