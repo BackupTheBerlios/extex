9466bfb744d26ea4eb7f6f32c6673902710508cc
gene <gene>
2004-06-04 08:36:40 +0000

    negate() added

--- ExTeX/src/de/dante/extex/interpreter/type/dimen/Dimen.java
+++ ExTeX/src/de/dante/extex/interpreter/type/dimen/Dimen.java
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
  */
+
 package de.dante.extex.interpreter.type.dimen;
 
 import java.io.Serializable;
@@ -32,58 +33,50 @@ import de.dante.util.GeneralException;
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class Dimen extends GlueComponent implements Serializable, FixedDimen {
 
     /**
-      * The constant <tt>ZERO_PT</tt> contains the immutable dimen register
-      * representing the length of 0pt.
-      */
-     public static final Dimen ZERO_PT = new ImmutableDimen(0);
+     * The constant <tt>ONE_INCH</tt> contains the immutable dimen register
+     * representing the length of 1in.
+     */
+    public static final Dimen ONE_INCH = new ImmutableDimen(ONE * 7227 / 100);
 
-     /**
-      * The constant <tt>ONE_PT</tt> contains the immutable dimen register
-      * representing the length of 1pt.
-      */
-     public static final Dimen ONE_PT = new ImmutableDimen(ONE);
+    /**
+     * The constant <tt>ONE_PT</tt> contains the immutable dimen register
+     * representing the length of 1pt.
+     */
+    public static final Dimen ONE_PT = new ImmutableDimen(ONE);
 
-     /**
-      * The constant <tt>ONE_INCH</tt> contains the immutable dimen register
-      * representing the length of 1in.
-      */
-     public static final Dimen ONE_INCH = new ImmutableDimen(ONE * 7227 / 100);
+    /**
+     * The constant <tt>ZERO_PT</tt> contains the immutable dimen register
+     * representing the length of 0pt.
+     */
+    public static final Dimen ZERO_PT = new ImmutableDimen(0);
 
-     /**
-      * Creates a new object.
-      * The length stored in it is initialized to 0pt.
-      */
-     public Dimen() {
+    /**
+     * Creates a new object.
+     * The length stored in it is initialized to 0pt.
+     */
+    public Dimen() {
 
-         super();
-     }
+        super();
+    }
 
-     /**
-      * Creates a new object.
-      * This method makes a new instance of the class with the given value.
-      *
-      * @param value the value to set
-      */
-     public Dimen(final long value) {
+    /**
+     * Creates a new object.
+     *
+     * @param context the interpreter context
+     * @param source the source for next tokens
+     *
+     * @throws GeneralException in case of an error
+     */
+    public Dimen(final Context context, final TokenSource source)
+            throws GeneralException {
 
-         super(value);
-     }
-
-     /**
-      * Creates a new object.
-      * This method makes a new instance of the class with the given value.
-      *
-      * @param value the value to set
-      */
-     public Dimen(final int value) {
-
-         super((long) value);
-     }
+        super(context, source, false);
+    }
 
     /**
      * Creates a new object.
@@ -99,45 +92,24 @@ public class Dimen extends GlueComponent implements Serializable, FixedDimen {
 
     /**
      * Creates a new object.
+     * This method makes a new instance of the class with the given value.
      *
-     * @param context the interpreter context
-     * @param source the source for next tokens
-     *
-     * @throws GeneralException in case of an error
+     * @param value the value to set
      */
-    public Dimen(final Context context, final TokenSource source)
-        throws GeneralException {
+    public Dimen(final int value) {
 
-        super(context, source, false);
+        super((long) value);
     }
 
     /**
-     * Setter for the value.
-     * The order of the arument is ignored.
+     * Creates a new object.
+     * This method makes a new instance of the class with the given value.
      *
-     * @param d the new value
+     * @param value the value to set
      */
-    public void set(final FixedGlueComponent d) {
+    public Dimen(final long value) {
 
-        set(d.getValue());
-    }
-
-    /**
-     * Set the value from the data gathered by parsing a token source.
-     *
-     * @param context the interpreter context
-     * @param source the source for next tokens
-     *
-     * @throws GeneralException in case of an error
-     *
-     * @see de.dante.extex.interpreter.type.glue.GlueComponent#set(
-     *      de.dante.extex.interpreter.context.Context,
-     *      de.dante.extex.interpreter.TokenSource)
-     */
-    public void set(final Context context, final TokenSource source)
-             throws GeneralException {
-
-        set(context, source, true);
+        super(value);
     }
 
     /**
@@ -153,36 +125,6 @@ public class Dimen extends GlueComponent implements Serializable, FixedDimen {
     public void add(final FixedDimen d) {
 
         setValue(getValue() + d.getValue());
-    }
-
-    /**
-     * Subtract the value of the argument from the current value.
-     * This operation modifies the instance.
-     *
-     * <p>
-     * |<i>this</i>| := |<i>this</i>| - |<i>d</i>|
-     * </p>
-     *
-     * @param d the Dimen to subtract
-     */
-    public void subtract(final FixedDimen d) {
-
-        setValue(getValue() - d.getValue());
-    }
-
-    /**
-     * Multiply the current value with a given number.
-     * This operation modifies this instance.
-     *
-     * <p>
-     * |<i>this</i>| := |<i>this</i>| * <i>factor</i>
-     * </p>
-     *
-     * @param factor the factor to multiply with
-     */
-    public void multiply(final long factor) {
-
-        setValue(getValue() * factor);
     }
 
     /**
@@ -223,6 +165,78 @@ public class Dimen extends GlueComponent implements Serializable, FixedDimen {
         if (val > getValue()) {
             setValue(val);
         }
+    }
+
+    /**
+     * Multiply the current value with a given number.
+     * This operation modifies this instance.
+     *
+     * <p>
+     * |<i>this</i>| := |<i>this</i>| * <i>factor</i>
+     * </p>
+     *
+     * @param factor the factor to multiply with
+     */
+    public void multiply(final long factor) {
+
+        setValue(getValue() * factor);
+    }
+
+    /**
+     * Negate the value of the argument to the current value.
+     * This operation modifies the instance.
+     *
+     * <p>
+     * |<i>this</i>| :=  - |<i>this</i>|
+     * </p>
+     */
+    public void negate() {
+
+        setValue(-getValue());
+    }
+
+    /**
+     * Set the value from the data gathered by parsing a token source.
+     *
+     * @param context the interpreter context
+     * @param source the source for next tokens
+     *
+     * @throws GeneralException in case of an error
+     *
+     * @see de.dante.extex.interpreter.type.glue.GlueComponent#set(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource)
+     */
+    public void set(final Context context, final TokenSource source)
+            throws GeneralException {
+
+        set(context, source, true);
+    }
+
+    /**
+     * Setter for the value.
+     * The order of the arument is ignored.
+     *
+     * @param d the new value
+     */
+    public void set(final FixedGlueComponent d) {
+
+        set(d.getValue());
+    }
+
+    /**
+     * Subtract the value of the argument from the current value.
+     * This operation modifies the instance.
+     *
+     * <p>
+     * |<i>this</i>| := |<i>this</i>| - |<i>d</i>|
+     * </p>
+     *
+     * @param d the Dimen to subtract
+     */
+    public void subtract(final FixedDimen d) {
+
+        setValue(getValue() - d.getValue());
     }
 
 }
