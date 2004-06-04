346cbcc116338864860d356a43926270b328341e
gene <gene>
2004-06-04 08:34:03 +0000

    NmaedSkip renamed to SkipParameter

--- ExTeX/src/de/dante/extex/interpreter/primitives/register/skip/SkipParameter.java
+++ ExTeX/src/de/dante/extex/interpreter/primitives/register/skip/SkipParameter.java
@@ -29,7 +29,7 @@ import de.dante.util.GeneralException;
  *
  * <p>
  * All features are inherited from
- * {@link de.dante.extex.interpreter.primitives.register.skip.NamedSkip NamedSkip}.
+ * {@link de.dante.extex.interpreter.primitives.register.skip.SkipParameter SkipParameter}.
  * Just the key has to be provided under which this Glue has to be stored.
  * This key is constructed from the name, a hash mark and the running number.
  * </p>
@@ -40,16 +40,16 @@ import de.dante.util.GeneralException;
  * </pre>
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.1 $
  */
-public class NamedSkip extends NumberedSkip {
+public class SkipParameter extends SkipPrimitive {
 
     /**
      * Creates a new object.
      *
      * @param name the name for debugging
      */
-    public NamedSkip(final String name) {
+    public SkipParameter(final String name) {
 
         super(name);
     }
