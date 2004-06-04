49e5dd49ce1d4e453b44e89af21531aed896204b
gene <gene>
2004-06-04 08:31:40 +0000

    NumberedDimen renamed to DimenParameter

---
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/register/dimen/DimenParameter.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.register.dimen;
+
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.util.GeneralException;
+
+/**
+ * This class provides an implementation for the primitive <code>\dimen</code>.
+ * It sets the named dimen register to the value given,
+ * and as a side effect all prefixes are zeroed.
+ *
+ * <p>Example</p>
+ * <pre>
+ * \day=345
+ * </pre>
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class DimenParameter extends DimenPrimitive {
+
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for debugging
+     */
+    public DimenParameter(final String name) {
+
+        super(name);
+    }
+    
+    /**
+     * Return the key (the name of the primitive) for the register.
+     *
+     * @param source the source for new tokens
+     *
+     * @return the key for the current register
+     *
+     * @throws GeneralException in case that a derived class need to throw an
+     *             Exception this one is declared.
+     */
+    protected String getKey(final TokenSource source) throws GeneralException {
+
+        return getName();
+    }
+
+}
