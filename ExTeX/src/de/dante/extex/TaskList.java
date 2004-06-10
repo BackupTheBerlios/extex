8f365f4cbb114eed9b94ffeaeb15f21a54847297
gene <gene>
2004-06-10 06:23:08 +0000

    *** empty log message ***

--- ExTeX/src/de/dante/extex/TaskList.java
+++
@@ -1,9 +0,0 @@
-package de.dante.extex;
-
-public interface TaskList {
-
-//TODO: Clean up the Exceptions. Maybe some of them can be eliminated
-//        in the face of GeneralHelpingException
-
-//todo: assumption: node lists can not be dumped
-}
