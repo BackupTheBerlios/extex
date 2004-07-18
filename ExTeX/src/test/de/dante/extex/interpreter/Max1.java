96eff92fb204ec8c85d00bfd4dda04e526846edd
gene <gene>
2004-07-18 17:32:08 +0000

    method removeLastNode added.

--- ExTeX/src/test/de/dante/extex/interpreter/Max1.java
+++ ExTeX/src/test/de/dante/extex/interpreter/Max1.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003 Gerd Neugebauer
+ * Copyright (C) 2003-2004 Gerd Neugebauer
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -39,17 +39,196 @@ import de.dante.util.configuration.ConfigurationFactory;
 
 /**
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.24 $
+ * @version $Revision: 1.25 $
  */
 public class Max1 extends TestCase {
 
     /**
-     * Constructor for Max1.
-     *
-     * @param arg0 the name
+     * Inner class to collect the things the typesetter sees.
      */
-    public Max1(final String arg0) {
-        super(arg0);
+    private static class TestTypesetter implements Typesetter {
+        /**
+         */
+        private StringBuffer sb = new StringBuffer();
+
+        /**
+         * @see de.dante.extex.typesetter.Typesetter#add(
+         *      de.dante.extex.interpreter.type.node.CharNode)
+         */
+        public void add(final Node c) {
+            // nothing to do
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.Typesetter#add(
+         *      de.dante.extex.interpreter.type.font.Font,
+         *      java.lang.String)
+         */
+        public void add(final TypesettingContext font,
+                final UnicodeChar symbol) {
+            // nothing to do
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.ListMaker#addGlue(
+         *      de.dante.extex.interpreter.type.glue.Glue)
+         */
+        public void addGlue(final Glue g) {
+            sb.append(g.toString());
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.ListMaker#addSpace(
+         *      de.dante.extex.interpreter.context.TypesettingContext,
+         *      de.dante.extex.interpreter.type.count.Count)
+         */
+        public void addSpace(final TypesettingContext typesettingContext,
+                final Count spacefactor) {
+            sb.append(" ");
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.Typesetter#close()
+         */
+        public NodeList close() {
+            return null;
+        }
+
+        /**
+         * @see de.dante.util.configuration.Configurable#configure(
+         *      de.dante.util.configuration.Configuration)
+         */
+        public void configure(final Configuration config) {
+            // nothing to do
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.Typesetter#finish()
+         */
+        public void finish() {
+            // nothing to do
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.Typesetter#getCharNodeFactory()
+         */
+        public CharNodeFactory getCharNodeFactory() {
+            return null;
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.ListMaker#getLastNode()
+         */
+        public Node getLastNode() {
+
+            return null;
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.Typesetter#getMode()
+         */
+        public Mode getMode() {
+            return null;
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.Typesetter#open()
+         */
+        public void open() {
+            // nothing to do
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.Typesetter#openHbox()
+         */
+        public void openHbox() {
+
+            // TODO Auto-generated method stub
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.Typesetter#openVbox()
+         */
+        public void openVbox() {
+
+            // TODO Auto-generated method stub
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.ListMaker#par()
+         */
+        public void par() {
+            sb.append("\n\\par\n");
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.ListMaker#removeLastNode()
+         */
+        public void removeLastNode() {
+
+            // TODO Auto-generated method stub
+
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.Typesetter#setDocumentWriter(
+         *      de.dante.extex.documentWriter.DocumentWriter)
+         */
+        public void setDocumentWriter(final DocumentWriter doc) {
+            // nothing to do
+        }
+        /**
+         * @see de.dante.extex.typesetter.Typesetter#setParshape(
+         *      de.dante.extex.typesetter.paragraphBuilder.ParagraphShape)
+         */
+        public void setParshape(ParagraphShape parshape) {
+
+            // TODO Auto-generated method stub
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.ListMaker#setPrevDepth(
+         *      de.dante.extex.interpreter.type.dimen.Dimen)
+         */
+        public void setPrevDepth(final Dimen pd) {
+            // nothing to do
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.ListMaker#setSpacefactor(int)
+         */
+        public void setSpacefactor(final Count f) {
+            // nothing to do
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.Typesetter#shipout(
+         *      de.dante.extex.typesetter.NodeList)
+         */
+        public void shipout(final NodeList nodes) {
+            // nothing to do
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.ListMaker#toggleDisplaymath()
+         */
+        public void toggleDisplaymath() {
+            // nothing to do
+        }
+
+        /**
+         * @see de.dante.extex.typesetter.ListMaker#toggleMath()
+         */
+        public void toggleMath() {
+            // nothing to do
+        }
+
+        /**
+         * @see java.lang.Object#toString()
+         */
+        public String toString() {
+            return sb.toString();
+        }
     }
 
     /**
@@ -61,53 +240,12 @@ public class Max1 extends TestCase {
     }
 
     /**
-     * Trivial case: nothing in and nothing out
-     * @throws Exception in case of an error
+     * Constructor for Max1.
+     *
+     * @param arg0 the name
      */
-    public void testEmpty() throws Exception {
-        assertEquals("", doTest(""));
-    }
-
-    /**
-     * @throws Exception in case of an error
-     */
-    public void testSingle1() throws Exception {
-        assertEquals("a", doTest("a"));
-    }
-
-    /**
-     * @throws Exception in case of an error
-     */
-    public void testSingle2() throws Exception {
-        assertEquals("A", doTest("A"));
-    }
-
-    /**
-     * @throws Exception in case of an error
-     */
-    public void testSingle3() throws Exception {
-        assertEquals("2", doTest("2"));
-    }
-
-    /**
-     * @throws Exception in case of an error
-     */
-    public void testSingle4() throws Exception {
-        assertEquals(".", doTest("."));
-    }
-
-    /**
-     * @throws Exception in case of an error
-     */
-    public void testMacro1() throws Exception {
-        assertEquals("", doTest("\\relax"));
-    }
-
-    /**
-     * @throws Exception in case of an error
-     */
-    public void testMacro2() throws Exception {
-        assertEquals("\n\\par\n", doTest("\\par"));
+    public Max1(final String arg0) {
+        super(arg0);
     }
 
     /**
@@ -141,183 +279,53 @@ public class Max1 extends TestCase {
     }
 
     /**
-     * Inner class to collect the things the typesetter sees.
+     * Trivial case: nothing in and nothing out
+     * @throws Exception in case of an error
      */
-    private static class TestTypesetter implements Typesetter {
+    public void testEmpty() throws Exception {
+        assertEquals("", doTest(""));
+    }
 
-        /**
-         */
-        private StringBuffer sb = new StringBuffer();
+    /**
+     * @throws Exception in case of an error
+     */
+    public void testMacro1() throws Exception {
+        assertEquals("", doTest("\\relax"));
+    }
 
-        /**
-         * @see java.lang.Object#toString()
-         */
-        public String toString() {
-            return sb.toString();
-        }
+    /**
+     * @throws Exception in case of an error
+     */
+    public void testMacro2() throws Exception {
+        assertEquals("\n\\par\n", doTest("\\par"));
+    }
 
-        /**
-         * @see de.dante.extex.typesetter.ListMaker#addGlue(
-         *      de.dante.extex.interpreter.type.glue.Glue)
-         */
-        public void addGlue(final Glue g) {
-            sb.append(g.toString());
-        }
+    /**
+     * @throws Exception in case of an error
+     */
+    public void testSingle1() throws Exception {
+        assertEquals("a", doTest("a"));
+    }
 
-        /**
-         * @see de.dante.extex.typesetter.ListMaker#addSpace(
-         *      de.dante.extex.interpreter.context.TypesettingContext,
-         *      de.dante.extex.interpreter.type.count.Count)
-         */
-        public void addSpace(final TypesettingContext typesettingContext,
-                final Count spacefactor) {
-            sb.append(" ");
-        }
+    /**
+     * @throws Exception in case of an error
+     */
+    public void testSingle2() throws Exception {
+        assertEquals("A", doTest("A"));
+    }
 
-        /**
-         * @see de.dante.extex.typesetter.Typesetter#getCharNodeFactory()
-         */
-        public CharNodeFactory getCharNodeFactory() {
-            return null;
-        }
+    /**
+     * @throws Exception in case of an error
+     */
+    public void testSingle3() throws Exception {
+        assertEquals("2", doTest("2"));
+    }
 
-        /**
-         * @see de.dante.extex.typesetter.ListMaker#par()
-         */
-        public void par() {
-            sb.append("\n\\par\n");
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.Typesetter#setDocumentWriter(
-         *      de.dante.extex.documentWriter.DocumentWriter)
-         */
-        public void setDocumentWriter(final DocumentWriter doc) {
-            // nothing to do
-        }
-
-        /**
-         * @see de.dante.util.configuration.Configurable#configure(
-         *      de.dante.util.configuration.Configuration)
-         */
-        public void configure(final Configuration config) {
-            // nothing to do
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.Typesetter#add(
-         *      de.dante.extex.interpreter.type.node.CharNode)
-         */
-        public void add(final Node c) {
-            // nothing to do
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.Typesetter#close()
-         */
-        public NodeList close() {
-            return null;
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.Typesetter#getMode()
-         */
-        public Mode getMode() {
-            return null;
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.Typesetter#open()
-         */
-        public void open() {
-            // nothing to do
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.Typesetter#add(
-         *      de.dante.extex.interpreter.type.font.Font,
-         *      java.lang.String)
-         */
-        public void add(final TypesettingContext font,
-                final UnicodeChar symbol) {
-            // nothing to do
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.Typesetter#finish()
-         */
-        public void finish() {
-            // nothing to do
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.ListMaker#toggleDisplaymath()
-         */
-        public void toggleDisplaymath() {
-            // nothing to do
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.ListMaker#toggleMath()
-         */
-        public void toggleMath() {
-            // nothing to do
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.ListMaker#setSpacefactor(int)
-         */
-        public void setSpacefactor(final Count f) {
-            // nothing to do
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.ListMaker#setPrevDepth(
-         *      de.dante.extex.interpreter.type.dimen.Dimen)
-         */
-        public void setPrevDepth(final Dimen pd) {
-            // nothing to do
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.Typesetter#shipout(
-         *      de.dante.extex.typesetter.NodeList)
-         */
-        public void shipout(final NodeList nodes) {
-            // nothing to do
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.Typesetter#openHbox()
-         */
-        public void openHbox() {
-
-            // TODO Auto-generated method stub
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.Typesetter#openVbox()
-         */
-        public void openVbox() {
-
-            // TODO Auto-generated method stub
-        }
-
-        /**
-         * @see de.dante.extex.typesetter.ListMaker#getLastNode()
-         */
-        public Node getLastNode() {
-
-            return null;
-        }
-        /**
-         * @see de.dante.extex.typesetter.Typesetter#setParshape(
-         *      de.dante.extex.typesetter.paragraphBuilder.ParagraphShape)
-         */
-        public void setParshape(ParagraphShape parshape) {
-
-            // TODO Auto-generated method stub
-        }
+    /**
+     * @throws Exception in case of an error
+     */
+    public void testSingle4() throws Exception {
+        assertEquals(".", doTest("."));
     }
 
 }
