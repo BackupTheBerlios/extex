8d06b443f417f77a8313577cef34ea6cb6064d45
gene <gene>
2004-07-30 19:07:18 +0000

    new

---
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/register/count/Mag.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.register.count;
+
+import de.dante.extex.i18n.HelpingException;
+import de.dante.extex.interpreter.Flags;
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.interpreter.type.ExpandableCode;
+import de.dante.extex.interpreter.type.Theable;
+import de.dante.extex.interpreter.type.arithmetic.Advanceable;
+import de.dante.extex.interpreter.type.arithmetic.Divideable;
+import de.dante.extex.interpreter.type.arithmetic.Multiplyable;
+import de.dante.extex.interpreter.type.count.Count;
+import de.dante.extex.interpreter.type.count.CountConvertible;
+import de.dante.extex.interpreter.type.tokens.Tokens;
+import de.dante.extex.typesetter.Typesetter;
+import de.dante.util.GeneralException;
+
+/**
+ * This class provides an implementation for the primitive <code>\count</code>.
+ * It sets the named count register to the value given,
+ * and as a side effect all prefixes are zeroed.
+ *
+ * <doc name="mag">
+ * <h3>The Primitive <tt>\mag</tt></h3>
+ * <p>
+ *  ...
+ * </p>
+ * <p>
+ *  The formal description of this primitive is the following:
+ *  <pre class="syntax">
+ *    &lang;mag&rang;
+ *      &rarr; <tt>\mag</tt>  </pre>
+ * </p>
+ * <p>
+ *  Examples:
+ *  <pre class="TeXSample">
+ *    \count23=-456  </pre>
+ * </p>
+ * </doc>
+ *
+ * @see de.dante.extex.interpreter.type.arithmetic.Advanceable
+ * @see de.dante.extex.interpreter.type.arithmetic.Divideable
+ * @see de.dante.extex.interpreter.type.arithmetic.Multiplyable
+ * @see de.dante.extex.interpreter.type.Theable
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class Mag extends AbstractCount
+        implements
+            ExpandableCode,
+            Advanceable,
+            Multiplyable,
+            Divideable,
+            Theable,
+            CountConvertible {
+
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for debugging
+     */
+    public Mag(final String name) {
+
+        super(name);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.arithmetic.Advanceable#advance(
+     *      de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource)
+     */
+    public void advance(final Flags prefix, final Context context,
+            final TokenSource source) throws GeneralException {
+
+        source.getKeyword("by");
+
+        long value = Count.scanCount(context, source, null);
+        value += context.getMagnification();
+
+        context.setMagnification(value);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.AbstractAssignment#assign(
+     *      de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public void assign(final Flags prefix, final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws GeneralException {
+
+        source.getOptionalEquals();
+
+        long value = Count.scanCount(context, source, typesetter);
+        context.setMagnification(value);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.ExpandableCode#expand(
+     *      de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public void expand(final Flags prefix, final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws GeneralException {
+
+        source.push(new Tokens(context, Long.toString(context
+                .getMagnification())));
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.count.CountConvertible#convertCount(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public long convertCount(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws GeneralException {
+
+        return context.getMagnification();
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.arithmetic.Divideable#divide(
+     *      de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource)
+     */
+    public void divide(final Flags prefix, final Context context,
+            final TokenSource source) throws GeneralException {
+
+        source.getKeyword("by");
+
+        long value = Count.scanCount(context, source, null);
+
+        if (value == 0) {
+            throw new HelpingException("TTP.ArithOverflow");
+        }
+
+        value = context.getMagnification() / value;
+        context.setMagnification(value);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.arithmetic.Multiplyable#multiply(
+     *      de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource)
+     */
+    public void multiply(final Flags prefix, final Context context,
+            final TokenSource source) throws GeneralException {
+
+        source.getKeyword("by");
+
+        long value = Count.scanCount(context, source, null);
+        value *= context.getMagnification();
+        context.setMagnification(value);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.Theable#the(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public Tokens the(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws GeneralException {
+
+        return new Tokens(context, Long.toString(context.getMagnification()));
+    }
+
+}
---
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/register/dimen/Lastkern.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.register.dimen;
+
+import de.dante.extex.i18n.HelpingException;
+import de.dante.extex.interpreter.Flags;
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.interpreter.type.AbstractCode;
+import de.dante.extex.interpreter.type.Theable;
+import de.dante.extex.interpreter.type.count.CountConvertible;
+import de.dante.extex.interpreter.type.dimen.Dimen;
+import de.dante.extex.interpreter.type.dimen.DimenConvertible;
+import de.dante.extex.interpreter.type.node.KernNode;
+import de.dante.extex.interpreter.type.tokens.Tokens;
+import de.dante.extex.typesetter.Node;
+import de.dante.extex.typesetter.Typesetter;
+import de.dante.util.GeneralException;
+
+/**
+ * This class provides an implementation for the primitive
+ * <code>\lastkern</code>.
+ *
+ * <doc name="lastkern">
+ * <h3>The Primitive <tt>\lastkern</tt></h3>
+ * <p>
+ *  ...
+ * </p>
+ * <p>
+ *  Examples:
+ *  <pre class="TeXSample">
+ *    \dimen1=\lastkern  </pre>
+ * </p>
+ * </doc>
+ *
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class Lastkern extends AbstractCode
+        implements
+            CountConvertible,
+            DimenConvertible,
+            Theable {
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for debugging
+     */
+    public Lastkern(final String name) {
+
+        super(name);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.count.CountConvertible#convertCount(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public long convertCount(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws GeneralException {
+
+        Node node = typesetter.getLastNode();
+        return (node instanceof KernNode
+                ? ((KernNode) node).getWidth().getValue()
+                : 0);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.dimen.DimenConvertible#convertDimen(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public long convertDimen(Context context, TokenSource source,
+            Typesetter typesetter) throws GeneralException {
+
+        Node node = typesetter.getLastNode();
+        return (node instanceof KernNode
+                ? ((KernNode) node).getWidth().getValue()
+                : 0);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.Code#execute(
+     *      de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public boolean execute(final Flags prefix, final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws GeneralException {
+
+        throw new HelpingException("TTP.CantUseIn",
+                printableControlSequence(context), typesetter.getMode()
+                        .toString());
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.Theable#the(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public Tokens the(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws GeneralException {
+
+        Node node = typesetter.getLastNode();
+        Dimen pen = (node instanceof KernNode
+                ? ((KernNode) node).getWidth()
+                : Dimen.ZERO_PT);
+        return pen.toToks(context.getTokenFactory());
+    }
+
+}
