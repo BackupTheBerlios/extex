5a322be2f761ea96a8eba75dfc8f01d93cd0ef21
gene <gene>
2004-08-01 19:53:14 +0000

    first installer with IzPack complete

---
+++ ExTeX/util/Installer/IzPack/doc/izpack/html/internals.pl
@@ -0,0 +1,90 @@
+# LaTeX2HTML 2002-2-1 (1.70)
+# Associate internals original text with physical files.
+
+
+$key = q/userInput:descriptiontag/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ant-integration/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tag:os/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/panels-element/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/native-element/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/chap:userinput/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/archinstaller/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/guiprefs-element/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/info-element/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/comparch/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tag:file/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/resources-element/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/variables-element/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tag:fileset/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/localization-element/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/CC-license/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/webinstaller/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/packs-element/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/jar-element/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tag:singlefile/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/root-element/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ ExTeX/util/Installer/IzPack/doc/izpack/html/labels.pl
@@ -0,0 +1,181 @@
+# LaTeX2HTML 2002-2-1 (1.70)
+# Associate labels original text with physical files.
+
+
+$key = q/userInput:descriptiontag/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ant-integration/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tag:os/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/panels-element/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/native-element/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/chap:userinput/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/archinstaller/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/guiprefs-element/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/info-element/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/comparch/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tag:file/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/resources-element/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/variables-element/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tag:fileset/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/localization-element/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/CC-license/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/webinstaller/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/packs-element/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/jar-element/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tag:singlefile/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/root-element/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.70)
+# labels from external_latex_labels array.
+
+
+$key = q/userInput:descriptiontag/;
+$external_latex_labels{$key} = q|6.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ant-integration/;
+$external_latex_labels{$key} = q|3.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tag:os/;
+$external_latex_labels{$key} = q|2.3.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/panels-element/;
+$external_latex_labels{$key} = q|2.3.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/native-element/;
+$external_latex_labels{$key} = q|2.3.9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/chap:userinput/;
+$external_latex_labels{$key} = q|6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/archinstaller/;
+$external_latex_labels{$key} = q|1.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/guiprefs-element/;
+$external_latex_labels{$key} = q|2.3.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/info-element/;
+$external_latex_labels{$key} = q|2.3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/comparch/;
+$external_latex_labels{$key} = q|1.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tag:file/;
+$external_latex_labels{$key} = q|2.3.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/resources-element/;
+$external_latex_labels{$key} = q|2.3.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/variables-element/;
+$external_latex_labels{$key} = q|2.3.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tag:fileset/;
+$external_latex_labels{$key} = q|2.3.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/localization-element/;
+$external_latex_labels{$key} = q|2.3.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/CC-license/;
+$external_latex_labels{$key} = q|B|; 
+$noresave{$key} = "$nosave";
+
+$key = q/webinstaller/;
+$external_latex_labels{$key} = q|3.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/packs-element/;
+$external_latex_labels{$key} = q|2.3.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/jar-element/;
+$external_latex_labels{$key} = q|2.3.10|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tag:singlefile/;
+$external_latex_labels{$key} = q|2.3.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/root-element/;
+$external_latex_labels{$key} = q|2.3.1|; 
+$noresave{$key} = "$nosave";
+
+1;
+
