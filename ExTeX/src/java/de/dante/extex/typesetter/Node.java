b2ec7940a6c365dbb10ea2f8a6bbb1f2e10dd0b0
gene <gene>
2004-08-15 18:01:09 +0000

    documentation improved

--- ExTeX/src/java/de/dante/extex/typesetter/Node.java
+++ ExTeX/src/java/de/dante/extex/typesetter/Node.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
 package de.dante.extex.typesetter;
@@ -24,12 +24,18 @@ import de.dante.util.GeneralException;
 
 /**
  * A node is the basic data structure for the typesetter. It has a reference
- * point and three dimensions, namely width, height and depth (see figure).
- * <img src="doc-files/Node.gif" alt="" align="right"/>
+ * point and three dimensions, namely width, height, and depth (see figure).
+ * <img src="doc-files/Node.png" alt="" align="right"/>
+ * <p>
+ * Note that those dimensions are sometimes used for different purposes.
+ * For instance a KernNode does use the width to denote the actual size which
+ * can be applied in horizontal or in vertical direction, depending on the
+ * context.
+ * </p>
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.11 $
+ * @version $Revision: 1.12 $
  */
 public interface Node {
 
