ed11b74a75e157ee863e3448d377aeb6093c33a9
plaicy <plaicy>
2004-08-20 21:44:23 +0000

    - added JDEE-project files for test (does not run class ExTeX as default)

---
+++ ExTeX/src/test/.prj.el
@@ -0,0 +1,9 @@
+; author: Sebastian Waschik
+; created: 2004-07-26
+; RCS-ID: $Id: .prj.el,v 1.1 2004/08/20 21:44:23 plaicy Exp $
+(jde-project-file-version "1.0")
+(jde-set-variables
+ '(jde-run-application-class "")
+ '(jde-run-working-directory "../..")
+ '(jde-run-read-app-args nil)
+)
