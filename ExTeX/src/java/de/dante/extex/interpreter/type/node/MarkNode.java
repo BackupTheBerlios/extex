3cf190f4263ac8b2fdc43f310275da606222658d
gene <gene>
2004-08-29 20:41:58 +0000

    variable mark added to bring the node to life

--- ExTeX/src/java/de/dante/extex/interpreter/type/node/MarkNode.java
+++ ExTeX/src/java/de/dante/extex/interpreter/type/node/MarkNode.java
@@ -1,23 +1,25 @@
 /*
  * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
+
 package de.dante.extex.interpreter.type.node;
 
+import de.dante.extex.interpreter.type.tokens.Tokens;
 import de.dante.extex.typesetter.Node;
 import de.dante.extex.typesetter.NodeVisitor;
 import de.dante.util.GeneralException;
@@ -28,20 +30,39 @@ import de.dante.util.GeneralException;
  * @see "TeX -- The Program [141]"
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.8 $
+ * @version $Revision: 1.9 $
  */
 public class MarkNode extends AbstractNode implements Node {
 
     /**
-     * Creates a new object.
+     * The field <tt>mark</tt> contains the ...
      */
-    public MarkNode() {
+    private Tokens mark;
+
+    /**
+     * Creates a new object.
+     *
+     * @param theMark the mark tokens to store
+     */
+    public MarkNode(final Tokens theMark) {
+
         super();
+        mark = theMark;
+    }
+
+    /**
+     * Getter for mark.
+     *
+     * @return the mark.
+     */
+    public Tokens getMark() {
+
+        return this.mark;
     }
 
     /**
      * This method returns the printable representation.
-     * This is meant to produce a exaustive form as it is used in tracing
+     * This is meant to produce a exhaustive form as it is used in tracing
      * output to the log file.
      *
      * @return the printable representation
@@ -49,6 +70,7 @@ public class MarkNode extends AbstractNode implements Node {
      * @see "TeX -- The Program [196]"
      */
     public String toString() {
+
         return "mark "; //TODO
     }
 
@@ -62,16 +84,6 @@ public class MarkNode extends AbstractNode implements Node {
     }
 
     /**
-     * @see de.dante.extex.typesetter.Node#visit(de.dante.extex.typesetter.NodeVisitor,
-     *      java.lang.Object, java.lang.Object)
-     */
-    public Object visit(final NodeVisitor visitor, final Object value,
-            final Object value2) throws GeneralException {
-
-        return visitor.visitMark(value, value2);
-    }
-
-    /**
      * @see de.dante.extex.typesetter.Node#visit(
      *      de.dante.extex.typesetter.NodeVisitor,
      *      java.lang.Object)
@@ -82,5 +94,14 @@ public class MarkNode extends AbstractNode implements Node {
         return visitor.visitMark(this, value);
     }
 
+    /**
+     * @see de.dante.extex.typesetter.Node#visit(de.dante.extex.typesetter.NodeVisitor,
+     *      java.lang.Object, java.lang.Object)
+     */
+    public Object visit(final NodeVisitor visitor, final Object value,
+            final Object value2) throws GeneralException {
+
+        return visitor.visitMark(value, value2);
+    }
 }
 
