8ac9413d5a6a659dc5232223e4208e29aaa08127
plaicy <plaicy>
2004-08-31 19:28:54 +0000

    - added workaround

--- ExTeX/src/test/.prj.el
+++ ExTeX/src/test/.prj.el
@@ -1,9 +1,25 @@
 ; author: Sebastian Waschik
 ; created: 2004-07-26
-; RCS-ID: $Id: .prj.el,v 1.1 2004/08/20 21:44:23 plaicy Exp $
+; RCS-ID: $Id: .prj.el,v 1.2 2004/08/31 19:28:54 plaicy Exp $
 (jde-project-file-version "1.0")
 (jde-set-variables
  '(jde-run-application-class "")
  '(jde-run-working-directory "../..")
  '(jde-run-read-app-args nil)
+ ;; TODO: do the following better (TE)
+ '(jde-sourcepath
+   (quote
+    ("./../../src/java"
+     "./../../src/test")))
+ '(jde-global-classpath
+   (quote
+    ("./../../target/classes"
+    "./../../lib"
+    "./../../lib.develop")))
+ '(jde-checkstyle-classpath
+   (quote
+    ("./../../lib.develop/checkstyle-all-3.4.jar"
+     "./../../lib.develop/checkstyle-optional-3.4.jar")))
+ '(jde-checkstyle-style "./../../.checkstyle.cfg")
+
 )
