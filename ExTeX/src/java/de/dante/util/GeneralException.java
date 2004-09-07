0e595d25b48c579d5d1878780d3e7e055014f2cf
gene <gene>
2004-09-07 07:00:36 +0000

    reformatted

--- ExTeX/src/java/de/dante/util/GeneralException.java
+++ ExTeX/src/java/de/dante/util/GeneralException.java
@@ -1,31 +1,32 @@
 /*
  * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
-package de.dante.util;
 
+package de.dante.util;
 
 /**
  * This is a base class for exceptions which carry a return code.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class GeneralException extends Exception {
+
     /**
      * The field <tt>exitCode</tt> contains the exit code.
      */
@@ -35,6 +36,7 @@ public class GeneralException extends Exception {
      * Creates a new object with the default exit code of -1.
      */
     public GeneralException() {
+
         super();
     }
 
@@ -44,6 +46,7 @@ public class GeneralException extends Exception {
      * @param code the exit code
      */
     public GeneralException(final int code) {
+
         super();
         exitCode = code;
     }
@@ -54,6 +57,7 @@ public class GeneralException extends Exception {
      * @param message the message
      */
     public GeneralException(final String message) {
+
         super(message);
     }
 
@@ -64,6 +68,7 @@ public class GeneralException extends Exception {
      * @param cause the cause for a chained exception
      */
     public GeneralException(final String message, final Throwable cause) {
+
         super(message, cause);
     }
 
@@ -73,6 +78,7 @@ public class GeneralException extends Exception {
      * @param cause the cause for a chained exception
      */
     public GeneralException(final Throwable cause) {
+
         super(cause);
     }
 
@@ -82,6 +88,7 @@ public class GeneralException extends Exception {
      * @return the exit code
      */
     public int getExitCode() {
+
         return exitCode;
     }
 
@@ -91,6 +98,7 @@ public class GeneralException extends Exception {
      * @return the help information
      */
     public String getHelp() {
+
         return null;
     }
 }
