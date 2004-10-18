225fbad4e177280d599d9401713f7b7067c8f062
plaicy <plaicy>
2004-10-18 21:52:18 +0000

    - added class AbstractReadonlyCount - added primitives \lastnodetype and \eTeXVersion

---
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/register/count/AbstractReadonlyCount.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.register.count;
+
+import de.dante.extex.i18n.HelpingException;
+import de.dante.extex.interpreter.Flags;
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.interpreter.type.AbstractCode;
+import de.dante.extex.interpreter.type.Theable;
+import de.dante.extex.interpreter.type.count.CountConvertible;
+import de.dante.extex.interpreter.type.tokens.Tokens;
+import de.dante.extex.typesetter.Typesetter;
+import de.dante.util.GeneralException;
+
+/**
+ * This abstract base class provides the methods an read only count
+ * primitive.
+ *
+ * @author <a href="mailto:sebastian.waschik@gmx.de">Sebastian Waschik</a>
+ * @version $Revision: 1.1 $
+ */
+public abstract class AbstractReadonlyCount extends AbstractCode
+        implements
+            CountConvertible,
+            Theable {
+
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for debugging
+     */
+    public AbstractReadonlyCount(final String name) {
+
+        super(name);
+    }
+
+
+    /**
+     * @see de.dante.extex.interpreter.type.Code#execute(
+     *      de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public boolean execute(final Flags prefix, final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws GeneralException {
+
+        System.out.println("Test");
+        throw new HelpingException("TTP.CantUseIn",
+                printableControlSequence(context), typesetter.getMode()
+                        .toString());
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.Theable#the(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public Tokens the(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws GeneralException {
+        System.out.println("Test");
+        long value = convertCount(context, source, typesetter);
+        return new Tokens(context, Long.toString(value));
+    }
+
+}
---
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/register/count/Lastnodetype.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.register.count;
+
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.typesetter.Node;
+import de.dante.extex.typesetter.Typesetter;
+import de.dante.util.GeneralException;
+
+
+/**
+ * This abstract base class provides the methods an read only count
+ * primitive.
+ *
+ * @author <a href="mailto:sebastian.waschik@gmx.de">Sebastian Waschik</a>
+ * @version $Revision: 1.1 $
+ */
+
+public class Lastnodetype extends AbstractReadonlyCount {
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for debugging
+     */
+    public Lastnodetype(final String name) {
+
+        super(name);
+    }
+
+
+   /**
+     * @see de.dante.extex.interpreter.type.count.CountConvertible#convertCount(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public long convertCount(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws GeneralException {
+
+        Node lastNode = typesetter.getLastNode();
+
+        // TODO: unimplemented (TE)
+        return 1234;
+    }
+}
