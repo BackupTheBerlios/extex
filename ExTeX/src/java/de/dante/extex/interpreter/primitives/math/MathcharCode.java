1902d3da6f0dbd1c1a270b9fa11254cebbb72b4f
gene <gene>
2004-12-04 22:35:40 +0000

    reformatting

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/math/MathcharCode.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/math/MathcharCode.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
 package de.dante.extex.interpreter.primitives.math;
@@ -29,12 +29,13 @@ import de.dante.util.GeneralException;
  * ...
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class MathcharCode extends Mathchar {
 
     /**
-     * The field <tt>mathchar</tt> contains the ...
+     * The field <tt>mathchar</tt> contains the actual character in the form
+     * of a Count which can immediately be passed to the typesetter.
      */
     private Count mathchar;
 
@@ -42,16 +43,17 @@ public class MathcharCode extends Mathchar {
      * Creates a new object.
      *
      * @param name the name for debugging
-     * @param mc the code of the math char
+     * @param charCode the code of the math char
      */
-    public MathcharCode(final String name, final Count mc) {
+    public MathcharCode(final String name, final Count charCode) {
 
         super(name);
-        mathchar = mc;
+        mathchar = charCode;
     }
 
     /**
-     * @see de.dante.extex.interpreter.type.Code#execute(de.dante.extex.interpreter.Flags,
+     * @see de.dante.extex.interpreter.type.Code#execute(
+     *      de.dante.extex.interpreter.Flags,
      *      de.dante.extex.interpreter.context.Context,
      *      de.dante.extex.interpreter.TokenSource,
      *      de.dante.extex.typesetter.Typesetter)
