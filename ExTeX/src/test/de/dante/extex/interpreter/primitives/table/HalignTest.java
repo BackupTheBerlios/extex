fca09be23522a8a9d0ddfe54f4e5f7496efc812d
gene <gene>
2004-12-06 10:43:07 +0000

    new

---
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/table/HalignTest.java
@@ -0,0 +1,223 @@
+/*
+ * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.table;
+
+import java.util.Properties;
+
+import de.dante.extex.main.exception.MainException;
+import de.dante.test.ExTeXLauncher;
+
+/**
+ * This is a test suite for the primitive <tt>\halign</tt>.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class HalignTest extends ExTeXLauncher {
+
+    /**
+     * Constructor for RelaxTest.
+     *
+     * @param arg the name
+     */
+    public HalignTest(final String arg) {
+
+        super(arg);
+    }
+
+    /**
+     * Test case checking that an outer macro in the preamble leads to an error.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testMissingBrace1() throws Exception {
+
+        Properties properties = System.getProperties();
+        properties.setProperty("extex.config", "nextex");
+
+        runCode(properties,
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\catcode`&=4"
+                + "\\catcode`#=6"
+                + ""
+                + "\\halign a"
+                + "\\end ",
+                "Missing `{' inserted",
+                null);
+    }
+
+    /**
+     * Test case checking that an outer macro in the preamble leads to an error.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testEof1() throws Exception {
+
+        Properties properties = System.getProperties();
+        properties.setProperty("extex.config", "nextex");
+
+        runCode(properties,
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\catcode`&=4"
+                + "\\catcode`#=6"
+                + ""
+                + "\\halign",
+                "Unexpected end of file while processing \\halign",
+                null);
+    }
+
+    /**
+     * Test case checking that an outer macro in the preamble leads to an error.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testOuter1() throws Exception {
+
+        Properties properties = System.getProperties();
+        properties.setProperty("extex.config", "nextex");
+
+        runCode(properties,
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\catcode`&=4"
+                + "\\catcode`#=6"
+                + "\\outer\\def\\x{}"
+                + ""
+                + "\\halign{\\x#&\\cr}"
+                + "\\end ",
+                "Forbidden control sequence found while scanning preamble of \\halign",
+                null);
+    }
+
+    /**
+     * Test case checking that an outer macro in the preamble leads to an error.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testOuter2() throws Exception {
+
+        Properties properties = System.getProperties();
+        properties.setProperty("extex.config", "nextex");
+
+        runCode(properties,
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\catcode`&=4"
+                + "\\catcode`#=6"
+                + "\\outer\\def\\x{}"
+                + ""
+                + "\\halign{a#\\x&\\cr}"
+                + "\\end ",
+                "Forbidden control sequence found while scanning preamble of \\halign",
+                null);
+    }
+
+    /**
+     * Test case checking that an outer macro in the preamble leads to an error.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testOuter3() throws Exception {
+
+        Properties properties = System.getProperties();
+        properties.setProperty("extex.config", "nextex");
+
+        runCode(properties,
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\catcode`&=4"
+                + "\\catcode`#=6"
+                + "\\outer\\def\\x{}"
+                + ""
+                + "\\halign{a#b&a#\\x&\\cr}"
+                + "\\end ",
+                "Forbidden control sequence found while scanning preamble of \\halign",
+                null);
+    }
+
+    /**
+     * Test case checking that a missing sharp in the preamble leads to an error.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testMissingSharp1() throws Exception {
+
+        Properties properties = System.getProperties();
+        properties.setProperty("extex.config", "nextex");
+
+        runCode(properties,
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\catcode`&=4"
+                + "\\catcode`#=6"
+                + ""
+                + "\\halign{a&#\\cr}"
+                + "\\end ",
+                "Missing # inserted in alignment preamble",
+                null);
+    }
+
+    /**
+     * Test case checking that a missing sharp in the preamble leads to an error.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testMissingSharp2() throws Exception {
+
+        Properties properties = System.getProperties();
+        properties.setProperty("extex.config", "nextex");
+
+        runCode(properties,
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\catcode`&=4"
+                + "\\catcode`#=6"
+                + ""
+                + "\\halign{#a&abc\\cr}"
+                + "\\end ",
+                "Missing # inserted in alignment preamble",
+                null);
+    }
+
+    /**
+     * Test case checking that several sharps in the preamble leads to an error.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testSharp1() throws Exception {
+
+        Properties properties = System.getProperties();
+        properties.setProperty("extex.config", "nextex");
+
+        runCode(properties,
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\catcode`&=4"
+                + "\\catcode`#=6"
+                + ""
+                + "\\halign{#a#\\cr}"
+                + "\\end ",
+                "Only one # is allowed per tab",
+                null);
+    }
+
+}
