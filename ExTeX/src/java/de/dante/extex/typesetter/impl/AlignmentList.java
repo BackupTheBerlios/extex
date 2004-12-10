4b378f109b116e075f8c39751e8935afede62bc8
gene <gene>
2004-12-10 08:34:03 +0000

    some signatures changed

--- ExTeX/src/java/de/dante/extex/typesetter/impl/AlignmentList.java
+++ ExTeX/src/java/de/dante/extex/typesetter/impl/AlignmentList.java
@@ -18,6 +18,8 @@
  */
 package de.dante.extex.typesetter.impl;
 
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.extex.interpreter.context.Context;
 import de.dante.util.GeneralException;
 
 
@@ -25,7 +27,7 @@ import de.dante.util.GeneralException;
  * ...
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public interface AlignmentList {
 
@@ -40,17 +42,19 @@ public interface AlignmentList {
 
     /**
      * ...
-     *
+     * @param context TODO
+     * @param source TODO
      * @throws GeneralException in case of an error
      */
-    void cr() throws GeneralException;
+    void cr(Context context, TokenSource source) throws GeneralException;
 
     /**
      * ...
-     *
+     * @param context TODO
+     * @param source TODO
      * @throws GeneralException in case of an error
      */
-    void crcr() throws GeneralException;
+    void crcr(Context context, TokenSource source) throws GeneralException;
 
     /**
      * ...
