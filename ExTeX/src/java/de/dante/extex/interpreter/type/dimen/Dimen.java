a86dcef64aa6fc03e3dc1e3df8dd1574f2f57f7b
gene <gene>
2004-12-11 17:08:51 +0000

    imports corrected

--- ExTeX/src/java/de/dante/extex/interpreter/type/dimen/Dimen.java
+++ ExTeX/src/java/de/dante/extex/interpreter/type/dimen/Dimen.java
@@ -25,7 +25,6 @@ import de.dante.extex.i18n.ArithmeticOverflowHelpingException;
 import de.dante.extex.i18n.HelpingException;
 import de.dante.extex.interpreter.TokenSource;
 import de.dante.extex.interpreter.context.Context;
-import de.dante.extex.interpreter.type.box.Box;
 import de.dante.extex.interpreter.type.glue.FixedGlueComponent;
 import de.dante.extex.interpreter.type.glue.GlueComponent;
 import de.dante.util.GeneralException;
@@ -38,7 +37,7 @@ import de.dante.util.framework.i18n.LocalizerFactory;
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.11 $
+ * @version $Revision: 1.12 $
  */
 public class Dimen extends GlueComponent implements Serializable, FixedDimen {
 
