9d890b01fef596842005910a6494b88430a3eb1a
gene <gene>
2004-12-15 07:41:43 +0000

    todo removed

--- ExTeX/src/java/de/dante/extex/interpreter/type/muskip/Muskip.java
+++ ExTeX/src/java/de/dante/extex/interpreter/type/muskip/Muskip.java
@@ -33,7 +33,7 @@ import de.dante.util.GeneralException;
  * ...
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  */
 public class Muskip implements Serializable {
 
@@ -125,7 +125,7 @@ public class Muskip implements Serializable {
 
         Token t = source.getToken();
         if (t == null) {
-            throw new EofHelpingException("mu");//TODO i18n?
+            throw new EofHelpingException("mu");
         }
         long value = GlueComponent.scanFloat(source, t);
         if (!source.getKeyword("mu")) {
