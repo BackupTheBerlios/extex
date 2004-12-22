8a4145f12035dc87361315c5ff937c92790c2847
gene <gene>
2004-12-22 22:33:47 +0000

    new

---
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/register/font/FontPrimitiveTest.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2004 Gerd Neugebauer
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.register.font;
+
+import java.util.Properties;
+
+import de.dante.test.ExTeXLauncher;
+
+/**
+ * This is a test suite for the primitive <tt>\font</tt>.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class FontPrimitiveTest extends ExTeXLauncher {
+
+    /**
+     * Constructor for the test.
+     *
+     * @param arg the name
+     */
+    public FontPrimitiveTest(final String arg) {
+
+        super(arg);
+    }
+
+    /**
+     * Test case checking that \fontdimen on unset keys returns 0 pt.
+     * @throws Exception in case of an error
+     */
+    public void testNoMacro1() throws Exception {
+
+        runCode(//--- input code ---
+                "\\font noFont at 10pt "
+                + "\\end",
+                //--- log message ---
+                "Missing control sequence inserted",
+                //--- output channel ---
+                "");
+    }
+
+    /**
+     * Test case checking that \font throws an error when the font is not found.
+     * @throws Exception in case of an error
+     */
+    public void testUndefined1() throws Exception {
+
+        runCode(//--- input code ---
+                "\\font\\abc=noFont at 10pt "
+                + "\\end",
+                //--- log message ---
+                "Font \\abc=noFont not loadable: Metric (TFM) file not found",
+                //--- output channel ---
+                "");
+    }
+
+    /**
+     * Test case checking that \font can load a font (cmtt12) without
+     * additional attributes.
+     * @throws Exception in case of an error
+     */
+    public void testFont1() throws Exception {
+
+        Properties properties = System.getProperties();
+        properties.setProperty("extex.output", "text");
+
+        runCode(properties,
+                //--- input code ---
+                "\\font\\abc=cmtt12 "
+                + "\\abc x"
+                + "\\end",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "\n\\cmtt12 x\n");
+    }
+
+    /**
+     * Test case checking that \font can load a font (cmtt12) with a given
+     * size attribute.
+     * @throws Exception in case of an error
+     */
+    public void testFont2() throws Exception {
+
+        runCode(//--- input code ---
+                "\\font\\abc=cmtt12 at 10pt "
+                + "\\end",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "\n");
+    }
+
+    /**
+     * Test case checking that \font can load a font (cmtt12) with a given
+     * positive scaled attribute.
+     * @throws Exception in case of an error
+     */
+    public void testFont3() throws Exception {
+
+        runCode(//--- input code ---
+                "\\font\\abc=cmtt12 scaled 2000 "
+                + "\\end",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "\n");
+    }
+
+    /**
+     * Test case checking that \font can load a font (cmtt12) to an active
+     * character.
+     * @throws Exception in case of an error
+     */
+    public void testFont4() throws Exception {
+
+        runCode(//--- input code ---
+                "\\catcode`\\/=11\\relax "
+                + "\\font/=cmtt12 "
+                + "\\end",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "\n");//TODO gene: check
+    }
+
+    /**
+     * Test case checking that \font produces an error message when the scaled
+     * attribute is negative.
+     * @throws Exception in case of an error
+     */
+    public void testNegativeScale1() throws Exception {
+
+        runCode(//--- input code ---
+                "\\font\\abc=cmtt12 scaled -2000 "
+                + "\\end",
+                //--- log message ---
+                "Illegal magnification has been changed to 1000",
+                //--- output channel ---
+                "\n");
+    }
+
+}
