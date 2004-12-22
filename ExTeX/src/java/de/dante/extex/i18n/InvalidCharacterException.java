cf31d7606cd6e783972979f39fec4f1a12161700
gene <gene>
2004-12-22 08:44:12 +0000

    new

---
+++ ExTeX/src/java/de/dante/extex/i18n/InvalidCharacterException.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.i18n;
+
+import de.dante.util.UnicodeChar;
+import de.dante.util.framework.i18n.LocalizerFactory;
+
+/**
+ * This exception is raised when an unexpected end of file is encountered.
+ * <p>
+ * The localization format is taken from the resource bundle of the parent
+ * class under the key <tt>UnexpectedEofIn</tt>.
+ * </p>
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class InvalidCharacterException extends HelpingException {
+
+    /**
+     * Creates a new object.
+     */
+    public InvalidCharacterException(final UnicodeChar uc) {
+
+        super(LocalizerFactory.getLocalizer(HelpingException.class.getName()),
+                "TTP.InvalidChar", uc.toString());
+    }
+
+}
