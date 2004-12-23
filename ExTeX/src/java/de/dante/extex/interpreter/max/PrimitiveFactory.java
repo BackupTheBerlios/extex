739201fce62228fae7f145130cc9f4094cd87e45
gene <gene>
2004-12-23 10:43:25 +0000

    minor improvement

--- ExTeX/src/java/de/dante/extex/interpreter/max/PrimitiveFactory.java
+++ ExTeX/src/java/de/dante/extex/interpreter/max/PrimitiveFactory.java
@@ -27,6 +27,7 @@ import de.dante.extex.interpreter.context.Context;
 import de.dante.extex.interpreter.type.Code;
 import de.dante.extex.interpreter.type.InitializableCode;
 import de.dante.extex.scanner.Catcode;
+import de.dante.extex.scanner.CodeToken;
 import de.dante.extex.scanner.TokenFactory;
 import de.dante.util.GeneralException;
 import de.dante.util.configuration.Configuration;
@@ -45,7 +46,7 @@ import de.dante.util.framework.logger.LogEnabled;
  * </pre>
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class PrimitiveFactory extends AbstractFactory {
 
@@ -99,8 +100,9 @@ public class PrimitiveFactory extends AbstractFactory {
             Code code = (Code) createInstanceForConfiguration(cfg, Code.class,
                     name);
 
-            context.setCode(tokenFactory.createToken(Catcode.ESCAPE, name,
-                    Namespace.DEFAULT_NAMESPACE), code, true);
+            context.setCode((CodeToken) tokenFactory.createToken(
+                    Catcode.ESCAPE, name, Namespace.DEFAULT_NAMESPACE), code,
+                    true);
             if (code instanceof LogEnabled) {
                 ((LogEnabled) code).enableLogging(outputLogger);
             }
