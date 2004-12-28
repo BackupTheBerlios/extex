d1a5f1c88c5bc0dbb0c9d45cb18a895b211a0a9f
gene <gene>
2004-12-28 10:01:04 +0000

    visibility fixed

--- ExTeX/src/java/de/dante/extex/typesetter/type/noad/CloseNoad.java
+++ ExTeX/src/java/de/dante/extex/typesetter/type/noad/CloseNoad.java
@@ -29,7 +29,7 @@ import de.dante.extex.typesetter.type.noad.util.MathContext;
  * @see "TTP [682]"
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class CloseNoad extends AbstractNucleusNoad {
 
@@ -38,7 +38,7 @@ public class CloseNoad extends AbstractNucleusNoad {
      *
      * @param nucleus the nucleus
      */
-    protected CloseNoad(final Noad nucleus) {
+    public CloseNoad(final Noad nucleus) {
 
         super(nucleus);
     }
