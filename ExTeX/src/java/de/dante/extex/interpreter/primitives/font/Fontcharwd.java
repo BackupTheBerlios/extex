d03658c048cb6c0a5c44e53ef7b4c7c432fdc4b5
gene <gene>
2004-12-29 09:23:28 +0000

    new implementation of the eTeX primitive

---
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/font/Fontcharwd.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.font;
+
+import de.dante.extex.font.Glyph;
+import de.dante.extex.i18n.CantUseHelpingException;
+import de.dante.extex.interpreter.Flags;
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.interpreter.type.AbstractCode;
+import de.dante.extex.interpreter.type.ExpandableCode;
+import de.dante.extex.interpreter.type.Theable;
+import de.dante.extex.interpreter.type.dimen.Dimen;
+import de.dante.extex.interpreter.type.dimen.DimenConvertible;
+import de.dante.extex.interpreter.type.font.Font;
+import de.dante.extex.interpreter.type.tokens.Tokens;
+import de.dante.extex.typesetter.Typesetter;
+import de.dante.util.GeneralException;
+import de.dante.util.UnicodeChar;
+
+/**
+ * This class provides an implementation for the primitive
+ * <code>\fontcharwd</code>.
+ *
+ * <doc name="fontcharwd">
+ * <h3>The Primitive <tt>\fontcharwd</tt></h3>
+ * <p>
+ *  TODO missing documentation
+ * </p>
+ * <p>
+ *  The formal description of this primitive is the following:
+ *  <pre class="syntax">
+ *    <tt>\fontcharwd</tt>    </pre>
+ * </p>
+ * <p>
+ *  Examples:
+ *  <pre class="TeXSample">
+ *    \fontcharwd\tenrm `a  </pre>
+ * </p>
+ * </doc>
+ *
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class Fontcharwd extends AbstractCode
+        implements
+            ExpandableCode,
+            DimenConvertible,
+            Theable {
+
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for debugging
+     */
+    public Fontcharwd(final String name) {
+
+        super(name);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.dimen.DimenConvertible#convertDimen(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public long convertDimen(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws GeneralException {
+
+        Font fnt = source.getFont();
+        UnicodeChar uc = source.scanCharacterCode();
+        Glyph glyph = fnt.getGlyph(uc);
+        if (glyph == null) {
+            return 0;
+        }
+        Dimen width = glyph.getWidth();
+        if (width == null) {
+            return 0;
+        }
+
+        return width.getValue();
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.Code#execute(
+     *      de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public boolean execute(final Flags prefix, final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws GeneralException {
+
+        throw new CantUseHelpingException(printableControlSequence(context),
+                typesetter.getMode().toString());
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.ExpandableCode#expand(
+     *      de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public void expand(final Flags prefix, final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws GeneralException {
+
+        long value = convertDimen(context, source, typesetter);
+        source.push(new Tokens(context, value));
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.Theable#the(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public Tokens the(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws GeneralException {
+
+        long value = convertDimen(context, source, typesetter);
+        return new Tokens(context, value);
+    }
+
+}
---
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/interaction/Interactionmode.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.interaction;
+
+import de.dante.extex.interpreter.Flags;
+import de.dante.extex.interpreter.Interaction;
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.interpreter.type.AbstractAssignment;
+import de.dante.extex.interpreter.type.count.CountConvertible;
+import de.dante.extex.typesetter.Typesetter;
+import de.dante.util.GeneralException;
+
+/**
+ * This class provides an implementation for the primitive
+ * <code>\interactionmode</code>. It does simply nothing, but as a side
+ * effect all prefixes are zeroed and the interaction mode is set to
+ * <tt>batchmode</tt>.
+ *
+ * <doc name="interactionmode">
+ * <h3>The Primitive <tt>\interactionmode</tt></h3>
+ * <p>
+ *  TODO missing documentation
+ * </p>
+ * <p>
+ *  The formal description of this primitive is the following:
+ *  <pre class="syntax">
+ *    &lang;interactionmode&rang;
+ *      &rarr; <tt>\interactionmode</tt>  </pre>
+ * </p>
+ * <p>
+ *  Examples:
+ *  <pre class="TeXSample">
+ *    \interactionmode  </pre>
+ * </p>
+ * </doc>
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class Interactionmode extends AbstractAssignment
+        implements
+            CountConvertible {
+
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for debugging
+     */
+    public Interactionmode(final String name) {
+
+        super(name);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.AbstractAssignment#assign(
+     *      de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public void assign(final Flags prefix, final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws GeneralException {
+
+        source.getOptionalEquals();
+        long mode = source.scanNumber();
+        context.setInteraction(Interaction.get((int) mode), prefix.isGlobal());
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.count.CountConvertible#convertCount(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public long convertCount(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws GeneralException {
+
+        return Interaction.get(context.getInteraction());
+    }
+
+}
