ffb5d1f576f9e3e0d68bb0d6869a6310c278a2ef
gene <gene>
2004-12-31 17:15:14 +0000

    todo removed

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/group/Begingroup.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/group/Begingroup.java
@@ -50,7 +50,7 @@ import de.dante.util.configuration.ConfigurationException;
  * </doc>
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.14 $
+ * @version $Revision: 1.15 $
  */
 public class Begingroup extends AbstractCode {
 
@@ -78,7 +78,7 @@ public class Begingroup extends AbstractCode {
         try {
             context.openGroup();
         } catch (ConfigurationException e) {
-            throw new GeneralException(e); //TODO gene: incomplete
+            throw new GeneralException(e);
         }
 
         return true;
