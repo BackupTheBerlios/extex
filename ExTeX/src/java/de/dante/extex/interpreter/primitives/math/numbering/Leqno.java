0d1d85ce993e52316c4fe0465453a01a78c63c72
gene <gene>
2005-01-03 10:36:39 +0000

    exception specialized

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/math/numbering/Leqno.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/math/numbering/Leqno.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -22,12 +22,11 @@ package de.dante.extex.interpreter.primitives.math.numbering;
 import de.dante.extex.interpreter.Flags;
 import de.dante.extex.interpreter.TokenSource;
 import de.dante.extex.interpreter.context.Context;
-import de.dante.extex.interpreter.exception.CantUseInException;
+import de.dante.extex.interpreter.exception.helping.CantUseInException;
 import de.dante.extex.interpreter.primitives.math.AbstractMathCode;
 import de.dante.extex.typesetter.ListMaker;
 import de.dante.extex.typesetter.Typesetter;
-import de.dante.extex.typesetter.listMaker.EqConsumer;
-import de.dante.extex.typesetter.listMaker.math.NoadConsumer;
+import de.dante.extex.typesetter.listMaker.math.EqConsumer;
 import de.dante.util.GeneralException;
 
 /**
@@ -52,7 +51,7 @@ import de.dante.util.GeneralException;
  * </doc>
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class Leqno extends AbstractMathCode {
 
@@ -75,7 +74,7 @@ public class Leqno extends AbstractMathCode {
      */
     public boolean execute(final Flags prefix, final Context context,
             final TokenSource source, final Typesetter typesetter)
-            throws GeneralException {
+            throws CantUseInException {
 
         ListMaker lm = typesetter.getListMaker();
         if (!(lm instanceof EqConsumer)) {
