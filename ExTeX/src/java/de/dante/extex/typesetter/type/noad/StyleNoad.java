ed58a69b0b21b3687b85727ef21cd1e60025067c
gene <gene>
2005-01-03 10:51:46 +0000

    *** empty log message ***

--- ExTeX/src/java/de/dante/extex/typesetter/type/noad/StyleNoad.java
+++ ExTeX/src/java/de/dante/extex/typesetter/type/noad/StyleNoad.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -28,7 +28,7 @@ import de.dante.extex.typesetter.type.noad.util.MathContext;
  * processing.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  */
 public final class StyleNoad implements Noad {
 
