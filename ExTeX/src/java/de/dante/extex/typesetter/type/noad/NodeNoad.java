d11c8f11fdd94c2bdb02b7bf7f9a150fc9638289
gene <gene>
2005-01-03 10:52:38 +0000

    new

---
+++ ExTeX/src/java/de/dante/extex/typesetter/type/noad/NodeNoad.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+package de.dante.extex.typesetter.type.noad;
+
+import de.dante.extex.typesetter.Node;
+import de.dante.extex.typesetter.NodeList;
+import de.dante.extex.typesetter.TypesetterOptions;
+import de.dante.extex.typesetter.type.noad.util.MathContext;
+
+
+/**
+ * This noad contains a node which is passed through the math apparatus.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class NodeNoad implements Noad {
+
+    /**
+     * The field <tt>node</tt> contains the encapsulated node.
+     */
+    private Node node;
+
+    /**
+     * Creates a new object.
+     *
+     * @param node the node to
+     */
+    public NodeNoad(final Node node) {
+
+        super();
+        this.node = node;
+    }
+
+    /**
+     * @see de.dante.extex.typesetter.type.noad.Noad#getSubscript()
+     */
+    public Noad getSubscript() {
+
+        return null;
+    }
+
+    /**
+     * @see de.dante.extex.typesetter.type.noad.Noad#getSuperscript()
+     */
+    public Noad getSuperscript() {
+
+        return null;
+    }
+
+    /**
+     * @see de.dante.extex.typesetter.type.noad.Noad#setSubscript(
+     *      de.dante.extex.typesetter.type.noad.Noad)
+     */
+    public void setSubscript(final Noad subscript) {
+
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @see de.dante.extex.typesetter.type.noad.Noad#setSuperscript(
+     *      de.dante.extex.typesetter.type.noad.Noad)
+     */
+    public void setSuperscript(final Noad superscript) {
+
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @see de.dante.extex.typesetter.type.noad.Noad#toString(
+     *      java.lang.StringBuffer)
+     */
+    public void toString(final StringBuffer sb) {
+
+        node.toString(sb, "");
+    }
+
+    /**
+     * @see de.dante.extex.typesetter.type.noad.Noad#toString(
+     *      java.lang.StringBuffer, int)
+     */
+    public void toString(final StringBuffer sb, final int depth) {
+
+        node.toString(sb, ""); //TODO gene: incorrect
+    }
+
+    /**
+     * @see de.dante.extex.typesetter.type.noad.Noad#typeset(
+     *      de.dante.extex.typesetter.NodeList,
+     *      de.dante.extex.typesetter.type.noad.util.MathContext,
+     *      de.dante.extex.typesetter.TypesetterOptions)
+     */
+    public void typeset(final NodeList list, final MathContext mathContext,
+            final TypesetterOptions context) {
+
+        list.add(node);
+    }
+
+    /**
+     * @see de.dante.extex.typesetter.type.noad.Noad#visit(
+     *      de.dante.extex.typesetter.type.noad.NoadVisitor)
+     */
+    public void visit(final NoadVisitor visitor) {
+
+        // TODO gene: visit unimplemented
+
+    }
+
+}
