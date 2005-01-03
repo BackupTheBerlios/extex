51f025cd3590e862db3e1d56c8a8aaa0978349fa
gene <gene>
2005-01-03 10:39:38 +0000

    BinNoad renamed to BinaryNoad

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/math/Mathbin.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/math/Mathbin.java
@@ -24,7 +24,7 @@ import de.dante.extex.interpreter.TokenSource;
 import de.dante.extex.interpreter.context.Context;
 import de.dante.extex.typesetter.Typesetter;
 import de.dante.extex.typesetter.listMaker.math.NoadConsumer;
-import de.dante.extex.typesetter.type.noad.BinNoad;
+import de.dante.extex.typesetter.type.noad.BinaryNoad;
 import de.dante.extex.typesetter.type.noad.Noad;
 import de.dante.util.GeneralException;
 
@@ -51,7 +51,7 @@ import de.dante.util.GeneralException;
  * </doc>
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class Mathbin extends AbstractMathCode {
 
@@ -78,7 +78,7 @@ public class Mathbin extends AbstractMathCode {
 
         NoadConsumer nc = getListMaker(context, typesetter);
         Noad noad = nc.scanNoad(context, source);
-        nc.add(new BinNoad(noad));
+        nc.add(new BinaryNoad(noad));
         return true;
     }
 
