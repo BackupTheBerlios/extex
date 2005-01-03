513d4eef68ed9945d62a5c529b6b6fae69a90057
gene <gene>
2005-01-03 10:43:37 +0000

    todo added

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/math/MathTest.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/math/MathTest.java
@@ -25,7 +25,7 @@ import de.dante.test.ExTeXLauncher;
  * This is a test suite for the primitive <tt>\mathaccent</tt>.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class MathTest extends ExTeXLauncher {
 
@@ -49,7 +49,7 @@ public class MathTest extends ExTeXLauncher {
                 "\\catcode`$=3 "
                 + "$a$",
                 //--- log message ---
-                "?",
+                "?", //TODO gene: check
                 //--- output channel ---
                 null);
     }
