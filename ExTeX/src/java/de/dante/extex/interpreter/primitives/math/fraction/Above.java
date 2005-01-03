9c89d88c56956836ce237620c4411649d0576b27
gene <gene>
2005-01-03 15:03:03 +0000

    documentation improved

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/math/fraction/Above.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/math/fraction/Above.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -45,12 +45,12 @@ import de.dante.util.GeneralException;
  * <p>
  *  Examples:
  *  <pre class="TeXSample">
- *    \above  </pre>
+ *    {a \above b}  </pre>
  * </p>
  * </doc>
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class Above extends AbstractMathCode {
 
