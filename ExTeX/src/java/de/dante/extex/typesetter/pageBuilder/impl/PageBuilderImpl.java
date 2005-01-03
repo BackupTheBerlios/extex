13fbeaf4fcfc7c059a0fbc5cb3af467c1cadb9d5
gene <gene>
2005-01-03 10:48:54 +0000

    PanicException replaced by GeneralException

--- ExTeX/src/java/de/dante/extex/typesetter/pageBuilder/impl/PageBuilderImpl.java
+++ ExTeX/src/java/de/dante/extex/typesetter/pageBuilder/impl/PageBuilderImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -22,7 +22,6 @@ package de.dante.extex.typesetter.pageBuilder.impl;
 import java.io.IOException;
 
 import de.dante.extex.documentWriter.DocumentWriter;
-import de.dante.extex.i18n.PanicException;
 import de.dante.extex.typesetter.NodeList;
 import de.dante.extex.typesetter.pageBuilder.PageBuilder;
 import de.dante.util.GeneralException;
@@ -31,7 +30,7 @@ import de.dante.util.GeneralException;
  * TODO gene: missing JavaDoc.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class PageBuilderImpl implements PageBuilder {
 
@@ -62,7 +61,7 @@ public class PageBuilderImpl implements PageBuilder {
         try {
             documentWriter.close();
         } catch (IOException e) {
-            throw new PanicException(e);
+            throw new GeneralException(e);
         }
 
     }
@@ -81,7 +80,7 @@ public class PageBuilderImpl implements PageBuilder {
         try {
             this.documentWriter.shipout(nodes);
         } catch (IOException e) {
-            throw new PanicException(e);
+            throw new GeneralException(e);
         }
     }
 
@@ -117,7 +116,7 @@ public class PageBuilderImpl implements PageBuilder {
         try {
             this.documentWriter.shipout(nodes);
         } catch (IOException e) {
-            throw new PanicException(e);
+            throw new GeneralException(e);
         }
     }
 }
