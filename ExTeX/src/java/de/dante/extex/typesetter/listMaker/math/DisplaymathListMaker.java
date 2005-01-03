4888f7699513d74642d86257d7cfbed23bf40fc7
gene <gene>
2005-01-03 10:47:57 +0000

    minor improvements

--- ExTeX/src/java/de/dante/extex/typesetter/listMaker/math/DisplaymathListMaker.java
+++ ExTeX/src/java/de/dante/extex/typesetter/listMaker/math/DisplaymathListMaker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -19,23 +19,21 @@
 
 package de.dante.extex.typesetter.listMaker.math;
 
-import de.dante.extex.interpreter.exception.CantUseInException;
+import de.dante.extex.interpreter.exception.helping.CantUseInException;
 import de.dante.extex.interpreter.type.node.HorizontalListNode;
 import de.dante.extex.typesetter.Mode;
 import de.dante.extex.typesetter.NodeList;
 import de.dante.extex.typesetter.TypesetterOptions;
-import de.dante.extex.typesetter.listMaker.EqConsumer;
 import de.dante.extex.typesetter.listMaker.ListManager;
 import de.dante.extex.typesetter.type.noad.MathList;
 import de.dante.extex.typesetter.type.noad.StyleNoad;
 import de.dante.extex.typesetter.type.noad.util.MathContext;
-import de.dante.util.GeneralException;
 
 /**
  * This is the list maker for the display math formulae.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class DisplaymathListMaker extends MathListMaker implements EqConsumer {
 
@@ -69,8 +67,8 @@ public class DisplaymathListMaker extends MathListMaker implements EqConsumer {
 
         HorizontalListNode list = new HorizontalListNode();
 
-        getNoades().typeset(list, new MathContext(StyleNoad.DISPLAYSTYLE, context),
-                context);
+        getNoades().typeset(list,
+                new MathContext(StyleNoad.DISPLAYSTYLE, context), context);
 
         if (eqno != null) {
             //TODO gene: unimplemented
@@ -80,17 +78,23 @@ public class DisplaymathListMaker extends MathListMaker implements EqConsumer {
     }
 
     /**
+     * @see de.dante.extex.typesetter.ListMaker#getMode()
+     */
+    public Mode getMode() {
+
+        return Mode.DISPLAYMATH;
+    }
+
+    /**
      * This method switches the collection of material to the target "equation
      * number".
      *
      * @param left the indicator on which side to produce the equation number.
      *  A value <code>true</code> indicates that the left side should be used.
      *
-     * @throws GeneralException in case on an error
-     *
-     * @see de.dante.extex.typesetter.listMaker.EqConsumer#switchToNumber(boolean)
+     * @see de.dante.extex.typesetter.listMaker.math.EqConsumer#switchToNumber(boolean)
      */
-    public void switchToNumber(final boolean left) throws GeneralException {
+    public void switchToNumber(final boolean left) throws CantUseInException {
 
         if (eqno != null) {
             throw new CantUseInException(null, null);
@@ -99,14 +103,6 @@ public class DisplaymathListMaker extends MathListMaker implements EqConsumer {
         leq = left;
         eqno = new MathList();
         setInsertionPoint(eqno);
-    }
-
-    /**
-     * @see de.dante.extex.typesetter.ListMaker#getMode()
-     */
-    public Mode getMode() {
-
-        return Mode.DISPLAYMATH;
     }
 
 }
