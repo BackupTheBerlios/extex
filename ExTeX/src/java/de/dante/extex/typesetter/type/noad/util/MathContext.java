da8b481861309c4b0ce17f20be0ba6593e21189a
gene <gene>
2005-01-04 09:54:19 +0000

    conversion for mudimen added

--- ExTeX/src/java/de/dante/extex/typesetter/type/noad/util/MathContext.java
+++ ExTeX/src/java/de/dante/extex/typesetter/type/noad/util/MathContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -19,10 +19,13 @@
 
 package de.dante.extex.typesetter.type.noad.util;
 
+import de.dante.extex.interpreter.exception.ImpossibleException;
 import de.dante.extex.interpreter.primitives.register.font.NumberedFont;
+import de.dante.extex.interpreter.type.dimen.Dimen;
 import de.dante.extex.interpreter.type.font.Font;
 import de.dante.extex.interpreter.type.glue.Glue;
 import de.dante.extex.interpreter.type.glue.GlueComponent;
+import de.dante.extex.interpreter.type.muskip.Mudimen;
 import de.dante.extex.interpreter.type.muskip.Muskip;
 import de.dante.extex.typesetter.TypesetterOptions;
 import de.dante.extex.typesetter.type.noad.StyleNoad;
@@ -32,7 +35,7 @@ import de.dante.extex.typesetter.type.noad.StyleNoad;
  * mathematical appearance.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class MathContext {
 
@@ -100,6 +103,8 @@ public class MathContext {
      * @param muglue the math glue to convert
      *
      * @return a new instance of a glue corresponding to the parameter
+     *
+     * @see "TTP [716]"
      */
     public Glue convert(final Muskip muglue) {
 
@@ -113,8 +118,7 @@ public class MathContext {
             fnt = context.getFont(NumberedFont.key(context, "scriptscriptfont",
                     "2"));
         } else {
-            //TODO gene: impossible
-            throw new RuntimeException("impossible");
+            throw new ImpossibleException("undefined style");
         }
 
         long factor = fnt.getEm().getValue();
@@ -133,6 +137,46 @@ public class MathContext {
         }
 
         return new Glue(length, stretch, shrink);
+    }
+
+    /**
+     * Convert a mudimen into a dimen.
+     *
+     * From The TeXbook:
+     *
+     * <p><i>
+     * There are 18 mu to an em, where the em is taken from family~2
+     * (the math symbols family). In other words, ^|\textfont|~|2| defines the em
+     * value for |mu| in display and text styles; ^|\scriptfont|~|2| defines the
+     * em for script size material; and ^|\scriptscriptfont|~|2| defines it for
+     * scriptscript size.
+     * </i></p>
+     *
+     * @param mudimen the math dimen to convert
+     *
+     * @return a new instance of a Dimen corresponding to the parameter
+     *
+     * @see "TTP [717]"
+     */
+    public Dimen convert(final Mudimen mudimen) {
+
+        Font fnt = null;
+
+        if (style == StyleNoad.TEXTSTYLE || style == StyleNoad.DISPLAYSTYLE) {
+            fnt = context.getFont(NumberedFont.key(context, "textfont", "2"));
+        } else if (style == StyleNoad.SCRIPTSTYLE) {
+            fnt = context.getFont(NumberedFont.key(context, "scriptfont", "2"));
+        } else if (style == StyleNoad.SCRIPTSCRIPTSTYLE) {
+            fnt = context.getFont(NumberedFont.key(context, "scriptscriptfont",
+                    "2"));
+        } else {
+            throw new ImpossibleException("undefined style");
+        }
+
+        Dimen length = new Dimen(mudimen.getLength());
+        length.multiply(fnt.getEm().getValue(), MU_UNIT);
+
+        return length;
     }
 
 }
