2bc9dff7fe987e77df3c4842170cf7e79427cebe
gene <gene>
2005-01-05 18:10:06 +0000

    todos removed

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/font/Fontvalue.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/font/Fontvalue.java
@@ -1,19 +1,19 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
 
@@ -45,7 +45,7 @@ import de.dante.util.GeneralException;
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.8 $
+ * @version $Revision: 1.9 $
  */
 public class Fontvalue extends AbstractAssignment implements Theable {
 
@@ -76,7 +76,6 @@ public class Fontvalue extends AbstractAssignment implements Theable {
         String key = source.scanTokensAsString(context);
         if (key == null || key.length() == 0) {
             throw new HelpingException(getLocalizer(), "FONT.fontkeynotfound");
-            //TODO gene: i18n
         }
 
         source.getOptionalEquals(context);
@@ -98,7 +97,6 @@ public class Fontvalue extends AbstractAssignment implements Theable {
         String key = source.scanTokensAsString(context);
         if (key == null || key.length() == 0) {
             throw new HelpingException(getLocalizer(), "FONT.fontkeynotfound");
-            //TODO gene: i18n
         }
         Dimen size = font.getFontDimen(key);
 
