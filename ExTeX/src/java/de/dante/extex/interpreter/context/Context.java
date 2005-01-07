8cb88d485db4ab0b3fecdb462f343e7897b871dd
gene <gene>
2005-01-07 13:52:43 +0000

    year corrected

--- ExTeX/src/java/de/dante/extex/interpreter/context/Context.java
+++ ExTeX/src/java/de/dante/extex/interpreter/context/Context.java
@@ -1,5 +1,5 @@
-5/*
- * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
+/*
+ * Copyright (C) 2003-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -51,7 +51,7 @@ import de.dante.util.configuration.ConfigurationException;
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.41 $
+ * @version $Revision: 1.42 $
  */
 public interface Context
         extends
