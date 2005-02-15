955d12f975449aaaf63e150663e521ea6855d86f
gene <gene>
2005-02-15 09:50:07 +0000

    several new test cases

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/macro/CsnameTest.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/macro/CsnameTest.java
@@ -25,7 +25,7 @@ import de.dante.test.ExTeXLauncher;
  * This is a test suite for the primitive <tt>\csname</tt>.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class CsnameTest extends ExTeXLauncher {
 
@@ -40,6 +40,98 @@ public class CsnameTest extends ExTeXLauncher {
     }
 
     /**
+     * Test case checking that the normal operation is performed on letter
+     * inputs only.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testLetters1() throws Exception {
+
+        runCode(//--- input code ---
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\def\\abc{-a-b-c-}" +
+                "\\csname abc\\endcsname\\end",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "-a-b-c-\n\n-a-b-c-\n\n");
+    }
+
+    /**
+     * Test case checking that the normal operation is performed on letter
+     * inputs only and whitespace is ignored.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testLetters2() throws Exception {
+
+        runCode(//--- input code ---
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\def\\abc{-a-b-c-}" +
+                "\\csname a b  c\\endcsname\\end",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "-a-b-c-\n\n-a-b-c-\n\n");
+    }
+
+    /**
+     * Test case checking that the normal operation is performed on letter
+     * inputs only and whitespace and \relax is ignored.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testLetters3() throws Exception {
+
+        runCode(//--- input code ---
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\def\\abc{-a-b-c-}" +
+                "\\csname a b \\relax c\\endcsname\\end",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "-a-b-c-\n\n-a-b-c-\n\n");
+    }
+
+    /**
+     * Test case checking that the normal operation is performed on letter
+     * and digit inputs only.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testMixed1() throws Exception {
+
+        runCode(//--- input code ---
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\def\\abc{-a-b-c-}" +
+                "\\csname abc 123\\endcsname\\end",
+                //--- log message ---
+                "Undefined control sequence",
+                //--- output channel ---
+                "");
+    }
+
+    /**
+     * Test case checking that the normal operation is performed on letter
+     * and digit inputs only.
+     *
+     * @throws Exception in case of an error
+     */
+    public void testError1() throws Exception {
+
+        runCode(//--- input code ---
+                "\\csname abc \\par\\endcsname\\end",
+                //--- log message ---
+                "Missing \\endcsname inserted",
+                //--- output channel ---
+                "");
+    }
+
+    /**
      * Test case checking that eof is recognized.
      *
      * @throws Exception in case of an error
@@ -51,7 +143,7 @@ public class CsnameTest extends ExTeXLauncher {
                 //--- log message ---
                 "",
                 //--- output channel ---
-                "\\=undefined\n");
+                "\\=undefined\n\n\\=undefined\n\n");
     }
 
 }
