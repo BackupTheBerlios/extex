0da01b9c7956162096180d11a70abc9e8281cbc1
gene <gene>
2005-03-29 12:52:20 +0000

    typo corrected

--- ExTeX/src/java/de/dante/extex/typesetter/type/Node.java
+++ ExTeX/src/java/de/dante/extex/typesetter/type/Node.java
@@ -41,7 +41,7 @@ import de.dante.util.UnicodeChar;
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public interface Node extends Knot {
 
@@ -168,7 +168,7 @@ public interface Node extends Knot {
 
     /**
      * This method puts the printable representation into the string buffer.
-     * This is meant to produce a exaustive form as it is used in tracing
+     * This is meant to produce a exhaustive form as it is used in tracing
      * output to the log file.
      *
      * @param sb the output string buffer
