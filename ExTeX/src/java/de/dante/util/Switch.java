06d4462593e29d36caea155b9f7fa301b07ad7cd
gene <gene>
2005-04-18 10:43:12 +0000

    formatting corrected

--- ExTeX/src/java/de/dante/util/Switch.java
+++ ExTeX/src/java/de/dante/util/Switch.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -25,7 +25,7 @@ package de.dante.util;
  * also a setter for the encapsulated boolean value.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class Switch {
 
@@ -54,6 +54,7 @@ public class Switch {
 
         return value;
     }
+
     /**
      * Setter for value.
      *
