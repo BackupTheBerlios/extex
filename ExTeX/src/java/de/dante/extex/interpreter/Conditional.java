e9aee542c00a0b6e16504cbcd3456aefd2e0c16d
gene <gene>
2005-04-19 16:01:55 +0000

    typo corrected

--- ExTeX/src/java/de/dante/extex/interpreter/Conditional.java
+++ ExTeX/src/java/de/dante/extex/interpreter/Conditional.java
@@ -1,19 +1,19 @@
 /*
- * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2005 The ExTeX Group and individual authors listed below
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
 
@@ -24,10 +24,11 @@ import java.io.Serializable;
 import de.dante.util.Locator;
 
 /**
- * This class represents a Confitional for a normal \if \else \fi construct.
+ * This class represents a conditional for a normal <tt>\if \else \fi</tt>
+ * construct.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.9 $
+ * @version $Revision: 1.10 $
  */
 public class Conditional implements Serializable {
 
@@ -40,12 +41,12 @@ public class Conditional implements Serializable {
     /**
      * Creates a new object.
      *
-     * @param aLocator the locator
+     * @param locator the locator
      */
-    public Conditional(final Locator aLocator) {
+    public Conditional(final Locator locator) {
 
         super();
-        this.locator = aLocator;
+        this.locator = locator;
     }
 
     /**
