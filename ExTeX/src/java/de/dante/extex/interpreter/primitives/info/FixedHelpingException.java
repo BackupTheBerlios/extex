93e7916ce62a6f0244dcd8a370532651d2189667
gene <gene>
2005-04-27 08:12:43 +0000

    documentation improved

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/info/FixedHelpingException.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/info/FixedHelpingException.java
@@ -1,21 +1,22 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
+
 package de.dante.extex.interpreter.primitives.info;
 
 import de.dante.extex.interpreter.exception.helping.HelpingException;
@@ -25,14 +26,22 @@ import de.dante.extex.interpreter.exception.helping.HelpingException;
  * help on the error encoutered. Thus it has two levels of information: the
  * first level is the message and the second level is the additional help.
  * <p>
- * In contrast to {@link de.dante.extex.interpreter.exception.helping.HelpingException HelpingException}
- * the messages are not mapped.
+ * In contrast to
+ * {@link de.dante.extex.interpreter.exception.helping.HelpingException HelpingException}
+ * the messages are not mapped. Thus they are not subject to
+ * internationalization.
  * </p>
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class FixedHelpingException extends HelpingException {
+
+    /**
+     * The field <tt>help</tt> contains the string which is shown if further
+     * help is requested.
+     */
+    private String help;
 
     /**
      * The field <tt>message</tt> contains the message. We need to keep it here
@@ -40,12 +49,6 @@ public class FixedHelpingException extends HelpingException {
      * the underlying Exception.
      */
     private String message;
-
-    /**
-     * The field <tt>help</tt> contains the string which is shown if further
-     * help is requested.
-     */
-    private String help;
 
     /**
      * Creates a new object.
