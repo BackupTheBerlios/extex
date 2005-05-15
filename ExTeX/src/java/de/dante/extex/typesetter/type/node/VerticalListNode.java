50b28bbe0ee80bba80fa996f8192f7d6f86890c5
gene <gene>
2005-05-15 07:45:31 +0000

    improvements

--- ExTeX/src/java/de/dante/extex/typesetter/type/node/VerticalListNode.java
+++ ExTeX/src/java/de/dante/extex/typesetter/type/node/VerticalListNode.java
@@ -19,8 +19,13 @@
 
 package de.dante.extex.typesetter.type.node;
 
+import java.util.logging.Logger;
+
+import de.dante.extex.interpreter.type.dimen.Dimen;
 import de.dante.extex.interpreter.type.dimen.FixedDimen;
 import de.dante.extex.interpreter.type.glue.FixedGlue;
+import de.dante.extex.interpreter.type.glue.Glue;
+import de.dante.extex.typesetter.Badness;
 import de.dante.extex.typesetter.type.Node;
 import de.dante.extex.typesetter.type.NodeList;
 import de.dante.extex.typesetter.type.NodeVisitor;
@@ -33,7 +38,7 @@ import de.dante.util.GeneralException;
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class VerticalListNode extends AbstractNodeList implements NodeList {
 
@@ -53,21 +58,111 @@ public class VerticalListNode extends AbstractNodeList implements NodeList {
      */
     public void addSkip(final FixedGlue glue) {
 
-        Node gNode = new GlueNode(glue); // TODO gene: use factory?
+        Node gNode = new GlueNode(glue);
         gNode.setHeight(glue.getLength());
         add(gNode);
+    }
+
+    /**
+     * compute the penalty for the spilt at a given position.
+     *
+     * @param penalty the base penalty optionally coming from a penalty node
+     * @param ht the actual height
+     * @param height the desired height
+     *
+     * @return the penalty in the range 0 to 10000, including
+     */
+    private long computePenalty(final long penalty, final Glue ht,
+            final FixedDimen height) {
+
+        long badness = Badness.badness(height.getValue(), //
+                ht.getLength().getValue());
+        long p = penalty;
+        // TODO gene: computePenalty unimplemented
+        return p;
+    }
+
+    /**
+     * Split off material from a vlist of a desired height. The splitting is
+     * performed at a position with minimal penalty. The list is stretched to
+     * the desired height.
+     *
+     * @param height the target height
+     * @param traceLogger the logger for tracing
+     * @param logger the logger of normal logging output
+     *
+     * @return the split off material
+     */
+    public VerticalListNode split(final FixedDimen height, final Logger logger,
+            final Logger traceLogger) {
+
+        long penalty;
+        long bestPenalty = 10001;
+        Dimen bestHeight = Dimen.ZERO_PT;
+        Glue ht = new Glue(0);
+        int size = size();
+        int bestSplit = size;
+
+        for (int i = 0; i < size; i++) {
+            Node node = get(i);
+            node.addHeightTo(ht);
+            if (i + 1 >= size || !(get(i + 1) instanceof PenaltyNode)) {
+                penalty = computePenalty((node instanceof PenaltyNode
+                        ? ((PenaltyNode) node).getPenalty()
+                        : 0), ht, height);
+                if (penalty < bestPenalty) {
+                    bestPenalty = penalty;
+                    bestSplit = i;
+                    bestHeight = ht.getLength();
+                }
+            }
+        }
+
+        if (traceLogger != null) {
+            traceLogger.info("% split" + "???" + " to " + height + ","
+                    + bestHeight + " p=" + bestPenalty);
+        }
+        VerticalListNode result = new VerticalListNode();
+        for (int i = 0; i < bestSplit; i++) {
+            result.add(get(i));
+        }
+
+        return result;
     }
 
     /**
      * Spread the list vertically to a desired size by distributing the
      * differences to the glues contained.
      *
-     * @param size the target size
+     * @param height the target size
+     *
+     * @return the badness of the spread
      */
-    public void spread(final FixedDimen size) {
+    public long spread(final FixedDimen height) {
 
-        //TODO gene: spread() unimplemented
-        throw new RuntimeException("unimplemented");
+        Glue ht = new Glue(0);
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            get(i).addHeightTo(ht);
+        }
+
+        Dimen length = ht.getLength();
+        for (int i = 0; i < size; i++) {
+            get(i).spread(height, length);
+        }
+
+        return Badness.badness(height.getValue(), length.getValue());
+    }
+
+    /**
+     * @see de.dante.extex.typesetter.type.Node#toString(
+     *      java.lang.StringBuffer,
+     *      java.lang.String)
+     */
+    public void toString(final StringBuffer sb, final String prefix) {
+
+        sb.append("\\vbox");
+        super.toString(sb, prefix);
     }
 
     /**
@@ -81,13 +176,14 @@ public class VerticalListNode extends AbstractNodeList implements NodeList {
     }
 
     /**
-     * @see de.dante.extex.typesetter.type.Node#toString(java.lang.StringBuffer,
-     *      java.lang.String)
+     * @see de.dante.extex.typesetter.type.node.AbstractNodeList#updateDimensions(
+     *      de.dante.extex.typesetter.type.Node)
      */
-    public void toString(final StringBuffer sb, final String prefix) {
+    protected void updateDimensions(final Node node) {
 
-        sb.append("\\vbox");
-        super.toString(sb, prefix);
+        getWidth().max(node.getWidth());
+        getHeight().add(node.getHeight()); //TODO gene: incorrect
+        getDepth().add(node.getDepth()); //TODO gene: incorrect
     }
 
     /**
@@ -100,16 +196,4 @@ public class VerticalListNode extends AbstractNodeList implements NodeList {
 
         return visitor.visitVerticalList(this, value);
     }
-
-    /**
-     * @see de.dante.extex.typesetter.type.node.AbstractNodeList#updateDimensions(
-     *      de.dante.extex.typesetter.type.Node)
-     */
-    protected void updateDimensions(final Node node) {
-
-        getWidth().max(node.getWidth());
-        getHeight().add(node.getHeight()); //TODO gene: incorrect
-        getDepth().add(node.getDepth()); //TODO gene: incorrect
-    }
-
 }
