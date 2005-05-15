622f00e74f2089ffdaceb26f9065811f28fc5651
gene <gene>
2005-05-15 07:40:15 +0000

    reformatted

--- ExTeX/src/java/de/dante/util/UnicodeChar.java
+++ ExTeX/src/java/de/dante/util/UnicodeChar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2005 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -16,6 +16,7 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
+
 package de.dante.util;
 
 import java.io.Serializable;
@@ -34,7 +35,7 @@ import com.ibm.icu.text.UTF16;
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.19 $
+ * @version $Revision: 1.20 $
  */
 public class UnicodeChar implements Serializable {
 
@@ -49,17 +50,6 @@ public class UnicodeChar implements Serializable {
      * (32 bit).
      */
     private int code;
-
-    /**
-     * Creates a new object from an integer code point.
-     *
-     * @param codePoint the 32-bit code point
-     */
-    public UnicodeChar(final int codePoint) {
-
-        super();
-        this.code = codePoint;
-    }
 
     /**
      * Creates a new object from a 16-bit character.
@@ -85,7 +75,85 @@ public class UnicodeChar implements Serializable {
     }
 
     /**
-     * Init with a char32 from a String at position idx.
+     * Init with a char32 from a <code>CharBuffer</code> at position idx.
+     * <p>
+     * This use the code from <code>UTF16.charAt(String,int)</code> and
+     * change String to CharBuffer.
+     *
+     * @param cb the <code>CharBuffer</code>
+     * @param idx the position in the charbuffer
+     */
+    public UnicodeChar(final CharBuffer cb, final int idx) {
+
+        super();
+
+        if (idx < 0 || idx >= cb.length()) {
+            throw new StringIndexOutOfBoundsException(idx);
+        }
+
+        char single = cb.charAt(idx);
+        if (single < UTF16.LEAD_SURROGATE_MIN_VALUE
+                || single > UTF16.TRAIL_SURROGATE_MAX_VALUE) {
+            this.code = single;
+        } else {
+
+            this.code = single;
+
+            // Convert the UTF-16 surrogate pair if necessary.
+            // For simplicity in usage, and because the frequency of pairs is
+            // low, look both directions.
+            if (single <= UTF16.LEAD_SURROGATE_MAX_VALUE) {
+                if (cb.length() > (idx + 1)) {
+                    char trail = cb.charAt(idx + 1);
+                    if (trail >= UTF16.TRAIL_SURROGATE_MIN_VALUE
+                            && trail <= UTF16.TRAIL_SURROGATE_MAX_VALUE) {
+                        this.code = UCharacterProperty.getRawSupplementary(
+                                single, trail);
+                    } else {
+                        throw new RuntimeException("This can't happen?");
+                    }
+                } else {
+                    throw new RuntimeException("This can't happen?");
+                }
+            } else if (idx > 0) {
+                char lead = cb.charAt(idx - 1);
+                if (lead >= UTF16.LEAD_SURROGATE_MIN_VALUE
+                        && lead <= UTF16.LEAD_SURROGATE_MAX_VALUE) {
+                    this.code = UCharacterProperty.getRawSupplementary(lead,
+                            single);
+                } else {
+                    throw new RuntimeException("This can't happen?");
+                }
+            } else {
+                throw new RuntimeException("This can't happen?");
+            }
+        }
+    }
+
+    /**
+     * Creates a new object from an integer code point.
+     *
+     * @param codePoint the 32-bit code point
+     */
+    public UnicodeChar(final int codePoint) {
+
+        super();
+        this.code = codePoint;
+    }
+
+    /**
+     * Creates a new object from a Unicode name.
+     *
+     * @param unicodeName Unicode name as String
+     */
+    public UnicodeChar(final String unicodeName) {
+
+        super();
+        this.code = UCharacter.getCharFromName(unicodeName);
+    }
+
+    /**
+     * Init with a char32 from a String at position index.
      *
      * @param s the <code>String</code>
      * @param index the position in the string
@@ -109,80 +177,52 @@ public class UnicodeChar implements Serializable {
     }
 
     /**
-     * Init with a char32 from a <code>CharBuffer</code> at position idx.
+     * Compares a <code>UnicodeChar</code> character with the value of this
+     * object. They are considered equal if the are both UnicodeChars and have
+     * the same code.
      * <p>
-     * This use the code from <code>UTF16.charAt(String,int)</code> and
-     * change String to CharBuffer.
+     * The general signature for comparison to an arbitray object is required
+     * for the implementation of {@link java.util.HashMap HashMap} and friends.
+     * </p>
      *
-     * @param cb the <code>CharBuffer</code>
-     * @param idx the position in the charbuffer
+     * @param unicodeChar the character to compare
+     * @return <code>true</code> if the characters are equal, otherwise
+     *         <code>false</code>
      */
-    public UnicodeChar(final CharBuffer cb, final int idx) {
+    public boolean equals(final Object unicodeChar) {
 
-        super();
-
-        if (idx < 0 || idx >= cb.length()) {
-            throw new StringIndexOutOfBoundsException(idx);
-        }
-
-        char single = cb.charAt(idx);
-        if (single < UTF16.LEAD_SURROGATE_MIN_VALUE
-            || single > UTF16.TRAIL_SURROGATE_MAX_VALUE) {
-            this.code = single;
-        } else {
-
-            this.code = single;
-
-            // Convert the UTF-16 surrogate pair if necessary.
-            // For simplicity in usage, and because the frequency of pairs is
-            // low, look both directions.
-            if (single <= UTF16.LEAD_SURROGATE_MAX_VALUE) {
-                if (cb.length() > (idx + 1)) {
-                    char trail = cb.charAt(idx + 1);
-                    if (trail >= UTF16.TRAIL_SURROGATE_MIN_VALUE
-                        && trail <= UTF16.TRAIL_SURROGATE_MAX_VALUE) {
-                        this.code = UCharacterProperty.getRawSupplementary(single,
-                                                                      trail);
-                    } else {
-                        throw new RuntimeException("This can't happen?");
-                    }
-                } else {
-                    throw new RuntimeException("This can't happen?");
-                }
-            } else if (idx > 0) {
-                char lead = cb.charAt(idx - 1);
-                if (lead >= UTF16.LEAD_SURROGATE_MIN_VALUE
-                        && lead <= UTF16.LEAD_SURROGATE_MAX_VALUE) {
-                    this.code = UCharacterProperty.getRawSupplementary(lead,
-                            single);
-                } else {
-                    throw new RuntimeException("This can't happen?");
-                }
-            } else {
-                throw new RuntimeException("This can't happen?");
-            }
-        }
+        return ((unicodeChar instanceof UnicodeChar) && //
+        this.code == ((UnicodeChar) unicodeChar).getCodePoint());
     }
 
     /**
-     * Creates a new object from a Unicode name.
+     * Return the count of char16 of the code.
      *
-     * @param unicodeName Unicode name as String
+     * @return the count of char16 for this code
      */
-    public UnicodeChar(final String unicodeName) {
+    public int getChar16Count() {
 
-        super();
-        this.code = UCharacter.getCharFromName(unicodeName);
+        return UTF16.getCharCount(this.code);
     }
 
     /**
-     * Return the unicode codepoint.
+     * Return the unicode code point.
      *
-     * @return the unicode codepoint
+     * @return the unicode code point
      */
     public int getCodePoint() {
 
         return this.code;
+    }
+
+    /**
+     * Returns the bidirection property of the character.
+     *
+     * @return the bidirection property
+     */
+    public int getDirection() {
+
+        return UCharacter.getDirection(this.code);
     }
 
     /**
@@ -193,6 +233,51 @@ public class UnicodeChar implements Serializable {
     public String getUnicodeName() {
 
         return UCharacter.getName(this.code);
+    }
+
+    /**
+     * Computes the hash code for the character. The hash code of equal objects
+     * must be equal, but the hash code of different object need not to be
+     * different. This is needed for the implementations of HashMap and friends.
+     *
+     * @return the hash code
+     */
+    public int hashCode() {
+
+        return this.code;
+    }
+
+    /**
+     * Test, if the code is a digit.
+     *
+     * @return <code>true</code>, if the code is a digit,
+     * otherwise <code>false</code>
+     */
+    public boolean isDigit() {
+
+        return UCharacter.isDigit(this.code);
+    }
+
+    /**
+     * Test, if the character is a letter.
+     *
+     * @return <code>true</code>, if the code is a letter,
+     * otherwise <code>false</code>
+     */
+    public boolean isLetter() {
+
+        return UCharacter.isLetter(this.code);
+    }
+
+    /**
+     * Test, if the code is printable.
+     *
+     * @return <code>true</code>, if the code is printable,
+     * otherwise <code>false</code>
+     */
+    public boolean isPrintable() {
+
+        return UCharacter.isPrintable(this.code);
     }
 
     /**
@@ -209,60 +294,6 @@ public class UnicodeChar implements Serializable {
     }
 
     /**
-     * Returns the uppercase character of this object.
-     * <p>
-     * (this method does not use the TeX uccode!)
-     * </p>
-     *
-     * @return character in uppercase
-     */
-    public int toUpperCase() {
-
-        return UCharacter.toUpperCase(this.code);
-    }
-
-    /**
-     * Compares a <code>UnicodeChar</code> character with the value of this
-     * object. They are considered equal if the are both UnicodeChars and have
-     * the same code.
-     * <p>
-     * The general signature for comparison to an arbitray object is required
-     * for the implementation of {@link java.util.HashMap HashMap} and friends.
-     * </p>
-     *
-     * @param unicodeChar the character to compare
-     * @return <code>true</code> if the characters are equal, otherwise
-     *         <code>false</code>
-     */
-    public boolean equals(final Object unicodeChar) {
-
-        return ((unicodeChar instanceof UnicodeChar) && //
-                this.code == ((UnicodeChar) unicodeChar).getCodePoint());
-    }
-
-    /**
-     * Computes the hash code for the character. The hash code of equal objects
-     * must be equal, but the hash code of different object need not to be
-     * different. This is needed for the implementations of HashMap and friends.
-     *
-     * @return the hash code
-     */
-    public int hashCode() {
-
-        return this.code;
-    }
-
-    /**
-     * Returns the bidirection property of the character.
-     *
-     * @return the bidirection property
-     */
-    public int getDirection() {
-
-        return UCharacter.getDirection(this.code);
-    }
-
-    /**
      * Returns a String of this object.
      *
      * @return String representation of the stored value.
@@ -273,46 +304,16 @@ public class UnicodeChar implements Serializable {
     }
 
     /**
-     * Test, if the character is a letter.
+     * Returns the uppercase character of this object.
+     * <p>
+     * (this method does not use the TeX uccode!)
+     * </p>
      *
-     * @return <code>true</code>, if the code is a letter,
-     * otherwise <code>false</code>
+     * @return character in uppercase
      */
-    public boolean isLetter() {
+    public int toUpperCase() {
 
-        return UCharacter.isLetter(this.code);
-    }
-
-    /**
-     * Test, if the code is a digit.
-     *
-     * @return <code>true</code>, if the code is a digit,
-     * otherwise <code>false</code>
-     */
-    public boolean isDigit() {
-
-        return UCharacter.isDigit(this.code);
-    }
-
-    /**
-     * Test, if the code is printable.
-     *
-     * @return <code>true</code>, if the code is printable,
-     * otherwise <code>false</code>
-     */
-    public boolean isPrintable() {
-
-        return UCharacter.isPrintable(this.code);
-    }
-
-    /**
-     * Return the count of char16 of the code.
-     *
-     * @return the count of char16 for this code
-     */
-    public int getChar16Count() {
-
-        return UTF16.getCharCount(this.code);
+        return UCharacter.toUpperCase(this.code);
     }
 
 }
