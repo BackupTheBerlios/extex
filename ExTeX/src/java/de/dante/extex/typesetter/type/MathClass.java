141def7f25a77ec0e2fb320fe13b3af3475f3e72
gene <gene>
2005-05-16 12:34:48 +0000

    extended form implemented

--- ExTeX/src/java/de/dante/extex/typesetter/type/MathClass.java
+++ ExTeX/src/java/de/dante/extex/typesetter/type/MathClass.java
@@ -19,12 +19,13 @@
 
 package de.dante.extex.typesetter.type;
 
+
 /**
  * This class provides the classification of mathematical characters.
  * In fact it is a finite enumeration which exposes the values as constants.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public abstract class MathClass {
 
@@ -32,7 +33,7 @@ public abstract class MathClass {
      * This is a inner class for a binary operator.
      *
      * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
-     * @version $Revision: 1.4 $
+     * @version $Revision: 1.5 $
      */
     private static final class BinaryMathClass extends MathClass {
 
@@ -61,7 +62,7 @@ public abstract class MathClass {
      * This is a inner class for closing.
      *
      * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
-     * @version $Revision: 1.4 $
+     * @version $Revision: 1.5 $
      */
     private static final class ClosingMathClass extends MathClass {
 
@@ -82,7 +83,7 @@ public abstract class MathClass {
          */
         public Object visit(final MathClassVisitor visitor, final Object arg) {
 
-            return visitor.visitclosing(arg);
+            return visitor.visitClosing(arg);
         }
     }
 
@@ -90,7 +91,7 @@ public abstract class MathClass {
      * This is a inner class for large operators.
      *
      * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
-     * @version $Revision: 1.4 $
+     * @version $Revision: 1.5 $
      */
     private static final class LargeMathClass extends MathClass {
 
@@ -119,7 +120,7 @@ public abstract class MathClass {
      * This is a inner class for opening.
      *
      * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
-     * @version $Revision: 1.4 $
+     * @version $Revision: 1.5 $
      */
     private static final class OpeningMathClass extends MathClass {
 
@@ -148,7 +149,7 @@ public abstract class MathClass {
      * This is a inner class for ordinary characters.
      *
      * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
-     * @version $Revision: 1.4 $
+     * @version $Revision: 1.5 $
      */
     private static final class OrdinaryMathClass extends MathClass {
 
@@ -177,7 +178,7 @@ public abstract class MathClass {
      * This is a inner class for punctation marks.
      *
      * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
-     * @version $Revision: 1.4 $
+     * @version $Revision: 1.5 $
      */
     private static final class PunctationMathClass extends MathClass {
 
@@ -206,7 +207,7 @@ public abstract class MathClass {
      * This is a inner class for relation symbols.
      *
      * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
-     * @version $Revision: 1.4 $
+     * @version $Revision: 1.5 $
      */
     private static final class RelationMathClass extends MathClass {
 
@@ -235,7 +236,7 @@ public abstract class MathClass {
      * This is a inner class for variable width characters.
      *
      * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
-     * @version $Revision: 1.4 $
+     * @version $Revision: 1.5 $
      */
     private static final class VariableMathClass extends MathClass {
 
@@ -262,55 +263,55 @@ public abstract class MathClass {
 
     /**
      * The field <tt>BINARY</tt> contains the instance representing the binary
-     * class. This class has the code 2 in TeX.
+     * class. This class has the code 2 in <logo>TeX</logo>.
      */
     public static final MathClass BINARY = new BinaryMathClass();
 
     /**
      * The field <tt>CLOSING</tt> contains the instance representing the closing
-     * class. This class has the code 5 in TeX.
+     * class. This class has the code 5 in <logo>TeX</logo>.
      */
     public static final MathClass CLOSING = new ClosingMathClass();
 
     /**
      * The field <tt>LARGE</tt> contains the instance representing the large
-     * class. This class has the code 1 in TeX.
+     * class. This class has the code 1 in <logo>TeX</logo>.
      */
     public static final MathClass LARGE = new LargeMathClass();
 
     /**
      * The field <tt>OPENING</tt> contains the instance representing the opening
-     * class. This class has the code 4 in TeX.
+     * class. This class has the code 4 in <logo>TeX</logo>.
      */
     public static final MathClass OPENING = new OpeningMathClass();
 
     /**
      * The field <tt>ORDINARY</tt> contains the instance representing the
-     * ordinary class. This class has the code 0 in TeX.
+     * ordinary class. This class has the code 0 in <logo>TeX</logo>.
      */
     public static final MathClass ORDINARY = new OrdinaryMathClass();
 
     /**
      * The field <tt>PUNCTUATION</tt> contains the instance representing the
-     * punctation class. This class has the code 6 in TeX.
+     * punctation class. This class has the code 6 in <logo>TeX</logo>.
      */
     public static final MathClass PUNCTUATION = new PunctationMathClass();
 
     /**
      * The field <tt>RELATION</tt> contains the instance representing the
-     * relation class. This class has the code 3 in TeX.
+     * relation class. This class has the code 3 in <logo>TeX</logo>.
      */
     public static final MathClass RELATION = new RelationMathClass();
 
     /**
      * The field <tt>VARIABLE</tt> contains the instance representing the
-     * variable width class. This class has the code 7 in TeX.
+     * variable width class. This class has the code 7 in <logo>TeX</logo>.
      */
     public static final MathClass VARIABLE = new VariableMathClass();
 
     /**
-     * The field <tt>mc</tt> contains the mapping from TeX numbers to
-     * instances.
+     * The field <tt>mc</tt> contains the mapping from <logo>TeX</logo> numbers
+     * to instances.
      *
      * @see MathClass#getMathClass(int)
      */
@@ -318,40 +319,49 @@ public abstract class MathClass {
             OPENING, CLOSING, PUNCTUATION, VARIABLE};
 
     /**
-     * Factory method for the math class which maps the TeX encoding into the
-     * appropriate instance. The following table gives a mapping from TeX
-     * numbers to instances:
+     * Factory method for the math class which maps the <logo>TeX</logo>
+     * encoding into the appropriate instance. The following table gives a
+     * mapping from <logo>TeX</logo> numbers to instances:
      * <table>
      *  <tr>
-     *   <td>0</td><td>ORDINARY</td>
+     *   <td>0</td>
+     *   <td>{@link de.dante.extex.typesetter.type.MathClass#ORDINARY ORDINARY}</td>
      *  </tr>
      *  <tr>
-     *   <td>1</td><td>LARGE</td>
+     *   <td>1</td>
+     *   <td>{@link de.dante.extex.typesetter.type.MathClass#LARGE LARGE}</td>
      *  </tr>
      *  <tr>
-     *   <td>2</td><td>BINARY</td>
+     *   <td>2</td>
+     *   <td>{@link de.dante.extex.typesetter.type.MathClass#BINARY BINARY}</td>
      *  </tr>
      *  <tr>
-     *   <td>3</td><td>RELATION</td>
+     *   <td>3</td>
+     *   <td>{@link de.dante.extex.typesetter.type.MathClass#RELATION RELATION}</td>
      *  </tr>
      *  <tr>
-     *   <td>4</td><td>OPENING</td>
+     *   <td>4</td>
+     *   <td>{@link de.dante.extex.typesetter.type.MathClass#OPENING OPENING}</td>
      *  </tr>
      *  <tr>
-     *   <td>5</td><td>CLOSING</td>
+     *   <td>5</td>
+     *   <td>{@link de.dante.extex.typesetter.type.MathClass#CLOSING CLOSING}</td>
      *  </tr>
      *  <tr>
-     *   <td>6</td><td>PUNCTATION</td>
+     *   <td>6</td>
+     *   <td>{@link de.dante.extex.typesetter.type.MathClass#PUNCTATION PUNCTATION}</td>
      *  </tr>
      *  <tr>
-     *   <td>7</td><td>VARIABLE</td>
+     *   <td>7</td>
+     *   <td>{@link de.dante.extex.typesetter.type.MathClass#VARIABLE VARIABLE}</td>
      *  </tr>
      * </table>
      *
      *
-     * @param n the TeX encoded index of the class
+     * @param n the <logo>TeX</logo> encoded index of the class
      *
-     * @return the MathClass instance corresponding to the TeX code
+     * @return the MathClass instance corresponding to the <logo>TeX</logo>
+     *  code
      */
     public static final MathClass getMathClass(final int n) {
 
