947097ea97cd1e25738613cd63c255587c6467fe
gene <gene>
2005-06-06 08:15:55 +0000

    new

---
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/conditional/Iffontchar.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.conditional;
+
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.interpreter.exception.InterpreterException;
+import de.dante.extex.interpreter.type.font.Font;
+import de.dante.extex.typesetter.Typesetter;
+import de.dante.util.UnicodeChar;
+
+/**
+ * This class provides an implementation for the primitive
+ * <code>\iffontchar</code>.
+ *
+ * <doc name="iffontchar">
+ * <h3>The Primitive <tt>\iffontchar</tt></h3>
+ * <p>
+ *  The primitive <tt>\iffontchar</tt> can be used to check whether a certain
+ *  glyph exists in a font. For this purpose it takes a font and the code of a
+ *  character and performs the test. If the character exists the then branch is
+ *  expanded otherwise the else branch.
+ * </p>
+ * <p>
+ *  The formal description of this primitive is the following:
+ *  <pre class="syntax">
+ *    &lang;iffontchar&rang;
+ *      &rarr; <tt>\iffontchar</tt> ... &lang;true text&rang; <tt>\fi</tt>
+ *      | <tt>\iffontchar</tt> ... &lang;true text&rang; <tt>\else</tt> &lang;false text&rang; <tt>\fi</tt> </pre>
+ * </p>
+ * <p>
+ *  Examples:
+ *  <pre class="TeXSample">
+ *    \iffontchar abc \fi  </pre>
+ * </p>
+ * </doc>
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class Iffontchar extends AbstractIf {
+
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for debugging
+     */
+    public Iffontchar(final String name) {
+
+        super(name);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.primitives.conditional.AbstractIf#conditional(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    protected boolean conditional(final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws InterpreterException {
+
+        Font font = source.getFont(context);
+        UnicodeChar uc = source.scanCharacterCode(context);
+        return (font.getGlyph(uc) != null);
+    }
+}
