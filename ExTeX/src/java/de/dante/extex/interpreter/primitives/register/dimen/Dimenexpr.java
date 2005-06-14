452e9dd7ca7881d6f65e579f67325f347f4fa24d
gene <gene>
2005-06-14 10:02:06 +0000

    \dimenexpr added

---
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/register/dimen/Dimenexpr.java
@@ -0,0 +1,327 @@
+/*
+ * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.register.dimen;
+
+import de.dante.extex.interpreter.Flags;
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.interpreter.exception.InterpreterException;
+import de.dante.extex.interpreter.exception.helping.ArithmeticOverflowException;
+import de.dante.extex.interpreter.exception.helping.EofException;
+import de.dante.extex.interpreter.exception.helping.HelpingException;
+import de.dante.extex.interpreter.exception.helping.MissingNumberException;
+import de.dante.extex.interpreter.primitives.Relax;
+import de.dante.extex.interpreter.type.AbstractCode;
+import de.dante.extex.interpreter.type.Code;
+import de.dante.extex.interpreter.type.ExpandableCode;
+import de.dante.extex.interpreter.type.Theable;
+import de.dante.extex.interpreter.type.count.CountConvertible;
+import de.dante.extex.interpreter.type.dimen.Dimen;
+import de.dante.extex.interpreter.type.dimen.DimenConvertible;
+import de.dante.extex.interpreter.type.tokens.Tokens;
+import de.dante.extex.scanner.type.Catcode;
+import de.dante.extex.scanner.type.CatcodeException;
+import de.dante.extex.scanner.type.CodeToken;
+import de.dante.extex.scanner.type.LetterToken;
+import de.dante.extex.scanner.type.OtherToken;
+import de.dante.extex.scanner.type.Token;
+import de.dante.extex.typesetter.Typesetter;
+
+/**
+ * This class provides an implementation for the primitive <code>\dimenexpr</code>.
+ *
+ * <doc name="dimenexpr">
+ * <h3>The Primitive <tt>\dimenexpr</tt></h3>
+ * <p>
+ *  The primitive <tt>\dimenexpr</tt> provides a means to use a inline way of
+ *  writing mathematical expressions to be evaluated. Mathematical expressions
+ *  can be evaluated in <logo>ExTeX</logo> using <tt>\advance</tt>,
+ *  <tt>\multiply</tt>, and <tt>\divide</tt>. Nevertheless those primitives
+ *  result in an assignment. This is not the case for <tt>\dimenexpr</tt>. Here
+ *  the intermediate results are not stored in dimen registers but kept
+ *  internally. Also the application of <tt>\afterassignment</tt> and
+ *  <tt>\tracingassigns</tt> is suppressed.
+ * </p>
+ * <p>
+ *  The mathematical expression to be evaluated can be made up of the basic
+ *  operations addition (+), subtraction (-), multiplication (*) with numbers,
+ *  and division(/) by numbers. The unary minus can be used. Parentheses can be
+ *  used for grouping. Anything which looks like a length can be used as
+ *  argument. White-space can be used freely without any harm.
+ * </p>
+ * <p>
+ *  The expression is terminated at the first token which can not be part of
+ *  an expression. For instance a letter may signal the end of the expression.
+ *  If the expression should terminate without a proper token following it,
+ *  the token <tt>\relax</tt> can be used to signal the end of the expression.
+ *  This <tt>\relax</tt> token is silently consumed by <tt>\dimenexpr</tt>.
+ * </p>
+ * <p>
+ *  The primitive <tt>\dimenexpr</tt> can be used in any place where a dimen is
+ *  required. This includes assignments to dimen registers and comparisons.
+ * </p>
+ * <p>
+ *  The formal description of this primitive is the following:
+ *  <pre class="syntax">
+ *    &lang;dimenexpr&rang;
+ *      &rarr; <tt>\dimenexpr</tt> &lang;expr&rang; <tt>\relax</tt>
+ *      |   <tt>\dimenexpr</tt> &lang;expr&rang;
+ *
+ *    &lang;expr&rang;
+ *      &rarr; &lang;dimen&rang;
+ *      |   &lang;operand&rang;
+ *      |   &lang;operand&rang; <tt>+</tt> &lang;operand&rang;
+ *      |   &lang;operand&rang; <tt>-</tt> &lang;operand&rang;
+ *
+ *    &lang;operand&rang;
+ *      &rarr; &lang;dimen&rang;
+ *      |   &lang;operand&rang;
+ *      |   &lang;operand&rang; <tt>*</tt> &lang;number&rang;
+ *      |   &lang;number&rang; <tt>*</tt> &lang;operand&rang;
+ *      |   &lang;operand&rang; <tt>/</tt> &lang;number&rang;
+ *      |   <tt>-</tt> &lang;expr&rang;
+ *      |   <tt>(</tt> &lang;expr&rang; <tt>)</tt>   </pre>
+ * </p>
+ * <h4>Examples</h4>
+ * <pre class="TeXSample">
+ *   \count1=\dimenexpr 23pt \relax </pre>
+ * <pre class="TeXSample">
+ *   \count1=\dimenexpr 2 * 3pt \relax </pre>
+ * <pre class="TeXSample">
+ *   \count1=\dimenexpr 2pt*\count2  </pre>
+ * <pre class="TeXSample">
+ *   \count1=\dimenexpr 2*(1pt+3em)  </pre>
+ * <pre class="TeXSample">
+ *   \count1=\dimenexpr 2*-\dimen0  </pre>
+ * </doc>
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class Dimenexpr extends AbstractCode
+        implements
+            DimenConvertible,
+            CountConvertible,
+            Theable {
+
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for debugging
+     */
+    public Dimenexpr(final String name) {
+
+        super(name);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.dimen.DimenConvertible#convertDimen(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public long convertDimen(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws InterpreterException {
+
+        long result = evalExpr(context, source, typesetter);
+        Token t = source.getToken(context);
+        if (!(t instanceof CodeToken)
+                || !(context.getCode((CodeToken) t) instanceof Relax)) {
+            source.push(t);
+        }
+        return result;
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.count.CountConvertible#convertCount(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public long convertCount(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws InterpreterException {
+
+        long result = evalExpr(context, source, typesetter);
+        Token t = source.getToken(context);
+        if (!(t instanceof CodeToken)
+                || !(context.getCode((CodeToken) t) instanceof Relax)) {
+            source.push(t);
+        }
+        return result;
+    }
+
+    /**
+     * Evaluate an expression.
+     *
+     * @param context the interpreter context
+     * @param source the source for new tokens
+     * @param typesetter the typesetter
+     *
+     * @return the result
+     *
+     * @throws InterpreterException in case of an error
+     */
+    private long evalExpr(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws InterpreterException {
+
+        long val = evalOperand(context, source, typesetter);
+
+        for (;;) {
+
+            Token t = source.getNonSpace(context);
+            if (t == null) {
+                throw new EofException(getName());
+
+            } else if (t.equals(Catcode.OTHER, '+')) {
+                val += evalOperand(context, source, typesetter);
+            } else if (t.equals(Catcode.OTHER, '-')) {
+                val -= evalOperand(context, source, typesetter);
+
+            } else {
+                source.push(t);
+                return val;
+            }
+        }
+    }
+
+    private long evalTerminal(final Context context, final TokenSource source,
+            final Typesetter typesetter, final Token start)
+            throws InterpreterException {
+
+        Token t = start;
+        for (;;) {
+            if (t instanceof OtherToken) {
+                if (t.equals(Catcode.OTHER, '(')) {
+                    long val = evalExpr(context, source, typesetter);
+                    t = source.getToken(context);
+                    if (!t.equals(Catcode.OTHER, ')')) {
+                        throw new HelpingException(getLocalizer(),
+                                "MissingParenthesis", (t == null ? "null" : t
+                                        .toString()));
+                    }
+                    return val;
+                } else if (t.equals(Catcode.OTHER, '-')) {
+                    return -evalOperand(context, source, typesetter);
+                } else if (t.equals(Catcode.OTHER, '.')
+                        || t.equals(Catcode.OTHER, ',')) {
+                    source.push(t);
+                    return new Dimen(context, source, typesetter).getValue();
+                }
+                long pre = source.scanNumber(context, t);
+                t = source.getToken(context);
+                if (t == null) {
+                    return pre;
+                } else if (t.equals(Catcode.OTHER, '.')
+                        || t.equals(Catcode.OTHER, ',')) {
+                    source.push(t);
+                    source.push(new Tokens(context, pre));
+                    return new Dimen(context, source, typesetter).getValue();
+                }
+                source.push(t);
+                t = source.getNonSpace(context);
+                if (t == null) {
+                    throw new MissingNumberException();
+
+                } else if (t instanceof LetterToken) {
+                    source.push(t);
+                    source.push(new Tokens(context, pre));
+                    return new Dimen(context, source, typesetter).getValue();
+                } else {
+                    source.push(t);
+                    source.push(new Tokens(context, pre));
+                    return source.scanNumber(context);
+                }
+
+            } else if (t instanceof CodeToken) {
+                Code code = context.getCode((CodeToken) t);
+                if (code instanceof DimenConvertible) {
+                    return ((DimenConvertible) code).convertDimen(context,
+                            source, typesetter);
+                } else if (code instanceof CountConvertible) {
+                    return ((CountConvertible) code).convertCount(context,
+                            source, typesetter);
+                } else if (code instanceof ExpandableCode) {
+                    ((ExpandableCode) code).expand(Flags.NONE, context, source,
+                            typesetter);
+                } else {
+                    throw new MissingNumberException();
+                }
+
+            } else {
+                throw new MissingNumberException();
+            }
+        }
+    }
+
+    /**
+     * Evaluate an operand.
+     *
+     * @param context the interpreter context
+     * @param source the source for new tokens
+     * @param typesetter the typesetter
+     *
+     * @return the result
+     *
+     * @throws InterpreterException in case of an error
+     */
+    private long evalOperand(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws InterpreterException {
+
+        long val = evalTerminal(context, source, typesetter, source
+                .getNonSpace(context));
+
+        for (Token t = source.getNonSpace(context); t != null; t = source
+                .getNonSpace(context)) {
+
+            if (t.equals(Catcode.OTHER, '*')) {
+                val *= evalOperand(context, source, typesetter);
+            } else if (t.equals(Catcode.OTHER, '/')) {
+                long x = evalOperand(context, source, typesetter);
+                if (x == 0) {
+                    throw new ArithmeticOverflowException(getName());
+                }
+                val /= x;
+            } else {
+                source.push(t);
+                return val;
+            }
+        }
+        return val;
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.Theable#the(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public Tokens the(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws InterpreterException {
+
+        try {
+            //todo gene: find a solution without intermediary object
+            Dimen d = new Dimen(convertDimen(context, source, typesetter));
+            return d.toToks(context.getTokenFactory());
+        } catch (CatcodeException e) {
+            throw new InterpreterException(e);
+        }
+    }
+
+}
---
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/register/dimen/DimenexprTest.java
@@ -0,0 +1,473 @@
+/*
+ * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+package de.dante.extex.interpreter.primitives.register.dimen;
+
+import de.dante.test.ExTeXLauncher;
+
+
+/**
+ * This is a test suite for the primitive <tt>\dimenexpr</tt>.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class DimenexprTest extends ExTeXLauncher {
+
+    /**
+     * Command line interface.
+     * @param args the arguments
+     */
+    public static void main(final String[] args) {
+
+        junit.textui.TestRunner.run(DimenexprTest.class);
+    }
+
+    /**
+     * Constructor for NumexprTest.
+     *
+     * @param arg the name
+     */
+    public DimenexprTest(final String arg) {
+
+        super(arg);
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that <tt>\dimenexpr</tt> without any term produces an
+     *  error.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void testEOF1() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr \\relax",
+                //--- log message ---
+                "Missing number, treated as zero",
+                //--- output channel ---
+                "");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that <tt>\dimenexpr</tt> with a non-balanced
+     *  parenthesis produces an error.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void testError2() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr ( 123 \\relax",
+                //--- log message ---
+                "Missing ) inserted for expression",
+                //--- output channel ---
+                "");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that <tt>\dimenexpr</tt> with a non-balanced
+     *  parenthesis produces an error.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void testError4() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr ( 123pt \\relax",
+                //--- log message ---
+                "Missing ) inserted for expression",
+                //--- output channel ---
+                "");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that <tt>\dimenexpr</tt> with a non-balanced
+     *  parenthesis produces an error.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void testError1() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr ( \\relax",
+                //--- log message ---
+                "Missing number, treated as zero",
+                //--- output channel ---
+                "");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that <tt>\dimenexpr</tt> produces an error for
+     *  division by zero.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void testError3() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 6pt/0\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "Arithmetic overflow",
+                //--- output channel ---
+                "");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that <tt>\dimenexpr</tt> can add two numbers.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test1() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 1pt + 2pt\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "3.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that <tt>\dimenexpr</tt> can add multiply numbers.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test2() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 2*3pt\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "6.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that <tt>\dimenexpr</tt> can subtract two numbers.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test3() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 5pt-2pt\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "3.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that <tt>\dimenexpr</tt> can divide two numbers.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test4() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 6pt/2\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "3.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> * binds more than +.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test10() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 1pt + 2*3pt\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "7.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> * binds more than +.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test11() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 2pt*3 + 1pt\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "7.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> / binds more than +.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test12() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 4pt/2 + 10pt\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "12.0pt \n\n");
+    }
+
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> / binds more than +.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test13() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 10pt + 4pt/2\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "12.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> * binds more than -.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test14() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 1pt - 2pt*3\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "-5.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> * binds more than -.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test15() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 2pt*3 - 1pt\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "5.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> / binds more than -.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test16() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 4pt/2 - 10pt\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "-8.0pt \n\n");
+    }
+
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> / binds more than -.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test17() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 10pt - 4pt/2\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "8.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> parentheses work.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test21() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 2*(1pt + 2pt)\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "6.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> the unary minus is
+     *  treated correctly.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test31() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 2*-3pt\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "-6.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> the unary minus is
+     *  treated correctly &ndash; even if repeated twice.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test32() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr 2*--3pt\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "6.0pt \n\n");
+    }
+
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> the unary minus is
+     *  treated correctly.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test33() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr -2pt + 3pt\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "1.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that in <tt>\dimenexpr</tt> the unary minus is
+     *  treated correctly &ndash; even if repeated twice.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test34() throws Exception {
+
+        runCode(//--- input code ---
+                "\\dimen1=\\dimenexpr --2pt+3pt\\relax"
+                + "\\the\\dimen1 ",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "5.0pt \n\n");
+    }
+
+    /**
+     * <testcase primitive="dimenexpr">
+     *  Test case checking that <tt>\dimenexpr</tt> can be used after
+     *  <tt>\the</tt>.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void testThe0() throws Exception {
+
+        runCode(//--- input code ---
+                "\\the\\dimenexpr 2*3pt\\relax",
+                //--- log message ---
+                "",
+                //--- output channel ---
+                "6.0pt\n\n");
+    }
+
+}
