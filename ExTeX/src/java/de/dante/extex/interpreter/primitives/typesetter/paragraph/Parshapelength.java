dd45614e91d3e280e9212008f5c34f1a0e8a1997
gene <gene>
2005-06-15 21:39:45 +0000

    some more primitives implemented (\parshape*)

---
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/typesetter/paragraph/Parshapelength.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.typesetter.paragraph;
+
+import de.dante.extex.interpreter.Flags;
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.interpreter.exception.InterpreterException;
+import de.dante.extex.interpreter.exception.helping.CantUseInException;
+import de.dante.extex.interpreter.type.AbstractCode;
+import de.dante.extex.interpreter.type.Theable;
+import de.dante.extex.interpreter.type.count.CountConvertible;
+import de.dante.extex.interpreter.type.dimen.DimenConvertible;
+import de.dante.extex.interpreter.type.tokens.Tokens;
+import de.dante.extex.typesetter.Typesetter;
+import de.dante.extex.typesetter.paragraphBuilder.ParagraphShape;
+
+/**
+ * This class provides an implementation for the primitive <code>\relax</code>.
+ *
+ * <doc name="parshapelength">
+ * <h3>The Primitive <tt>\parshapelength</tt></h3>
+ * <p>
+ *  The primitive <tt>\parshapelength</tt> ...
+ * </p>
+ *
+ * <h4>Syntax</h4>
+ *  The formal description of this primitive is the following:
+ *  <pre class="syntax">
+ *    &lang;parshapelength&rang;
+ *        &rarr; <tt>\parshapelength</tt> {@linkplain
+ *        de.dante.extex.interpreter.TokenSource#scanNumber()
+ *        &lang;8-bit&nbsp;number&rang;} </pre>
+ *
+ * <h4>Examples</h4>
+ *  <pre class="TeXSample">
+ *    \dimen2=\parshapelength 3  </pre>
+ *  <pre class="TeXSample">
+ *    \dimen2=\parshapelength -3  </pre>
+ *
+ * </doc>
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class Parshapelength extends AbstractCode
+        implements
+            CountConvertible,
+            DimenConvertible,
+            Theable {
+
+    /**
+     * Creates a new object.
+     *
+     * @param name the name for debugging
+     */
+    public Parshapelength(final String name) {
+
+        super(name);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.count.CountConvertible#convertCount(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public long convertCount(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws InterpreterException {
+
+        return convertDimen(context, source, typesetter);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.count.DimenConvertible#convertDimen(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public long convertDimen(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws InterpreterException {
+
+        int n = (int) source.scanInteger(context, typesetter);
+        ParagraphShape parshape = context.getParshape();
+        return (parshape == null || n < 0 ? 0 : parshape.getLength(n).getValue());
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.Code#execute(
+     *      de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public void execute(final Flags prefix, final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws InterpreterException {
+
+        throw new CantUseInException(printableControlSequence(context),
+                typesetter.getMode().toString());
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.Theable#the(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public Tokens the(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws InterpreterException {
+
+        return new Tokens(context, convertCount(context, source, typesetter));
+    }
+}
