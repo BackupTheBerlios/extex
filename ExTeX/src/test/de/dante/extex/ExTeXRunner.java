1cc5a2e674ebca4563ff702d3fc1e548e2224114
mgn <mgn>
2005-08-05 08:06:46 +0000

    Test: TestRunner

---
+++ ExTeX/src/test/de/dante/extex/ExTeXRunner.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex;
+
+import java.util.Properties;
+
+import junit.framework.TestCase;
+import de.dante.extex.font.FontFactory;
+import de.dante.util.configuration.Configuration;
+import de.dante.util.configuration.ConfigurationException;
+import de.dante.util.configuration.ConfigurationFactory;
+import de.dante.util.resource.ResourceFinder;
+
+/**
+ * ExTeX-TestRunner.
+ *
+ * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
+ * @version $Revision: 1.1 $
+ */
+
+public class ExTeXRunner extends TestCase {
+
+    /**
+     * myextex
+     */
+    protected MyExTeX extex;
+
+    /**
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+
+        extex = new MyExTeX(System.getProperties(), ".extex-test");
+
+    }
+
+    /**
+     * inner ExTeX class.
+     */
+    public class MyExTeX extends ExTeX {
+
+        /**
+         * Creates a new object and initializes the properties from given
+         * properties and possibly from a user's properties in the file
+         * <tt>.extex</tt>.
+         * The user properties are loaded from the users home directory and the
+         * current directory.
+         *
+         * @param theProperties the properties to consider
+         * @param dotFile the name of the local configuration file. In the case
+         *            that this value is <code>null</code> no user properties
+         *            will be considered.
+         *
+         * @throws Exception in case of an error
+         */
+        public MyExTeX(final Properties theProperties, final String dotFile)
+                throws Exception {
+
+            super(theProperties, dotFile);
+            makeConfig();
+        }
+
+        /**
+         * Creates a new object and supplies some properties for those keys which
+         * are not contained in the properties already.
+         * A detailed list of the properties supported can be found in section
+         * <a href="#settings">Settings</a>.
+         *
+         * @param theProperties the properties to start with. This object is
+         *  used and modified. The caller should provide a new instance if this is
+         *  not desirable.
+         *
+         * @throws Exception in case of an error
+         */
+        public MyExTeX(final Properties theProperties) throws Exception {
+
+            super(theProperties);
+            makeConfig();
+        }
+
+        /**
+         * the config
+         */
+        private Configuration config;
+
+        /**
+         * create the config
+         */
+        private void makeConfig() throws ConfigurationException {
+
+            config = new ConfigurationFactory().newInstance("config/extex.xml");
+
+        }
+
+        /**
+         * the finder
+         */
+        private ResourceFinder finder;
+
+        /**
+         * Returns the finder.
+         * @return Returns the finder.
+         * @throws ConfigurationException if an error occurs.
+         */
+        public ResourceFinder getResourceFinder() throws ConfigurationException {
+
+            if (finder == null) {
+                finder = makeResourceFinder(config);
+            }
+            return finder;
+        }
+
+        /**
+         * the font factroy
+         */
+        private FontFactory fontFactory;
+
+        /**
+         * Returns the font factory.
+         * @return Returns the font factory.
+         * @throws ConfigurationException if an error occurs
+         */
+        public FontFactory getFontFactory() throws ConfigurationException {
+
+            if (fontFactory == null) {
+                fontFactory = makeFontFactory(config, getResourceFinder());
+            }
+            return fontFactory;
+        }
+    }
+}
