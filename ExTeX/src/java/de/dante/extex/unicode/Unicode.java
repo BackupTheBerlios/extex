264aa56ae24fc29437cb72995a3bfd9ff3cdf68b
gene <gene>
2005-08-05 07:29:26 +0000

    new

---
+++ ExTeX/src/java/de/dante/extex/unicode/Unicode.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+package de.dante.extex.unicode;
+
+
+/**
+ * Central utility class for Unicode compatibility in <logo>ExTeX</logo>.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public final class Unicode {
+
+    /**
+     * Creates a new object.
+     */
+    private Unicode() {
+
+        super();
+    }
+
+    /**
+     * The constant <tt>OFFSET</tt> contains the offset for shifting code points
+     * to a private Unicode area.
+     */
+    public static final int OFFSET = 0xEE00;
+    //public static final int OFFSET = 0;
+
+}
