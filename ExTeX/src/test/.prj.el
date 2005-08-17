b0af56a7399d19990198b522687a5517cdf1742b
plaicy <plaicy>
2005-08-17 22:03:54 +0000

    - correct pathes

--- ExTeX/src/test/.prj.el
+++ ExTeX/src/test/.prj.el
@@ -1,6 +1,6 @@
 ; author: Sebastian Waschik
 ; created: 2004-07-26
-; RCS-ID: $Id: .prj.el,v 1.2 2004/08/31 19:28:54 plaicy Exp $
+; RCS-ID: $Id: .prj.el,v 1.3 2005/08/17 22:03:54 plaicy Exp $
 (jde-project-file-version "1.0")
 (jde-set-variables
  '(jde-run-application-class "")
@@ -15,11 +15,11 @@
    (quote
     ("./../../target/classes"
     "./../../lib"
-    "./../../lib.develop")))
+    "./../../develop/lib")))
  '(jde-checkstyle-classpath
    (quote
-    ("./../../lib.develop/checkstyle-all-3.4.jar"
-     "./../../lib.develop/checkstyle-optional-3.4.jar")))
+    ("./../../develop/lib/checkstyle-all-3.4.jar"
+     "./../../develop/lib/checkstyle-optional-3.4.jar")))
  '(jde-checkstyle-style "./../../.checkstyle.cfg")
 
 )
