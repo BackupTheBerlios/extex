931e2b17e81b0e6aa8b20ad17eb30c5d9afd042f
gene <gene>
2005-10-06 10:56:18 +0000

    test cases from register.box.EveryhboxTest integrated into register.toks.EveryhboxTest

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/register/toks/EveryhboxTest.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/register/toks/EveryhboxTest.java
@@ -23,7 +23,7 @@ package de.dante.extex.interpreter.primitives.register.toks;
  * This is a test suite for the primitive <tt>\everyhbox</tt>.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class EveryhboxTest extends AbstractToksRegisterTester {
 
@@ -44,6 +44,76 @@ public class EveryhboxTest extends AbstractToksRegisterTester {
     public EveryhboxTest(final String arg) {
 
         super(arg, "everyhbox", "", "");
+    }
+
+    /**
+     * <testcase primitive="everyhbox">
+     *   Test case checking that a hbox containing "abc" in font cmtt12 has the
+     *   width 37.05002pt where "123" is added to the box by an
+     *   <tt>\everyhbox</tt>.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void testErr1() throws Exception {
+
+        runFailureCode(//--- input code ---
+                "\\catcode`{=1 "
+                + "\\catcode`}=2 "
+                + "\\everyhbox123"
+                + "\\end",
+                //--- log message ---
+                "Missing `{' inserted");
+    }
+
+    /**
+     * <testcase primitive="everyhbox">
+     *   Test case checking that a hbox containing "abc" in font cmtt12 has the
+     *   width 37.05002pt where "123" is added to the box by an
+     *   <tt>\everyhbox</tt>.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void testEveryHbox1() throws Exception {
+
+        runCode(//--- input code ---
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\relax"
+                + "\\font\\fnt cmtt12 \\fnt"
+                + "\\everyhbox{123}"
+                + "\\setbox1=\\hbox{abc} "
+                + "\\the\\wd1 "
+                + "\\end",
+                //--- output channel ---
+                "37.05002pt" + TERM); // checked with TeX
+    }
+
+    /**
+     * <testcase primitive="everyhbox">
+     *   Test case checking that a hbox containing "abc" in font cmtt12 has the
+     *   width 55.57503pt where "123" is added to the box by an \everyhbox and
+     *   "..." is prepended by \afterassignment.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void testEveryHboxAfterassignment1() throws Exception {
+
+        runCode(//--- input code ---
+                "\\catcode`{=1"
+                + "\\catcode`}=2"
+                + "\\relax"
+                + "\\font\\fnt cmtt12 \\fnt"
+                + "\\everyhbox{123}"
+                + "\\def\\x{...}"
+                + "\\afterassignment\\x"
+                + "\\setbox1=\\hbox{abc} "
+                + "\\the\\wd1 "
+                + "\\end",
+                //--- output channel ---
+                "55.57503pt" + TERM); // checked wih TeX
     }
 
     //TODO implement the primitive specific test cases
