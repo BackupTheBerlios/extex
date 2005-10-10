2f241eb18ee62c5e632723ca785bd37ee337af15
gene <gene>
2005-10-10 15:50:51 +0000

    some missing methods added

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/parameter/Sfcode.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/parameter/Sfcode.java
@@ -23,10 +23,17 @@ import de.dante.extex.interpreter.Flags;
 import de.dante.extex.interpreter.TokenSource;
 import de.dante.extex.interpreter.context.Context;
 import de.dante.extex.interpreter.exception.InterpreterException;
+import de.dante.extex.interpreter.exception.helping.InvalidCodeException;
 import de.dante.extex.interpreter.type.AbstractAssignment;
+import de.dante.extex.interpreter.type.ExpandableCode;
+import de.dante.extex.interpreter.type.Theable;
 import de.dante.extex.interpreter.type.count.Count;
+import de.dante.extex.interpreter.type.count.CountConvertible;
+import de.dante.extex.interpreter.type.dimen.DimenConvertible;
+import de.dante.extex.interpreter.type.tokens.Tokens;
 import de.dante.extex.typesetter.Typesetter;
 import de.dante.util.UnicodeChar;
+import de.dante.util.exception.GeneralException;
 
 /**
  * This class provides an implementation for the primitive
@@ -51,9 +58,19 @@ import de.dante.util.UnicodeChar;
  * </doc>
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.12 $
+ * @version $Revision: 1.13 $
  */
-public class Sfcode extends AbstractAssignment {
+public class Sfcode extends AbstractAssignment
+        implements
+            ExpandableCode,
+            CountConvertible,
+            DimenConvertible,
+            Theable {
+
+    /**
+     * The field <tt>MAX_SF_CODE</tt> contains the ...
+     */
+    private static final int MAX_SF_CODE = 32767;
 
     /**
      * The constant <tt>serialVersionUID</tt> contains the id for serialization.
@@ -71,7 +88,8 @@ public class Sfcode extends AbstractAssignment {
     }
 
     /**
-     * @see de.dante.extex.interpreter.type.Code#execute(de.dante.extex.interpreter.Flags,
+     * @see de.dante.extex.interpreter.type.Code#execute(
+     *      de.dante.extex.interpreter.Flags,
      *      de.dante.extex.interpreter.context.Context,
      *      de.dante.extex.interpreter.TokenSource,
      *      de.dante.extex.typesetter.Typesetter)
@@ -80,12 +98,67 @@ public class Sfcode extends AbstractAssignment {
             final TokenSource source, final Typesetter typesetter)
             throws InterpreterException {
 
-        UnicodeChar charCode = source.scanCharacterCode(context, getName());
+        UnicodeChar charCode = source.scanCharacterCode(context, typesetter,
+                getName());
         source.getOptionalEquals(context);
-        Count sfCode = new Count(source.scanNumber(context));
+        Count sfCode = new Count(source.scanInteger(context, typesetter));
+
+        if (sfCode.lt(Count.ZERO) || sfCode.getValue() > MAX_SF_CODE) {
+            throw new InvalidCodeException(sfCode.toString(), //
+                    Integer.toString(MAX_SF_CODE));
+        }
 
         context.setSfcode(charCode, sfCode, prefix.isGlobal());
         prefix.clearGlobal();
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.count.CountConvertible#convertCount(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public long convertCount(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws InterpreterException {
+
+        UnicodeChar ucCode = source.scanCharacterCode(context, typesetter,
+                getName());
+        return context.getSfcode(ucCode).getValue();
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.dimen.DimenConvertible#convertDimen(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public long convertDimen(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws InterpreterException {
+
+        return convertCount(context, source, typesetter);
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.ExpandableCode#expand(
+     *      de.dante.extex.interpreter.Flags,
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource,
+     *      de.dante.extex.typesetter.Typesetter)
+     */
+    public void expand(final Flags prefix, final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws InterpreterException {
+
+        source.push(the(context, source, typesetter));
+    }
+
+    /**
+     * @see de.dante.extex.interpreter.type.Theable#the(
+     *      de.dante.extex.interpreter.context.Context,
+     *      de.dante.extex.interpreter.TokenSource, Typesetter)
+     */
+    public Tokens the(final Context context, final TokenSource source,
+            final Typesetter typesetter) throws InterpreterException {
+
+        return new Tokens(context, convertCount(context, source, typesetter));
     }
 
 }
