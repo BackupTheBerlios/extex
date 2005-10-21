fe0d8fcd5c148dc2b0db3ed0032f9735cbd680f0
gene <gene>
2005-10-21 09:31:27 +0000

    empty constructor added for native load

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/dynamic/java/JavaLoad.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/dynamic/java/JavaLoad.java
@@ -92,7 +92,7 @@ import de.dante.extex.typesetter.Typesetter;
  * </doc>
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.16 $
+ * @version $Revision: 1.17 $
  */
 public class JavaLoad extends AbstractCode implements Loader {
 
@@ -100,6 +100,16 @@ public class JavaLoad extends AbstractCode implements Loader {
      * The constant <tt>serialVersionUID</tt> contains the id for serialization.
      */
     private static final long serialVersionUID = 1L;
+
+    /**
+     * Creates a new object.
+     * This method is needed for the nativeload wrapper.
+     *
+     */
+    public JavaLoad() {
+
+        super("");
+    }
 
     /**
      * Creates a new object.
