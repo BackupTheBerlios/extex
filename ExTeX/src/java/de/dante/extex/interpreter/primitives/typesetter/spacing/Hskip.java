6c37ffbfc8ead0513132f37bc833e350fedeffd2
gene <gene>
2005-10-21 09:46:23 +0000

    exception specialized

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/typesetter/spacing/Hskip.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/typesetter/spacing/Hskip.java
@@ -26,7 +26,7 @@ import de.dante.extex.interpreter.exception.InterpreterException;
 import de.dante.extex.interpreter.primitives.typesetter.AbstractHorizontalCode;
 import de.dante.extex.interpreter.type.glue.Glue;
 import de.dante.extex.typesetter.Typesetter;
-import de.dante.util.exception.GeneralException;
+import de.dante.extex.typesetter.exception.TypesetterException;
 
 /**
  * This class provides an implementation for the primitive <code>\hskip</code>.
@@ -51,7 +51,7 @@ import de.dante.util.exception.GeneralException;
  *
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  */
 public class Hskip extends AbstractHorizontalCode {
 
@@ -85,7 +85,7 @@ public class Hskip extends AbstractHorizontalCode {
         Glue g = new Glue(source, context, typesetter);
         try {
             typesetter.addGlue(g);
-        } catch (GeneralException e) {
+        } catch (TypesetterException e) {
             throw new InterpreterException(e);
         }
     }
