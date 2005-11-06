38ae242bb62b72f49cb37f5c04ccd456bb3abe6c
gene <gene>
2005-11-06 17:20:30 +0000

    bugfix: using getToken in scanNumber()

--- ExTeX/src/java/de/dante/extex/interpreter/max/Moritz.java
+++ ExTeX/src/java/de/dante/extex/interpreter/max/Moritz.java
@@ -103,7 +103,7 @@ import de.dante.util.observer.NotObservableException;
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.83 $
+ * @version $Revision: 1.84 $
  */
 public class Moritz extends Max
         implements
@@ -217,7 +217,8 @@ public class Moritz extends Max
     }
 
     /**
-     * @see de.dante.extex.interpreter.TokenSource#closeAllStreams(Context)
+     * @see de.dante.extex.interpreter.TokenSource#closeAllStreams(
+     *      de.dante.extex.interpreter.context.Context)
      */
     public void closeAllStreams(final Context context)
             throws InterpreterException {
@@ -228,7 +229,8 @@ public class Moritz extends Max
     }
 
     /**
-     * @see de.dante.extex.interpreter.TokenSource#closeNextFileStream(Context)
+     * @see de.dante.extex.interpreter.TokenSource#closeNextFileStream(
+     *      de.dante.extex.interpreter.context.Context)
      */
     public void closeNextFileStream(final Context context)
             throws InterpreterException {
@@ -1159,8 +1161,8 @@ public class Moritz extends Max
                     case '9':
                         n = c - '0';
 
-                        for (t = scanToken(context); t instanceof OtherToken
-                                && t.getChar().isDigit(); t = scanToken(context)) {
+                        for (t = getToken(context); t instanceof OtherToken
+                                && t.getChar().isDigit(); t = getToken(context)) {
                             n = n * 10 + t.getChar().getCodePoint() - '0';
                         }
 
