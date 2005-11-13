b63def4f8099776ee53bf1974416688faa81e7b3
gene <gene>
2005-11-13 21:54:08 +0000

    new

---
+++ ExTeX/src/java/de/dante/extex/interpreter/exception/InterpreterExtensionInvalidException.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.exception;
+
+/**
+ * This exception signals that an invalid exception has been requested.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class InterpreterExtensionInvalidException extends InterpreterException {
+
+    /**
+     * The field <tt>serialVersionUID</tt> contains the ...
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Creates a new object.
+     *
+     * @param message the name of the class
+     */
+    public InterpreterExtensionInvalidException(final String message) {
+
+        super(message);
+    }
+
+    /**
+     * Creates a new object.
+     *
+     * @param message the name of the class
+     * @param cause the root of all evil
+     */
+    public InterpreterExtensionInvalidException(final String message,
+            final Throwable cause) {
+
+        super(message, cause);
+    }
+
+}
