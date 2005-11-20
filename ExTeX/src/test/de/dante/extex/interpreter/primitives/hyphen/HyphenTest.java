6dc99908146d277e1317e8f10b9db88f6bf9987c
gene <gene>
2005-11-20 17:59:49 +0000

    new

---
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/hyphen/HyphenTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.hyphen;
+
+import de.dante.test.NoFlagsPrimitiveTester;
+
+/**
+ * This is a test suite for the primitive <tt>\-</tt>.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class HyphenTest extends NoFlagsPrimitiveTester {
+
+    /**
+     * Creates a new object.
+     *
+     * @param arg the name
+     */
+    public HyphenTest(final String arg) {
+
+        super(arg, "-", "");
+    }
+
+    /**
+     * <testcase primitive="\-">
+     *  Test case checking that <tt>\-</tt> ...
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void testDisc1() throws Exception {
+
+        assertSuccess(//--- input code ---
+                "\\font\\f cmr10 \\f "
+                + "\\hsize=123pt "
+                + "abc\\-def"
+                + "\\end ",
+                //--- output channel ---
+                "abc--def" + TERM);
+    }
+
+}
