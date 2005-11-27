7ce9d8d441fce12ab2d364a34fc680c8befb17c9
gene <gene>
2005-11-27 08:50:15 +0000

    new test cases

---
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/register/count/PdfpkresolutionTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.register.count;
+
+
+/**
+ * This is a test suite for the primitive <tt>\pdfpkresolution</tt>.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class PdfpkresolutionTest extends AbstractCountRegisterTester {
+
+    /**
+     * Command line interface.
+     * @param args the arguments
+     */
+    public static void main(final String[] args) {
+
+        junit.textui.TestRunner.run(PdfpkresolutionTest.class);
+    }
+
+    /**
+     * Getter for the configuration name.
+     *
+     * @return the name of the configuration
+     */
+    protected String getConfig() {
+
+        return "pdftex.xml";
+    }
+
+    /**
+     * Creates a new object.
+     *
+     * @param arg the name
+     */
+    public PdfpkresolutionTest(final String arg) {
+
+        super(arg, "pdfpkresolution", "", "0");
+    }
+
+    //TODO implement the primitive specific test cases
+
+}
