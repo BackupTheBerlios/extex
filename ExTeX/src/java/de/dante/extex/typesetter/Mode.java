a180e5ed0075fc6e0d4905abfe1ccd6fff897761
gene <gene>
2005-12-04 19:26:14 +0000

    bugfix: VERTICAL should be a vertical mode

--- ExTeX/src/java/de/dante/extex/typesetter/Mode.java
+++ ExTeX/src/java/de/dante/extex/typesetter/Mode.java
@@ -26,7 +26,7 @@ import de.dante.util.framework.i18n.LocalizerFactory;
  * This class provides type-safe constants for the modes of a typesetter.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.9 $
+ * @version $Revision: 1.10 $
  */
 public abstract class Mode {
 
@@ -34,7 +34,7 @@ public abstract class Mode {
      * This inner class represents vertical modes.
      *
      * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
-     * @version $Revision: 1.9 $
+     * @version $Revision: 1.10 $
      */
     private static class HorizontalMode extends Mode {
 
@@ -63,7 +63,7 @@ public abstract class Mode {
      * This inner class represents math modes.
      *
      * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
-     * @version $Revision: 1.9 $
+     * @version $Revision: 1.10 $
      */
     private static class MathMode extends Mode {
 
@@ -92,7 +92,7 @@ public abstract class Mode {
      * This inner class represents vertical modes.
      *
      * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
-     * @version $Revision: 1.9 $
+     * @version $Revision: 1.10 $
      */
     private static class VerticalMode extends Mode {
 
@@ -153,7 +153,7 @@ public abstract class Mode {
      * The constant <tt>VERTICAL</tt> contains the vertical mode of the
      * typesetter.
      */
-    public static final Mode VERTICAL = new HorizontalMode("Mode.VerticalMode");
+    public static final Mode VERTICAL = new VerticalMode("Mode.VerticalMode");
 
     /**
      * The field <tt>localizer</tt> contains the localizer.
