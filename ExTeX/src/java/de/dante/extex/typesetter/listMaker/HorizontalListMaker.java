cb70376e556f7ab066c10b0b8915a723a6ebc5f1
gene <gene>
2006-02-03 15:22:40 +0000

    minor improvement

--- ExTeX/src/java/de/dante/extex/typesetter/listMaker/HorizontalListMaker.java
+++ ExTeX/src/java/de/dante/extex/typesetter/listMaker/HorizontalListMaker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -59,7 +59,7 @@ import de.dante.util.framework.configuration.exception.ConfigurationException;
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.26 $
+ * @version $Revision: 1.27 $
  */
 public class HorizontalListMaker extends AbstractListMaker {
 
@@ -283,15 +283,19 @@ public class HorizontalListMaker extends AbstractListMaker {
                 }
             }
         }
-        CharNode c = getManager().getCharNodeFactory().newInstance(tc, symbol);
-        nodes.add(c);
+        CharNode charNode = getManager().getCharNodeFactory().newInstance(tc,
+                symbol);
+        if (charNode != null) {
 
-        int f = c.getSpaceFactor();
+            nodes.add(charNode);
 
-        if (f != 0) {
-            spaceFactor = (spaceFactor < DEFAULT_SPACEFACTOR
-                    && f > DEFAULT_SPACEFACTOR //
-            ? DEFAULT_SPACEFACTOR : f);
+            int f = charNode.getSpaceFactor();
+
+            if (f != 0) {
+                spaceFactor = (spaceFactor < DEFAULT_SPACEFACTOR
+                        && f > DEFAULT_SPACEFACTOR //
+                ? DEFAULT_SPACEFACTOR : f);
+            }
         }
     }
 
