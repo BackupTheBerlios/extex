e0f026a20b6328edf6e851569ed69fae8f80ebb0
gene <gene>
2006-02-27 10:00:58 +0000

    new

---
+++ ExTeX/src/java/de/dante/extex/language/word/impl/TeXWords.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (C) 2006 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.language.word.impl;
+
+import java.util.List;
+
+import de.dante.extex.language.Language;
+import de.dante.extex.language.hyphenation.exception.HyphenationException;
+import de.dante.extex.language.word.WordTokenizer;
+import de.dante.extex.typesetter.type.Node;
+import de.dante.extex.typesetter.type.NodeList;
+import de.dante.extex.typesetter.type.node.CharNode;
+import de.dante.extex.typesetter.type.node.DiscretionaryNode;
+import de.dante.extex.typesetter.type.node.ExplicitKernNode;
+import de.dante.extex.typesetter.type.node.KernNode;
+import de.dante.extex.typesetter.type.node.LigatureNode;
+import de.dante.extex.typesetter.type.node.WhatsItNode;
+
+/**
+ * This class tokenizes a list of nodes according to the rules of
+ * <logo>TeX</logo>.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class TeXWords implements WordTokenizer {
+
+    /**
+     * Creates a new object.
+     *
+     */
+    public TeXWords() {
+
+        super();
+    }
+
+    /**
+     * TODO gene: missing JavaDoc
+     *
+     * @param node the character node to add to the word
+     * @param word the container to add the node to
+     */
+    private void addWord(final CharNode node, final List word) {
+
+        if (node instanceof LigatureNode) {
+            LigatureNode ln = (LigatureNode) node;
+            CharNode[] chars = ln.getChars();
+            for (int j = 0; j < chars.length; j++) {
+                word.add(chars[j].getCharacter());
+            }
+
+        } else {
+            word.add(((CharNode) node).getCharacter());
+        }
+    }
+
+    /**
+     * TODO gene: missing JavaDoc
+     *
+     * @param nodes the nodes to process
+     * @param word the word with hyphenation marks
+     * @param start the start index
+     * @param lang the language in effect
+     *
+     * @return the index of the first node past the ones processed
+     *
+     * @throws HyphenationException in case of an error
+     */
+    private int collectWord(final NodeList nodes, final List word,
+            final int start, final Language lang) throws HyphenationException {
+
+        int i = start;
+        int size = nodes.size();
+
+        Node n;
+        CharNode cn;
+
+        for (; i < size; i++) {
+            n = nodes.get(i);
+            if (n instanceof CharNode) {
+                cn = (CharNode) n;
+
+                if (cn.getTypesettingContext().getLanguage() != lang) {
+                    return i;
+                }
+
+                addWord(cn, word);
+
+            } else if (n instanceof WhatsItNode) {
+                // ignored
+            } else if (n instanceof DiscretionaryNode) {
+                i = collectWord(nodes, word, i + 1, lang);
+                return findWord(nodes, i, word);
+
+            } else if (n instanceof KernNode) {
+                if (n instanceof ExplicitKernNode) {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        return i;
+    }
+
+    /**
+     * @see de.dante.extex.language.word.WordTokenizer#findWord(
+     *      de.dante.extex.typesetter.type.NodeList, int, java.util.List)
+     */
+    public int findWord(final NodeList nodes, final int start, final List word)
+            throws HyphenationException {
+
+        int i = start;
+        int size = nodes.size();
+        word.clear();
+        Node n;
+
+        if (i >= size) {
+            return i;
+        }
+
+        do {
+            if (i >= size) {
+                return i;
+            }
+            n = nodes.get(i++);
+        } while (!(n instanceof CharNode));
+
+        addWord((CharNode) n, word);
+
+        return collectWord(nodes, word, i, ((CharNode) n)
+                .getTypesettingContext().getLanguage());
+    }
+
+}
