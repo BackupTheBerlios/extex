374c385a636ab8c8c77e5682820784cb87707256
gene <gene>
2006-02-27 10:06:50 +0000

    EJECT_PENALTY and INF_PENALTY added

--- ExTeX/src/java/de/dante/extex/typesetter/Badness.java
+++ ExTeX/src/java/de/dante/extex/typesetter/Badness.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2005-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -16,16 +16,22 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
-package de.dante.extex.typesetter;
 
+package de.dante.extex.typesetter;
 
 /**
  * This class provides some static methods to deal with badness values.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public final class Badness {
+
+    /**
+     * The constant <tt>EJECT_PENALTY</tt> contains the penalty which forces
+     * a line break. This is an equivalent to -&infin;.
+     */
+    public static final int EJECT_PENALTY = -10000;
 
     /**
      * The constant <tt>INF_BAD</tt> contains the value for infinite badness.
@@ -36,42 +42,94 @@ public final class Badness {
     public static final int INF_BAD = 10000;
 
     /**
-     * Creates a new object.
+     * The constant <tt>INF_BAD</tt> contains the value for infinite penalty.
+     * This is an equivalent to &infin;.
      */
-    private Badness() {
-    }
-
+    public static final int INF_PENALTY = 10000;
 
     /**
      * Compute the badness.
      *
-     * @param t total given t >= 0
-     * @param s sum
+     * <i>
+     * <p>
+     * 108. The next subroutine is used to compute the badness of
+     * glue, when a total t is supposed to be made from amounts that
+     * sum to s. According to The <logo>TeX</logo>book, the badness of this
+     * situation is 100(t/s)<sup>3</sup>; however, badness is simply a
+     * heuristic, so we need not squeeze out the last drop of accuracy when
+     * computing it. All we really want is an approximation that has
+     * similar properties.
+     * </p>
+     * <p>
+     * The actual method used to compute the badness is easier to read
+     * from the program than to describe in words. It produces an
+     * integer value that is a reasonably close approximation to
+     * 100(t/s)<sup>3</sup>, and all implementations of <logo>TeX</logo> should
+     * use precisely this method. Any badness of 2<sup>13</sup> or more is
+     * treated as infinitely bad, and represented by 10000.
+     * </p>
+     * <p>
+     * It is not difficult to prove that
+     * </p>
+     * <p>
+     * badness(t+1,s) &ge; badness(t,s) &ge; badness(t,s+1).
+     * </p>
+     * <p>
+     * The badness function defined here is capable of computing at
+     * most 1095 distinct values, but that is plenty.
+     * </p>
+     * <pre>
+     * define inf_bad=10000 {infinitely bad value}
+     * </pre>
+     * <pre>
+     *   function badness(t,s:scaled): halfword; {compute badness, given t &ge; 0}
+     *    var r: integer; {approximation to &alpha;t/s, where &alpha;<sup>3</sup> &asymp; 100&sdot;2<sup>18</sup>}
+     *  begin if t=0 then badness &larr; 0
+     *    else if s &le; 0 then badness &larr; inf_bad
+     *     else begin if t &le; 7230584 then r &larr; (t * 297) div s {297<sup>3</sup> = 99.94 × 2<sup>18</sup>}
+     *      else if s &ge; 1663497 then r &larr; t div (s div 297)
+     *       else r &larr; t;
+     *     if r&gt;1290 then badness &larr; inf_bad {1290<sup>3</sup>&lt;2<sup>31</sup>&lt;1291<sup>3</sup>}
+     *      else badness &larr; (r*r*r+'400000) div '1000000;
+     *     end ; {that was r<sup>3</sup>/2<sup>18</sup>, rounded to the nearest integer}
+     *  end ;
+     * </pre>
+     * </i>
+     *
+     * @param total total given total >= 0
+     * @param sum sum
      *
      * @return the computed badness
      *
      * @see "TTP [108]"
      */
-    public static int badness(final long t, final long s) {
+    public static int badness(final long total, final long sum) {
 
-        if (t == 0) {
+        if (total <= 0) {
             return 0;
-        } else if (s <= 0) {
-            return INF_BAD;
+        } else if (sum <= 0) {
+            return INF_PENALTY;
         }
 
-        long r; // approximation to a t/s, where a^3 approx 100 * 2^18}
+        long ts; // approximation to a t/s, where a^3 approx 100 * 2^18}
 
-        if (t <= 7230584) {
-            r = (t * 297) / s; // 297^3=99.94 × 2^18
-        } else if (s >= 1663497) {
-            r = t / (s / 297);
+        if (total <= 7230584) {
+            ts = (total * 297) / sum; // 297^3=99.94 × 2^18
+        } else if (sum >= 1663497) {
+            ts = total / (sum / 297);
         } else {
-            r = t;
+            ts = total;
         }
-        return (r > 1290 ? INF_BAD //1290^3<2^31<129^3
-                : (int) ((r * r * r + 0x20000) / 0x40000));
+        return (ts > 1290 ? INF_PENALTY //1290^3<2^31<129^3
+                : (int) ((ts * ts * ts + 0x20000) / 0x40000));
         // that was r^3/2^18, rounded to the nearest integer
+    }
+
+    /**
+     * Creates a new object.
+     */
+    private Badness() {
+
     }
 
 }
