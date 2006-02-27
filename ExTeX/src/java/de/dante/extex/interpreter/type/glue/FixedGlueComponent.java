97c2abf0ae3a58d9acba9b2e8c4aaa78d37a8400
gene <gene>
2006-02-27 10:29:46 +0000

    comparison operators added

--- ExTeX/src/java/de/dante/extex/interpreter/type/glue/FixedGlueComponent.java
+++ ExTeX/src/java/de/dante/extex/interpreter/type/glue/FixedGlueComponent.java
@@ -29,16 +29,35 @@ import de.dante.util.exception.GeneralException;
  * which do not modify the value.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.11 $
+ * @version $Revision: 1.12 $
  */
 public interface FixedGlueComponent {
 
     /**
-     * Getter for the value in scaled points (sp).
+     * Create a copy of this instance with the same order and value.
      *
-     * @return the value in internal units of scaled points (sp)
+     * @return a new copy of this instance
      */
-    long getValue();
+    GlueComponent copy();
+
+    /**
+     * Compares the current instance with another GlueComponent for equality.
+     *
+     * @param d the other GlueComponent to compare to. If this parameter is
+     * <code>null</code> then the comparison fails.
+     *
+     * @return <code>true</code> iff <i>|this| == |d| and ord(this) == ord(d)</i>
+     */
+    boolean eq(final FixedGlueComponent d);
+
+    /**
+     * Compares the current instance with another GlueComponent.
+     *
+     * @param d the other GlueComponent to compare to
+     *
+     * @return <code>true</code> iff this is greater or equal to d
+     */
+    boolean ge(final FixedGlueComponent d);
 
     /**
      * Getter for order.
@@ -48,11 +67,50 @@ public interface FixedGlueComponent {
     int getOrder();
 
     /**
-     * Create a copy of this instance with the same order and value.
+     * Getter for the value in scaled points (sp).
      *
-     * @return a new copy of this instance
+     * @return the value in internal units of scaled points (sp)
      */
-    GlueComponent copy();
+    long getValue();
+
+    /**
+     * Compares the current instance with another GlueComponent.
+     *
+     * @param d the other GlueComponent to compare to
+     *
+     * @return <code>true</code> iff <i>ord(this) == ord(d) && |this| &gt; |d|</i>
+     * or <i>ord(this) &gt; ord(d)</i>
+     */
+    boolean gt(final FixedGlueComponent d);
+
+    /**
+     * Compares the current instance with another GlueComponent.
+     *
+     * @param d the other GlueComponent to compare to
+     *
+     * @return <code>true</code> iff this is less or equal to d
+     */
+    boolean le(final FixedGlueComponent d);
+
+    /**
+     * Compares the current instance with another GlueComponent.
+     *
+     * @param d the other GlueComponent to compare to
+     *
+     * @return <code>true</code> iff <i>ord(this) == ord(d) && |this| &lt; |d|</i>
+     * or <i>ord(this) &lt; ord(d)</i>
+     */
+    boolean lt(final FixedGlueComponent d);
+
+    /**
+     * Compares the current instance with another GlueComponent for equality.
+     *
+     * @param d the other GlueComponent to compare to. If this parameter is
+     * <code>null</code> then the comparison fails.
+     *
+     * @return <code>false</code> iff <i>|this| == |d| and ord(this) == ord(d)</i>
+     */
+    boolean ne(final FixedGlueComponent d);
 
     /**
      * Determine the printable representation of the object.
@@ -93,8 +151,7 @@ public interface FixedGlueComponent {
      * @see #toString()
      * @see #toString(StringBuffer)
      */
-    Tokens toToks(final TokenFactory factory)
-            throws GeneralException;
+    Tokens toToks(TokenFactory factory) throws GeneralException;
 
     /**
      * Determine the printable representation of the object and return it as a
@@ -106,6 +163,8 @@ public interface FixedGlueComponent {
      *
      * @param toks the tokens to append to
      * @param factory the token factory to get the required tokens from
+     * @param c1 the first character for the order 0 value
+     * @param c2 the second character for the order 0 value
      *
      * @throws GeneralException in case of an error
      *
@@ -114,6 +173,7 @@ public interface FixedGlueComponent {
      * @see #toString()
      * @see #toString(StringBuffer)
      */
-    void toToks(final Tokens toks, final TokenFactory factory, char c1, char c2)
+    void toToks(Tokens toks, TokenFactory factory, char c1, char c2)
             throws GeneralException;
+
 }
