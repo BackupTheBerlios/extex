b58465b7e860b42216341ba73a243d6622487845
gene <gene>
2006-02-27 10:19:53 +0000

    documentation improved

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/interaction/AllTests.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/interaction/AllTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2005-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -16,20 +16,25 @@
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
+
 package de.dante.extex.interpreter.primitives.interaction;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-
 /**
- * TODO gene: missing JavaDoc.
+ * This is a test suite containing the tests in the interaction package.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
-public class AllTests {
+public final class AllTests {
 
+    /**
+     * Create the test suite.
+     *
+     * @return the suite
+     */
     public static Test suite() {
 
         TestSuite suite = new TestSuite(
