0b45c02034a65f1bacaf231b3a21a1eb225a7a42
gene <gene>
2006-02-28 09:00:24 +0000

    range check added

--- ExTeX/src/java/de/dante/util/UnicodeChar.java
+++ ExTeX/src/java/de/dante/util/UnicodeChar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2003-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -35,7 +35,7 @@ import com.ibm.icu.text.UTF16;
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.24 $
+ * @version $Revision: 1.25 $
  */
 public class UnicodeChar implements Serializable {
 
@@ -134,6 +134,10 @@ public class UnicodeChar implements Serializable {
     public UnicodeChar(final int codePoint) {
 
         super();
+        if (codePoint < UCharacter.MIN_VALUE
+                || codePoint > UCharacter.MAX_VALUE) {
+            throw new IllegalArgumentException("Codepoint out of bounds");
+        }
         this.code = codePoint;
     }
 
