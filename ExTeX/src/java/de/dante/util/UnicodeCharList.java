1c4d4c5ebef984b3dbabe7cf2a301d2ffcceba4f
gene <gene>
2006-03-08 10:39:48 +0000

    clear() added as delegate supporting equals() mapping to equality of content

--- ExTeX/src/java/de/dante/util/UnicodeCharList.java
+++ ExTeX/src/java/de/dante/util/UnicodeCharList.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -21,20 +21,22 @@ package de.dante.util;
 import java.util.ArrayList;
 
 /**
- * This class implements a <code>ArrayList</code> for <code>UnicodeChar</code>s.
+ * This class provides a list of <code>UnicodeChar</code>s.
  *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class UnicodeCharList {
 
     /**
-     * the internal <code>ArrayList</code>
+     * The field <tt>list</tt> contains the list.
      */
     private ArrayList list;
 
     /**
      * Create a new object.
+     * This list is initially empty.
      */
     public UnicodeCharList() {
 
@@ -44,7 +46,9 @@ public class UnicodeCharList {
 
     /**
      * Create a new object.
-     * @param   initsize    initsize for the <code>ArrayList</code>
+     * This list is initially empty.
+     *
+     * @param initsize initsize for the <code>ArrayList</code>
      */
     public UnicodeCharList(final int initsize) {
 
@@ -53,8 +57,9 @@ public class UnicodeCharList {
     }
 
     /**
-     * Add a new Element.
-     * @param uc    the <code>UnicodeChar</code> to add
+     * Add a new element.
+     *
+     * @param uc the <code>UnicodeChar</code> to add
      */
     public void add(final UnicodeChar uc) {
 
@@ -62,18 +67,67 @@ public class UnicodeCharList {
     }
 
     /**
-     * Return the <code>UnicodeChar</code> at index idx.
-     * @param idx   the index
-     * @return  the <code>UnicodeChar</code> at index idx.
+     * @see java.util.ArrayList#clear()
      */
-    public UnicodeChar get(final int idx) {
+    public void clear() {
 
-        return (UnicodeChar) list.get(idx);
+        this.list.clear();
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals(final Object obj) {
+
+        if (!(obj instanceof UnicodeCharList)) {
+            return false;
+        }
+
+        UnicodeCharList ucl = (UnicodeCharList) obj;
+        int size = list.size();
+        if (size != ucl.list.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (!list.get(i).equals(ucl.list.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Return the <code>UnicodeChar</code> at index.
+     *
+     * @param index the index
+     *
+     * @return  the <code>UnicodeChar</code> at index.
+     */
+    public UnicodeChar get(final int index) {
+
+        return (UnicodeChar) list.get(index);
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    public int hashCode() {
+
+        int size = list.size();
+        int hash = 0;
+
+        for (int i = 0; i < size; i++) {
+            hash += list.get(i).hashCode();
+        }
+        return hash;
     }
 
     /**
      * Return the size of the list.
-     * @return  the size of the list
+     *
+     * @return the size of the list
      */
     public int size() {
 
@@ -82,6 +136,7 @@ public class UnicodeCharList {
 
     /**
      * Return the <code>UnicodeCharList</code> as <code>String</code>.
+     *
      * @return the string of the list
      */
     public String toString() {
