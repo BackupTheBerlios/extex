54a3c52f19d94549eb4f6e37feb1aa1357f649c6
gene <gene>
2006-03-18 08:16:52 +0000

    preBreak eliminated

--- ExTeX/src/java/de/dante/extex/typesetter/paragraphBuilder/texImpl/ActiveNode.java
+++ ExTeX/src/java/de/dante/extex/typesetter/paragraphBuilder/texImpl/ActiveNode.java
@@ -20,103 +20,99 @@
 package de.dante.extex.typesetter.paragraphBuilder.texImpl;
 
 /**
- * TODO gene: missing JavaDoc.
+ * 819.
  *
- *  819.
-
- An active node for a given breakpoint contains six fields:
-
- link points to the next node in the list of active nodes; the
- last active node has link=last_active.
-
- break_node points to the passive node associated with this
- breakpoint.
-
- line_number is the number of the line that follows this
- breakpoint.
-
- fitness is the fitness classification of the line ending at this
- breakpoint.
-
- type is either hyphenated or unhyphenated, depending on
- whether this breakpoint is a disc_node.
-
- total_demerits is the minimum possible sum of demerits over all
- lines leading from the beginning of the paragraph to this breakpoint.
-
-
- The value of link(active) points to the first active node on a linked
- list
- of all currently active nodes. This list is in order by line_number,
- except that nodes with line_number > easy_line may be in any order
- relative
- to each other.
-
- define active_node_size=3  {number of words in active nodes}
-
- define fitness ::= subtype  {very_loose_fit .. tight_fit on
- final line for this break}
-
- define break_node ::= rlink  {pointer to the corresponding passive
- node}
-
- define line_number ::= llink  {line that begins at this breakpoint}
-
- define total_demerits(#) ::= mem[#+2].int  {the quantity that
- TeX minimizes}
-
- define unhyphenated=0  {the type of a normal active break node}
-
- define hyphenated=1  {the type of an active node that breaks at a
- disc_node}
-
- define last_active ::= active  {the active list ends where it begins}
-
+ * An active node for a given breakpoint contains six fields:
+ * <ul>
+ * <li>
+ *  <i>link</i> points to the next node in the list of active nodes; the
+ *  last active node has <i>link=last_active</i>.
+ * </li>
+ * <li>
+ *  <i>break_node</i> points to the passive node associated with this breakpoint.
+ * </li>
+ * <li>
+ *  <i>line_number</i> is the number of the line that follows this breakpoint.
+ * </li>
+ * <li>
+ *  <i>fitness</i> is the fitness classification of the line ending at this
+ *  breakpoint.
+ * </li>
+ * <li>
+ *  <i>type</i> is either hyphenated or unhyphenated, depending on
+ *  whether this breakpoint is a disc_node.
+ * </li>
+ * <li>
+ *  <i>total_demerits</i> is the minimum possible sum of demerits over all
+ *  lines leading from the beginning of the paragraph to this breakpoint.
+ * </li>
+ * <ul>
+ * <p>
+ *  The value of <i>link(active)</i> points to the first active node on a
+ *  linked list of all currently active nodes. This list is in order by
+ *  <i>line_number<i>, except that nodes with <i>line_number &gt; easy_line</i>
+ *  may be in any order relative to each other.
+ * </p>
+ *
+ * <pre>
+ * <b>define</b> active_node_size=3  {number of words in active nodes}
+ *
+ * <b>define</b> fitness ::= subtype  {very_loose_fit .. tight_fit on final line for this break}
+ *
+ * <b>define</b> break_node ::= rlink  {pointer to the corresponding passive node}
+ *
+ * <b>define</b> line_number ::= llink  {line that begins at this breakpoint}
+ *
+ * <b>define</b> total_demerits(#) ::= mem[#+2].int  {the quantity that TeX minimizes}
+ *
+ * <b>define</b> unhyphenated=0  {the type of a normal active break node}
+ *
+ * <b>define</b> hyphenated=1  {the type of an active node that breaks at a disc_node}
+ *
+ * <b>define</b> last_active ::= active  {the active list ends where it begins}
+ * </pre>
  *
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class ActiveNode {
 
     /**
-     * The field <tt>breakNode</tt> contains the ...
+     * The field <tt>breakNode</tt> contains the associated passive node.
      */
     private PassiveNode breakNode;
 
     /**
-     * The field <tt>demerits</tt> contains the ...
+     * The field <tt>demerits</tt> contains the demerits for this break
+     * point.
      */
     private long demerits;
 
     /**
-     * The field <tt>fitness</tt> contains the ...
+     * The field <tt>fitness</tt> contains the fitness classification.
      */
     private Fitness fitness;
 
     /**
-     * The field <tt>hyphenated</tt> contains the ...
+     * The field <tt>hyphenated</tt> contains the indicator for hyphenation at
+     * this break point.
      */
     private boolean hyphenated;
 
     /**
-     * The field <tt>lineNumber</tt> contains the ...
+     * The field <tt>lineNumber</tt> contains the sequence number of the line.
      */
     private int lineNumber;
 
     /**
-     * The field <tt>preBreak</tt> contains the ...
-     */
-    private int preBreak;
-
-    /**
      * Creates a new object.
      *
-     * @param fitness ...
-     * @param hyphenated ...
-     * @param demerits ...
-     * @param lineNumber ...
-     * @param breakNode ...
+     * @param fitness the fitness classification
+     * @param hyphenated the indicator for hyphenation at this break point
+     * @param demerits the demerits for this break point
+     * @param lineNumber the sequence number of the line
+     * @param breakNode the index of the break
      */
     public ActiveNode(final Fitness fitness, final boolean hyphenated,
             final long demerits, final int lineNumber,
@@ -158,16 +154,6 @@ public class ActiveNode {
     protected int getLineNumber() {
 
         return this.lineNumber;
-    }
-
-    /**
-     * Getter for preBreak.
-     *
-     * @return the preBreak
-     */
-    protected int getPreBreak() {
-
-        return this.preBreak;
     }
 
     /**
