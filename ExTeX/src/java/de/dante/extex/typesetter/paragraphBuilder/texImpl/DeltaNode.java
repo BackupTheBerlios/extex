8a8448c81305c9430d40e2c3aa79a217411db912
gene <gene>
2006-03-18 08:17:47 +0000

    reformatted

--- ExTeX/src/java/de/dante/extex/typesetter/paragraphBuilder/texImpl/DeltaNode.java
+++ ExTeX/src/java/de/dante/extex/typesetter/paragraphBuilder/texImpl/DeltaNode.java
@@ -21,31 +21,29 @@ package de.dante.extex.typesetter.paragraphBuilder.texImpl;
 
 import de.dante.extex.interpreter.type.glue.WideGlue;
 
-/*  822.
-
- The active list also contains "delta" nodes that help the
- algorithm compute the badness of individual lines. Such nodes
- appear only between two active nodes, and they have
- type=delta_node. If p and r are active nodes and if q is a delta
- node between them, so that link(p)=q and link(q)=r, then q tells
- the space difference between lines in the horizontal list that
- start after breakpoint p and lines that start after breakpoint r.
- In other words, if we know the length of the line that starts
- after p and ends at our current position, then the corresponding
- length of the line that starts after r is obtained by adding the
- amounts in node q. A delta node contains six scaled numbers, since
- it must record the net change in glue stretchability with respect
- to all orders of infinity. The natural width difference appears in
- mem[q+1].sc; the stretch differences in units of pt, fil, fill,
- and filll appear in mem[q+2 .. q+5].sc; and the shrink difference
- appears in mem[q+6].sc. The subtype field of a delta node is not
- used.
- */
-/**
- * TODO gene: missing JavaDoc.
+/** 822.
+ *
+ * The active list also contains "delta" nodes that help the
+ * algorithm compute the badness of individual lines. Such nodes
+ * appear only between two active nodes, and they have
+ * <i>type=delta_node</i>. If <i>p</i> and <i>r</i> are active nodes and if
+ * <i>q</i> is a delta node between them, so that <i>link(p)=q</i> and
+ * <i>link(q)=r</i>, then <i>q</i> tells the space difference between lines
+ * in the horizontal list that
+ * start after breakpoint <i>p</i> and lines that start after breakpoint <i>r</i>.
+ * In other words, if we know the length of the line that starts
+ * after <i>p</i> and ends at our current position, then the corresponding
+ * length of the line that starts after <i>r</i> is obtained by adding the
+ * amounts in node <i>q</i>. A delta node contains six scaled numbers, since
+ * it must record the net change in glue stretchability with respect
+ * to all orders of infinity. The natural width difference appears in
+ * <i>mem[q+1].sc</i>; the stretch differences in units of pt, fil, fill,
+ * and filll appear in <i>mem[q+2 .. q+5].sc</i>; and the shrink difference
+ * appears in <i>mem[q+6].sc</i>. The subtype field of a delta node is not
+ * used.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class DeltaNode extends WideGlue {
 
@@ -59,4 +57,5 @@ public class DeltaNode extends WideGlue {
         super();
         set(glue);
     }
+
 }
