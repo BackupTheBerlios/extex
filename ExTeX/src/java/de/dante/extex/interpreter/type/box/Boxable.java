ff354bd0d7c6c7c399861f353fdc3569fe99b665
gene <gene>
2006-04-09 14:11:56 +0000

    documentation improved

--- ExTeX/src/java/de/dante/extex/interpreter/type/box/Boxable.java
+++ ExTeX/src/java/de/dante/extex/interpreter/type/box/Boxable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -25,10 +25,11 @@ import de.dante.extex.interpreter.exception.InterpreterException;
 import de.dante.extex.typesetter.Typesetter;
 
 /**
- * This interface describes the capability to deliver a Box.
+ * This interface describes the capability to deliver a Box. Usually this box
+ * is gathered from the input stream and build by the typesetter.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public interface Boxable extends BoxOrRule {
 
--- ExTeX/src/java/de/dante/extex/interpreter/type/box/RuleConvertible.java
+++ ExTeX/src/java/de/dante/extex/interpreter/type/box/RuleConvertible.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -27,10 +27,11 @@ import de.dante.extex.typesetter.type.node.RuleNode;
 
 /**
  * This interface describes the possibility to provide a rule as result of the
- * operation.
+ * operation. Usually this box is gathered from the input stream and build by
+ * the typesetter.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public interface RuleConvertible extends BoxOrRule {
 
