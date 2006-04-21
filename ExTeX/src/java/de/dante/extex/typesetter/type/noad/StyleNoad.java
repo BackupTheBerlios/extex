c037d21bac0d05bc17ec4f44e2c888046259a33f
gene <gene>
2006-04-21 09:59:36 +0000

    support for variants added

--- ExTeX/src/java/de/dante/extex/typesetter/type/noad/StyleNoad.java
+++ ExTeX/src/java/de/dante/extex/typesetter/type/noad/StyleNoad.java
@@ -22,7 +22,6 @@ package de.dante.extex.typesetter.type.noad;
 import java.io.ObjectStreamException;
 import java.util.logging.Logger;
 
-import de.dante.extex.interpreter.exception.ImpossibleException;
 import de.dante.extex.typesetter.TypesetterOptions;
 import de.dante.extex.typesetter.exception.TypesetterException;
 import de.dante.extex.typesetter.type.NodeList;
@@ -33,38 +32,181 @@ import de.dante.util.framework.configuration.exception.ConfigurationException;
  * This Noad indicates a change in the style to be used for the further
  * processing.
  *
+ * <p>
+ *  <logo>TeX</logo> defined a set of variations to a style.
+ *  They are defined in the following table:
+ * </p>
+ * <table>
+ *  <tr>
+ *   <th>Style</th>
+ *   <th>No.</th>
+ *   <th>Cramped</th>
+ *   <th>Sub</th>
+ *   <th>Sup</th>
+ *   <th>Num</th>
+ *   <th>Denom</th>
+ *  </tr>
+ *  <tr>
+ *   <td>display style</td>
+ *   <td>0</td>
+ *   <td>1</td>
+ *   <td>5</td>
+ *   <td>4</td>
+ *   <td>2</td>
+ *   <td>3</td>
+ *  </tr>
+ *  <tr>
+ *   <td>cramped display style</td>
+ *   <td>1</td>
+ *   <td>1</td>
+ *   <td>5</td>
+ *   <td>5</td>
+ *   <td>3</td>
+ *   <td>3</td>
+ *  </tr>
+ *  <tr>
+ *   <td>text style</td>
+ *   <td>2</td>
+ *   <td>3</td>
+ *   <td>5</td>
+ *   <td>4</td>
+ *   <td>4</td>
+ *   <td>5</td>
+ *  </tr>
+ *  <tr>
+ *   <td>cramped text style</td>
+ *   <td>3</td>
+ *   <td>3</td>
+ *   <td>5</td>
+ *   <td>5</td>
+ *   <td>5</td>
+ *   <td>5</td>
+ *  </tr>
+ *  <tr>
+ *   <td>script style</td>
+ *   <td>4</td>
+ *   <td>5</td>
+ *   <td>7</td>
+ *   <td>6</td>
+ *   <td>6</td>
+ *   <td>7</td>
+ *  </tr>
+ *  <tr>
+ *   <td>cramped script style</td>
+ *   <td>5</td>
+ *   <td>5</td>
+ *   <td>7</td>
+ *   <td>7</td>
+ *   <td>7</td>
+ *   <td>7</td>
+ *   <td>7</td>
+ *  </tr>
+ *  <tr>
+ *   <td>script script style</td>
+ *   <td>6</td>
+ *   <td>7</td>
+ *   <td>7</td>
+ *   <td>6</td>
+ *   <td>6</td>
+ *   <td>7</td>
+ *  </tr>
+ *  <tr>
+ *   <td>cramped script script style</td>
+ *   <td>7</td>
+ *   <td>7</td>
+ *   <td>7</td>
+ *   <td>7</td>
+ *   <td>7</td>
+ *   <td>7</td>
+ *  </tr>
+ * </table>
+ *
+ * <p>
+ *  This mapping and the numbers therein are not visible directly. Instead
+ *  symbolic constants and methods are provided.
+ * <p>
+ *
+ *
+ *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.18 $
+ * @version $Revision: 1.19 $
  */
 public final class StyleNoad implements Noad {
+
+    /**
+     * The field <tt>styles</tt> contains the list of styles prepared for use.
+     * Some of them are available via constants; others can be acquired via
+     * some methods-
+     */
+    private static final StyleNoad[] STYLE = {//
+    new StyleNoad(0, "display", "\\displaystyle"),
+            new StyleNoad(1, "display", "\\displaystyle"),
+            new StyleNoad(2, "text", "\\textstyle"),
+            new StyleNoad(3, "text", "\\textstyle"),
+            new StyleNoad(4, "script", "\\scriptstyle"),
+            new StyleNoad(5, "script", "\\scriptstyle"),
+            new StyleNoad(6, "scriptscript", "\\scriptscriptstyle"),
+            new StyleNoad(7, "scriptscript", "\\scriptscriptstyle")};
+
+    /**
+     * The field <tt>CRAMPED</tt> contains the mapping to cramped styles.
+     */
+    private static final StyleNoad[] CRAMPED = {STYLE[1], STYLE[1], STYLE[3],
+            STYLE[3], STYLE[5], STYLE[5], STYLE[7], STYLE[7]};
+
+    /**
+     * The field <tt>DENOM</tt> contains the mapping to denom styles.
+     */
+    private static final StyleNoad[] DENOM = {STYLE[5], STYLE[6], STYLE[7],
+            STYLE[7], STYLE[7], STYLE[7], STYLE[7], STYLE[7]};
 
     /**
      * The constant <tt>DISPLAYSTYLE</tt> contains the value for the display
      * style.
      */
-    public static final StyleNoad DISPLAYSTYLE = new StyleNoad("text",
-            "\\displaystyle");
+    public static final StyleNoad DISPLAYSTYLE = STYLE[0];
+
+    /**
+     * The field <tt>NUM</tt> contains the mapping to num styles.
+     */
+    private static final StyleNoad[] NUM = {STYLE[2], STYLE[3], STYLE[4],
+            STYLE[5], STYLE[6], STYLE[6], STYLE[6], STYLE[7]};
 
     /**
      * The constant <tt>SCRIPTSCRIPTSTYLE</tt> contains the value for the
      * scriptscript style.
      */
-    public static final StyleNoad SCRIPTSCRIPTSTYLE = new StyleNoad(
-            "scriptscript", "\\scriptscriptstyle");
+    public static final StyleNoad SCRIPTSCRIPTSTYLE = STYLE[6];
 
     /**
      * The constant <tt>SCRIPTSTYLE</tt> contains the value for the script
      * style.
      */
-    public static final StyleNoad SCRIPTSTYLE = new StyleNoad("script",
-            "\\scriptstlye");
+    public static final StyleNoad SCRIPTSTYLE = STYLE[4];
+
+    /**
+     * The field <tt>SUB</tt> contains the mapping to sub styles.
+     */
+    private static final StyleNoad[] SUB = {STYLE[5], STYLE[5], STYLE[5],
+            STYLE[5], STYLE[7], STYLE[7], STYLE[7], STYLE[7]};
+
+    /**
+     * The field <tt>SUP</tt> contains the mapping to sup styles.
+     */
+    private static final StyleNoad[] SUP = {STYLE[4], STYLE[5], STYLE[4],
+            STYLE[5], STYLE[6], STYLE[7], STYLE[6], STYLE[7]};
 
     /**
      * The constant <tt>TEXTSTYLE</tt> contains the value for the text
      * style.
      */
-    public static final StyleNoad TEXTSTYLE = new StyleNoad("text",
-            "\\textstyle");
+    public static final StyleNoad TEXTSTYLE = STYLE[2];
+
+    /**
+     * The field <tt>no</tt> contains the <logo>TeX</logo> encoding of the
+     * style.
+     */
+    private int no;
 
     /**
      * The field <tt>printName</tt> contains the printable representation.
@@ -84,24 +226,46 @@ public final class StyleNoad implements Noad {
      * new instances. The constants defined in this class should be used
      * instead.
      *
+     * @param no the index of this style
      * @param style the style
      * @param printName the printable representation
      */
-    private StyleNoad(final String style, final String printName) {
+    private StyleNoad(final int no, final String style, final String printName) {
 
         super();
+        this.no = no;
         this.style = style;
         this.printName = printName;
     }
 
     /**
-     * Getter for style.
+     * Get the cramped style for this one.
      *
-     * @return the style
+     * @return the cramped style
      */
-    public String getStyleName() {
+    public StyleNoad cramped() {
 
-        return this.style + "style";
+        return CRAMPED[no];
+    }
+
+    /**
+     * Get the denom style for this one.
+     *
+     * @return the denom style
+     */
+    public StyleNoad denom() {
+
+        return DENOM[no];
+    }
+
+    /**
+     * Getter for font name.
+     *
+     * @return the font name
+     */
+    public String getFontName() {
+
+        return this.style + "font";
     }
 
     /**
@@ -112,6 +276,16 @@ public final class StyleNoad implements Noad {
     public String getName() {
 
         return this.style;
+    }
+
+    /**
+     * Getter for style.
+     *
+     * @return the style
+     */
+    public String getStyleName() {
+
+        return this.style + "style";
     }
 
     /**
@@ -131,10 +305,20 @@ public final class StyleNoad implements Noad {
     }
 
     /**
+     * Get the num style for this one.
+     *
+     * @return the num style
+     */
+    public StyleNoad num() {
+
+        return NUM[no];
+    }
+
+    /**
      * Return the singleton constant object after the serialized instance
      * has been read back in.
      * This is one of the magic methods of Java which is invoked when a
-     * serialized object is read back in.
+     * serialized object is deserialized.
      *
      * @return the one and only instance of this object
      *
@@ -142,16 +326,7 @@ public final class StyleNoad implements Noad {
      */
     protected Object readResolve() throws ObjectStreamException {
 
-        if ("textstyle".equals(style)) {
-            return TEXTSTYLE;
-        } else if ("displaystyle".equals(style)) {
-            return DISPLAYSTYLE;
-        } else if ("scriptstyle".equals(style)) {
-            return SCRIPTSTYLE;
-        } else if ("scriptscriptstyle".equals(style)) {
-            return SCRIPTSCRIPTSTYLE;
-        }
-        throw new ImpossibleException("style not found");
+        return STYLE[no];
     }
 
     /**
@@ -168,6 +343,34 @@ public final class StyleNoad implements Noad {
      */
     public void setSuperscript(final Noad superscript) {
 
+    }
+
+    /**
+     * Get the sub style for this one.
+     *
+     * @return the sub style
+     */
+    public StyleNoad sub() {
+
+        return SUB[no];
+    }
+
+    /**
+     * Get the sup style for this one.
+     *
+     * @return the sup style
+     */
+    public StyleNoad sup() {
+
+        return SUP[no];
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+
+        return printName;
     }
 
     /**
