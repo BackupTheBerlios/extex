379bae2a7338228fcd89ab77001fc845d2a82244
gene <gene>
2006-04-21 09:52:30 +0000

    new

---
+++ ExTeX/src/java/de/dante/extex/typesetter/type/noad/util/MathSpacing.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2006 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.typesetter.type.noad.util;
+
+/**
+ * TODO gene: missing JavaDoc.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public final class MathSpacing {
+
+    /**
+     * The field <tt>ORD_NOAD</tt> contains the ...
+     */
+    public static final MathSpacing ORD_NOAD = new MathSpacing();
+
+    /**
+     * The field <tt>OP_NOAD</tt> contains the ...
+     */
+    public static final MathSpacing OP_NOAD = new MathSpacing();
+
+    /**
+     * The field <tt>BIN_NOAD</tt> contains the ...
+     */
+    public static final MathSpacing BIN_NOAD = new MathSpacing();
+
+    /**
+     * The field <tt>REL_NOAD</tt> contains the ...
+     */
+    public static final MathSpacing REL_NOAD = new MathSpacing();
+
+    /**
+     * The field <tt>OPEN_NOAD</tt> contains the ...
+     */
+    public static final MathSpacing OPEN_NOAD = new MathSpacing();
+
+    /**
+     * The field <tt>CLOSE_NOAD</tt> contains the ...
+     */
+    public static final MathSpacing CLOSE_NOAD = new MathSpacing();
+
+    /**
+     * The field <tt>PUNCT_NOAD</tt> contains the ...
+     */
+    public static final MathSpacing PUNCT_NOAD = new MathSpacing();
+
+    /**
+     * The field <tt>INNER_NOAD</tt> contains the ...
+     */
+    public static final MathSpacing INNER_NOAD = new MathSpacing();
+
+    /**
+     * Creates a new object.
+     */
+    private MathSpacing() {
+
+        super();
+    }
+
+}
