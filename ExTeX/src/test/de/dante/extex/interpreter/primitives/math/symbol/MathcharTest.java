bf2dc912ad07b3082d8c02aa4ba3038e211bfd69
gene <gene>
2006-04-23 18:05:07 +0000

    reformatted

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/math/symbol/MathcharTest.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/math/symbol/MathcharTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -21,12 +21,11 @@ package de.dante.extex.interpreter.primitives.math.symbol;
 
 import de.dante.extex.interpreter.primitives.math.AbstractMathTester;
 
-
 /**
  * This is a test suite for the primitive <tt>\mathchar</tt>.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class MathcharTest extends AbstractMathTester {
 
@@ -39,5 +38,6 @@ public class MathcharTest extends AbstractMathTester {
 
         super(arg, "mathchar", "123 ");
     }
+
 
 }
