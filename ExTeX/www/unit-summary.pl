d78737dbd562f07930b9baae8361d055b3331fa9
gene <gene>
2006-05-19 15:17:25 +0000

    adding more functionality

--- ExTeX/www/unit-summary.pl
+++ ExTeX/www/unit-summary.pl
@@ -1,6 +1,6 @@
 #!/bin/perl.exe -w
 ##*****************************************************************************
-## $Id: unit-summary.pl,v 1.1 2006/05/08 15:59:13 gene Exp $
+## $Id: unit-summary.pl,v 1.2 2006/05/19 15:17:25 gene Exp $
 ##*****************************************************************************
 ## Author: Gerd Neugebauer
 ##=============================================================================
@@ -38,6 +38,8 @@ Gerd Neugebauer
 use strict;
 use File::Basename;
 use File::Copy;
+use File::Find
+;
 use FileHandle;
 
 #------------------------------------------------------------------------------
@@ -62,69 +64,157 @@ GetOptions("h|help"	=> \&usage,
 	   "v|verbose"	=> \$verbose,
 	  );
 
-my %cache = ('' => 1);
+my $BASEDIR = dirname($0) . '/../';
+
+my %info;
+my %infoAuthor;
+my %infoIncomplete = ( '' => 1 );
+
 my @units;
 my @primitives;
 my @configurations;
-my $targetdir  = dirname($0) . "/../target/www/unit";
+my $targetdir  = $BASEDIR . "target/www/unit";
+
 mkdir $targetdir if not -e $targetdir;
 
 chdir dirname($0);
 
+find({no_chdir => 1, wanted => \&analyze}, $BASEDIR . 'src/java');
+print "\n" if $verbose;
+
 foreach $_ (glob 'unit-summary/*') {
-  my $f = $targetdir."/".basename($_);
-  copy($_,$f) if not m/~$/;
+  next if m/CVS/;
+  my $f = $targetdir . "/" . basename($_);
+  copy($_, $f) if not m/~$/;
 }
 
-foreach $_ (glob '../src/java/config/unit/*.xml') {
-  print STDERR "--- $_\n" if $verbose;
+foreach $_ (glob $BASEDIR . 'src/java/config/unit/*.xml') {
   process_unit($_);
 }
 
-foreach $_ (glob '../src/java/config/*.xml') {
-  print STDERR "--- $_\n" if $verbose;
+foreach $_ (glob $BASEDIR . 'src/java/config/*.xml') {
   process_cfg($_);
 }
 
-my $out = HTML_start("$targetdir/overview.html",
-		     'Overview',
-		     'navi.css', <<__EOF__);
+make_overview();
+make_units();
+
+#------------------------------------------------------------------------------
+#
+sub make_overview {
+
+  my $out = HTML_start("$targetdir/overview.html",
+		       'Overview',
+		       'navi.css',
+		       '', <<__EOF__);
 <h1>ExTeX</h1>
 <a href="units.html" target="unitFrame">All Units</a>
 <p>Units</p>
 __EOF__
 
-foreach $_ (sort @units) {
-  print $out "<a href=\"unit-$_.html\" target=\"unitFrame\">$_</a><br />";
-}
+  foreach $_ (sort @units) {
+    print $out "<a href=\"unit-$_.html\" target=\"unitFrame\">$_</a><br />\n";
+  }
 
-print $out <<__EOF__;
+  print $out <<__EOF__;
 <p>Configurations</p>
 __EOF__
 
-foreach $_ (sort {return lc($$a[0]) cmp lc($$b[0])} @configurations) {
-  print $out "<a href=\"cfg_$$_[0].html\" target=\"infoFrame\">$$_[0]</a><br />";
+  foreach $_ (sort {return lc($$a[0]) cmp lc($$b[0])} @configurations) {
+    print $out "<a href=\"cfg_$$_[0].html\" target=\"infoFrame\">$$_[0]</a><br />\n";
+  }
+
+  HTML_end($out);
 }
 
-HTML_end($out);
-
-$out = HTML_start("$targetdir/units.html",
-		  'All Units',
-		  'navi.css', <<__EOF__);
+#------------------------------------------------------------------------------
+#
+sub make_units {
+  my $out = HTML_start("$targetdir/units.html",
+		       'All Units',
+		       'navi.css',
+		       '', <<__EOF__);
 <h2>All Units</h2>
 __EOF__
 
-foreach $_ (sort {return lc($$a[0]) cmp lc($$b[0])} @primitives) {
-  my @a = @$_;
-  if ($a[3] eq '') {
-    print $out "<a href=\"$a[2]\" target=\"infoFrame\">\\$a[0]</a><br />";
-  } else {
-    print $out "<span class=\"undef\">\\$a[0]</span><br />";
+  foreach $_ (sort {return lc($$a[0]) cmp lc($$b[0])} @primitives) {
+    my @a = @$_;
+    if ($a[3] eq '') {
+      print $out "<a href=\"$a[2]\" target=\"infoFrame\">\\$a[0]</a> ($a[4])<br />\n";
+    } else {
+      print $out "<span class=\"undef\">\\$a[0] ($a[4])</span><br />\n";
+    }
   }
+  
+  HTML_end($out);
 }
 
-HTML_end($out);
+#------------------------------------------------------------------------------
+# Function:     analyze
+#
+sub analyze {
+  my $file = $_;
+  if ( $_ eq 'CVS' ) {
+    $File::Find::prune = 1;
+    return
+  } 
+  return if not $_ =~ m/\.(java|html)$/;
+#  $noFiles++;
+#  print STDERR '.' if $verbose;
 
+  my $class = basename($_,'.java');
+  my $fd    = new FileHandle($_, 'r') || die "$_: $!\n";
+  s|^./||;
+  s:\.(java|html)$::;
+  s|/|.|g;
+  my $base     = $_;
+  my $inDoc    = undef;
+  my $type     = "";
+  my $name     = "";
+  my $contents = "";
+  my $package  = "";
+  my $author   = '';
+  local $_;
+
+  while(<$fd>) {
+    if (m/package (.*);/ ) { $package = $1; }
+    elsif ( $inDoc ) {
+      if (m/\<\/doc\>/) {
+	$info{"$type:$name"} = (exists $info{"$type:$name"} ? '?' : $contents);
+	$info{"$name"} = (exists $info{"$name"} ? '?' : $contents);
+        $info{"$package.$class:$type:$name"} = $contents;
+        $info{"$package.$class:$type"} = $contents;
+        $info{"$package.$class:$name"} = $contents;
+        $inDoc                = undef;
+        $type		      = "";
+        $name		      = "";
+        $contents	      = "";
+      } else {
+        s/^\s*\* ?//;
+        $contents  .= $_;
+      }
+    } elsif (m/\<doc(.*)\>/) {
+      $_        = $1;
+ #     $noDocs++;
+      if (m/name="([^"]*)"/) { $name = $1; }
+      if (m/type="([^"]*)"/) { $type = $1; }
+      if ( $name eq '' ) { print STDERR "\n$file: Missing name\n"; }
+      $inDoc = 1;
+    } elsif (m/\@author\s+(.*)/) {
+      $_ = $1;
+      s/^[^>]*>//;
+      s/<.*//;
+      $author .= '; ' if $author ne '';
+      $author .= $_;
+    } elsif (m/TODO.*unimplemented/) {
+      $infoIncomplete{"$package.$class"} = 1;
+    }
+  }
+
+  $infoAuthor{"$package.$class"} = $author;
+  
+  $fd->close();
+}
 
 #------------------------------------------------------------------------------
 #
@@ -134,7 +224,8 @@ sub process_cfg {
   my $ok     = 0;
   my $banner = '';
   my @u;
-  my $fd     = new FileHandle($file, 'r') || die "$file: $!\n";;
+  print STDERR "--- Processing configuration $cfg\n" if $verbose;
+  my $fd = new FileHandle($file, 'r') || die "$file: $!\n";;
 
   while (<$fd>) {
     if (m/<ExTeX/) {
@@ -151,7 +242,10 @@ sub process_cfg {
 
   push @configurations, [$cfg, $banner, @u];
 
-  $fd = HTML_start("$targetdir/cfg_$cfg.html", "The Configuration $cfg", 'info.css', <<__EOF__);
+  $fd = HTML_start("$targetdir/cfg_$cfg.html",
+		   "The Configuration $cfg",
+		   'info.css',
+		   '', <<__EOF__);
 <h1>The Configuration <tt>$cfg</tt></h1>
 <p>$banner</p>
 <h2>Units</h2>
@@ -175,21 +269,28 @@ sub process_unit {
   my $class = '';
   my $def;
   my @prim;
+  my $see = undef;
+
+  print STDERR "--- Processing unit $unit\n" if $verbose;
 
   push @units, $unit;
 
   my $fd = new FileHandle($file, 'r') || die "$file: $!\n";;
 
   while (<$fd>) {
-    $def = "" if m/<define/;
-    $def = "n" if m/<Define/;
-    $name = $1 if m/fine name="([a-z]*)"/i;
+    $see   = $1 if m/<unit.*src=\"([^"]*)\"/; #"
+    $def   = "" if m/<define/;
+    $def   = "n" if m/<Define/;
+    $name  = $1 if m/fine name="([a-z]*)"/i;
     $class = $1 if m/class="([^\"]*)"/;
 
     if (m/>/ and defined $name) {
+      $class = '' if $def ne '';
       my $s  = $class;
       $s     =~ s/.*primitives.//;
-      $class = '' if $def ne '';
+      if (not defined $info{"$class:$name"}) {
+	$s .= "_$name";
+      }
       push @prim, [$name, $class, $s, $def, $unit];
       push @primitives, [$name, $class, $s, $def, $unit];
       undef $name;
@@ -200,23 +301,30 @@ sub process_unit {
   $fd = HTML_start("$targetdir/unit-$unit.html",
 		   "Unit $unit",
 		   'navi.css',
+		   '',
 		   "<h2>Unit $unit</h2>\n");
 
   foreach $_ (sort {return lc($$a[0]) cmp lc($$b[0])} @prim) {
     my @a = @$_;
     if ($a[3] eq '') {
-      print $fd "<a href=\"$a[2]\" target=\"infoFrame\">\\$a[0]</a><br />";
+      print $fd "<a href=\"$a[2]\" target=\"infoFrame\">\\$a[0]</a><br />\n";
     } else {
-      print $fd "<span class=\"undef\">\\$a[0]</span><br />";
+      print $fd "<span class=\"undef\">\\$a[0]</span><br />\n";
     }
+  }
+
+  if (defined $see) {
+    print $fd <<__EOF__
+<p>
+ Further primitives are imported from
+ <a href="unit-$see.html" target="unitFrame">$see</a>.
+</p>
+__EOF__
   }
 
   HTML_end($fd);
 
   foreach $_ (@prim) {
-    my @a = @$_;
-    next if defined $cache{$a[2]};
-    $cache{$a[2]} = 1;
     doit(@$_);
   }
 }
@@ -225,48 +333,57 @@ sub process_unit {
 #
 sub doit {
   my ($name,$class,$short,$def,$unit) = @_;
-  my $doc  = '';
-  if ($class eq '') {
-    $doc = "<h2>The Primitive \\$name</h2>";
-  } else {
-    local $_ = $class;
-    s|\.|/|g;
-    my $java = "../src/java/$_.java";
-    my $fd   = new FileHandle($java, 'r') || die "$java: $!\n";
-    while(<$fd>) {
-      if (m/<doc/) {
-	while(<$fd>) {
-	  last if m|</doc|;
-	  s/^ *\* ?//;
-	  s///;
-	  s|TODO (.*)|<p><img src="construct.gif" alt="$1" title="$1"/></p>|;
-	  $doc  .= $_;
-        }
-      }
-    }
-    $fd->close();
+  my $doc    = '';
+  my $author = '';
+  my $warn   = '';
+  local $_;
+
+  return if $class eq '';
+
+  $doc = $info{"$class:$name"};
+  foreach $_ ("$class:", "register:", "parameter:", ":", "") {
+    $doc = $info{"$_$name"};
+    last if defined $doc and $doc ne '?';
+  }
+  if (not defined $doc) {
+    $doc = '';
+    print STDERR "*** $name undocumented\n" if $verbose;
+  } elsif($doc eq '?') {
+    $doc = '';
+    print STDERR "*** $name ambiguous\n" if $verbose;
   }
 
+  $warn = "<p><img src=\"unimplemented.png\"></p>\n"
+      if $infoIncomplete{$class};
+
   if ($doc eq '') {
-    $doc = <<__EOF__;
+    $warn = "<p><img src=\"unimplemented.png\"></p>\n";
+    $doc     = <<__EOF__;
 <h3>The Primitive \\$name</h3>
 <p><img src="construct.gif" alt="missing" title="missing"/></p>
 __EOF__
   } else {
-    $doc =~ s|\{[@]linkplain\s*\S+\s*([^\}]+)\}|$1|gsm;
-    $doc =~ s|\{[@]link\s*\S+\s*([^\}]+)\}|$1|gsm;
+    $doc  =~ s|TODO (.*)|<p><img src="construct.gif" alt="$1" title="$1"/></p>|g;
+    $doc =~ s|\{[@]linkplain\s*[^{}]+\)\s+([^\}]+)\}|$1|gsm;
+    $doc =~ s|\{[@]link\s*[^{}]+\)\s+([^\}]+)\}|$1|gsm;
   }
 
-  my $fd = HTML_start("$targetdir/$short",
-		      "Primitive \\$name",
-		      'info.css', <<__EOF__);
+  my @d    = localtime;
+  my $date = $d[5] + 1900;
+  my $fd   = HTML_start("$targetdir/$short",
+			"Primitive \\$name",
+			'info.css',
+			$author, <<__EOF__);
 <div class="unit">From unit $unit</div>
 <hr />
-$doc
+$warn$doc
 <hr />
 <div class="footer">Submit
  a <a href="https://developer.berlios.de/bugs/?group_id=1915">bug report</a>
  or a <a href="https://developer.berlios.de/feature/?group_id=1915">feature request</a>.
+</div>
+<div class="copyright">Copyright &copy; $date The ExTeX Group;
+ Use is subject to <a href="fdl.html">license terms</a>.
 </div>
 __EOF__
   HTML_end($fd);
@@ -275,16 +392,19 @@ __EOF__
 #------------------------------------------------------------------------------
 #
 sub HTML_start {
-  my $file  = shift;
-  my $title = shift;
-  my $style = shift;
-  my $fd    = new FileHandle($file, 'w') || die "$file: $!\n";;
+  my $file   = shift;
+  my $title  = shift;
+  my $style  = shift;
+  my $author = shift;
+  my $fd     = new FileHandle($file, 'w') || die "$file: $!\n";;
+
+  $author = "\n  <meta name=\"Author\" content=\"$author\">" if $author ne '';
 
   print $fd <<__EOF__;
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML//3.2//EN">
 <html>
 <head>
-  <title>$title</title>
+  <title>$title</title>$author
   <link rel="stylesheet" href="$style">
 </head>
 <body>
