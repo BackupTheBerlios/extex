be7aeba5dd729ee5e0e7640589516c7b1868f335
gene <gene>
2006-05-31 07:59:38 +0000

    parameter Context added to al methods

--- ExTeX/src/java/de/dante/extex/interpreter/context/extension/ExtensionPoint.java
+++ ExTeX/src/java/de/dante/extex/interpreter/context/extension/ExtensionPoint.java
@@ -21,28 +21,36 @@ package de.dante.extex.interpreter.context.extension;
 
 import java.io.Serializable;
 
+import de.dante.extex.interpreter.context.Context;
+
 /**
  * This interface describes the callbacks used for an extension point.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public interface ExtensionPoint extends Serializable {
 
     /**
      * This method is invoked upon initialization to prepare everything for
      * proper operation.
+     *
+     * @param context the interpreter context
      */
-    void init();
+    void init(Context context);
 
     /**
      * This method is invoked when a group is opened.
+     *
+     * @param context the interpreter context
      */
-    void beginGroup();
+    void beginGroup(Context context);
 
     /**
      * This method is invoked when a group is closed.
+     *
+     * @param context the interpreter context
      */
-    void endGroup();
+    void endGroup(Context context);
 
 }
