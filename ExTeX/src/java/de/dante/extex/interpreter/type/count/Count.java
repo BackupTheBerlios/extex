672bc95f8343613239629c6d87bdc7d03cbe3553
gene <gene>
2006-06-11 22:09:12 +0000

    new number parser created here.

--- ExTeX/src/java/de/dante/extex/interpreter/type/count/Count.java
+++ ExTeX/src/java/de/dante/extex/interpreter/type/count/Count.java
@@ -21,12 +21,23 @@ package de.dante.extex.interpreter.type.count;
 
 import java.io.Serializable;
 
+import de.dante.extex.interpreter.Flags;
 import de.dante.extex.interpreter.TokenSource;
 import de.dante.extex.interpreter.context.Context;
 import de.dante.extex.interpreter.exception.InterpreterException;
 import de.dante.extex.interpreter.exception.helping.ArithmeticOverflowException;
-import de.dante.extex.interpreter.exception.helping.EofException;
+import de.dante.extex.interpreter.exception.helping.HelpingException;
+import de.dante.extex.interpreter.exception.helping.MissingNumberException;
+import de.dante.extex.interpreter.type.Code;
+import de.dante.extex.interpreter.type.ExpandableCode;
+import de.dante.extex.interpreter.type.scaled.ScaledNumber;
 import de.dante.extex.interpreter.type.tokens.Tokens;
+import de.dante.extex.scanner.type.Catcode;
+import de.dante.extex.scanner.type.token.CodeToken;
+import de.dante.extex.scanner.type.token.ControlSequenceToken;
+import de.dante.extex.scanner.type.token.LetterToken;
+import de.dante.extex.scanner.type.token.OtherToken;
+import de.dante.extex.scanner.type.token.SpaceToken;
 import de.dante.extex.scanner.type.token.Token;
 import de.dante.extex.typesetter.Typesetter;
 import de.dante.util.exception.GeneralException;
@@ -39,7 +50,7 @@ import de.dante.util.framework.i18n.LocalizerFactory;
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
  * @author <a href="mailto:m.g.n@gmx.de">Michael Niedermair</a>
- * @version $Revision: 1.27 $
+ * @version $Revision: 1.28 $
  */
 public class Count implements Serializable, FixedCount {
 
@@ -68,6 +79,411 @@ public class Count implements Serializable, FixedCount {
     public static final Count ZERO = new ImmutableCount(0);
 
     /**
+     * This interface describes a binary operation on two longs.
+     *
+     * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+     * @version $Revision: 1.28 $
+     */
+    private interface BinOp {
+
+        /**
+         * Apply the operation on the arguments.
+         *
+         * @param arg1 the first argument
+         * @param arg2 the second argument
+         *
+         * @return the result
+         */
+        long apply(long arg1, long arg2);
+    }
+
+    /**
+     * This operation subtracts the second argument from the first one.
+     *
+     * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+     * @version $Revision: 1.28 $
+     */
+    private static final class Minus implements BinOp {
+
+        /**
+         * @see de.dante.extex.interpreter.primitives.register.count.Numexpr.BinOp#apply(long, long)
+         */
+        public long apply(final long arg1, final long arg2) {
+
+            return arg1 - arg2;
+        }
+    }
+
+    /**
+     * This operation adds the arguments.
+     *
+     * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+     * @version $Revision: 1.28 $
+     */
+    private static final class Plus implements BinOp {
+
+        /**
+         * @see de.dante.extex.interpreter.primitives.register.count.Numexpr.BinOp#apply(long, long)
+         */
+        public long apply(final long arg1, final long arg2) {
+
+            return arg1 + arg2;
+        }
+    }
+
+    /**
+     * This operation ignores the first argument and returns the second one.
+     *
+     * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+     * @version $Revision: 1.28 $
+     */
+    private static final class Second implements BinOp {
+
+        /**
+         * @see de.dante.extex.interpreter.primitives.register.count.Numexpr.BinOp#apply(long, long)
+         */
+        public long apply(final long arg1, final long arg2) {
+
+            return arg2;
+        }
+    }
+
+    /**
+     * The field <tt>MINUS</tt> contains the subtractor.
+     */
+    private static final BinOp MINUS = new Minus();
+
+    /**
+     * The field <tt>PLUS</tt> contains the adder.
+     */
+    private static final BinOp PLUS = new Plus();
+
+    /**
+     * The field <tt>SECOND</tt> contains the operation to select the second
+     * argument.
+     */
+    private static final BinOp SECOND = new Second();
+
+    /**
+     * Evaluate an expression.
+     *
+     * @param context the interpreter context
+     * @param source the source for new tokens
+     * @param typesetter the typesetter
+     *
+     * @return the result
+     *
+     * @throws InterpreterException in case of an error
+     */
+    private static long evalExpr(final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws InterpreterException {
+
+        long saveVal = 0;
+        BinOp op = SECOND;
+        long val = scanInteger(context, source, typesetter);
+
+        for (;;) {
+
+            Token t = source.getNonSpace(context);
+            if (t == null) {
+                throw new MissingNumberException();
+
+            } else if (t.equals(Catcode.OTHER, '*')) {
+                val *= scanInteger(context, source, typesetter);
+
+            } else if (t.equals(Catcode.OTHER, '/')) {
+                long x = scanInteger(context, source, typesetter);
+                if (x == 0) {
+                    throw new ArithmeticOverflowException("");
+                }
+                val /= x;
+
+            } else if (t.equals(Catcode.OTHER, '+')) {
+                saveVal = op.apply(saveVal, val);
+                val = scanInteger(context, source, typesetter);
+                op = PLUS;
+
+            } else if (t.equals(Catcode.OTHER, '-')) {
+                saveVal = op.apply(saveVal, val);
+                val = scanInteger(context, source, typesetter);
+                op = MINUS;
+
+            } else {
+                source.push(t);
+                return op.apply(saveVal, val);
+            }
+        }
+    }
+
+    /**
+     * Scan the input stream for an integer value.
+     *
+     * @param context the processor context
+     * @param source the source for new tokens
+     * @param typesetter the typesetter to use for conversion
+     *
+     * @return the value of the count
+     *
+     * @throws InterpreterException in case of an error
+     */
+    public static long scanInteger(final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws InterpreterException {
+
+        for (;;) {
+            Token t = source.getNonSpace(context);
+            if (t == null) {
+                throw new MissingNumberException();
+
+            } else if (t.equals(Catcode.OTHER, '(')) {
+                long val = evalExpr(context, source, typesetter);
+                t = source.getToken(context);
+                if (t.equals(Catcode.OTHER, ')')) {
+                    return val;
+                }
+
+                throw new HelpingException(LocalizerFactory
+                        .getLocalizer(ScaledNumber.class.getName()),
+                        "MissingParenthesis", (t == null ? "null" : t
+                                .toString()));
+
+            } else if (t.equals(Catcode.OTHER, '-')) {
+                return -scanInteger(context, source, typesetter);
+
+            } else if (t.equals(Catcode.OTHER, '+')) {
+                // continue
+
+            } else if (t instanceof CodeToken) {
+                Code code = context.getCode((CodeToken) t);
+                if (code instanceof CountConvertible) {
+                    return ((CountConvertible) code).convertCount(context,
+                            source, typesetter);
+
+                } else if (code instanceof ExpandableCode) {
+                    ((ExpandableCode) code).expand(Flags.NONE, context, source,
+                            typesetter);
+                } else {
+                    break;
+                }
+            } else {
+                return scanNumber(context, source, typesetter, t);
+            }
+        }
+
+        throw new MissingNumberException();
+    }
+
+    /**
+     * Scan the input stream for tokens making up a number, i.e. a sequence of
+     * digits with category code OTHER. The number can be preceded by optional
+     * white space.
+     * <p>
+     *  This method implements the generalization of several syntactic
+     *  definitions from <logo>TeX</logo>:
+     * </p>
+     *
+     * <doc type="syntax" name="number">
+     * <h3>A Number</h3>
+     *
+     * <pre class="syntax">
+     *   &lang;number&rang; </pre>
+     * <p>
+     *  A number consists of a non-empty sequence of digits with category code
+     *  {@link de.dante.extex.scanner.type.Catcode#OTHER OTHER}.
+     * </p>
+     *
+     * </doc>
+     *
+     *
+     * Scan the input stream for tokens making up a number, this is a sequence
+     * of digits with category code <tt>OTHER</tt>. The number can be preceded
+     * by optional white space. Alternate representations for an integer exist.
+     *
+     * @param context the processor context
+     * @param source the source for new tokens
+     * @param typesetter the typesetter
+     *
+     * @return the value of the integer scanned
+     *
+     * @throws InterpreterException in case that no number is found or the
+     *  end of file has been reached before an integer could be acquired
+     */
+    public static long scanNumber(final Context context,
+            final TokenSource source, final Typesetter typesetter)
+            throws InterpreterException {
+
+        return scanNumber(context, source, typesetter, source
+                .getNonSpace(context));
+    }
+
+    /**
+     * Scan the input stream for tokens making up a number, this is a sequence
+     * of digits with category code <tt>OTHER</tt>. The number can be preceded
+     * by optional white space. Alternate representations for an integer exist.
+     *
+     * @param context the processor context
+     * @param source the source for new tokens
+     * @param typesetter the typesetter
+     * @param token the first token
+     *
+     * @return the value of the integer scanned
+     *
+     * @throws InterpreterException in case that no number is found or the
+     *  end of file has been reached before an integer could be acquired
+     */
+    public static long scanNumber(final Context context,
+            final TokenSource source, final Typesetter typesetter,
+            final Token token) throws InterpreterException {
+
+        long n = 0;
+        Token t = token;
+        int no;
+
+        while (t != null) {
+
+            if (t instanceof OtherToken) {
+                int c = t.getChar().getCodePoint();
+                switch (c) {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        n = c - '0';
+
+                        for (t = source.getToken(context); t instanceof OtherToken
+                                && t.getChar().isDigit(); t = source
+                                .getToken(context)) {
+                            n = n * 10 + t.getChar().getCodePoint() - '0';
+                        }
+
+                        if (t instanceof SpaceToken) {
+                            source.skipSpace();
+                        } else {
+                            source.push(t);
+                        }
+                        return n;
+
+                    case '`':
+                        t = source.getToken(context);
+
+                        if (t instanceof ControlSequenceToken) {
+                            String s = ((ControlSequenceToken) t).getName();
+                            return ("".equals(s) ? 0 : s.charAt(0));
+                        } else if (t != null) {
+                            return t.getChar().getCodePoint();
+                        }
+                        // fall through to error handling
+                        break;
+
+                    case '\'':
+                        t = source.scanToken(context);
+                        if (!(t instanceof OtherToken)) {
+                            throw new MissingNumberException();
+                        }
+                        n = t.getChar().getCodePoint() - '0';
+                        if (n < 0 || n > 7) {
+                            throw new MissingNumberException();
+                        }
+                        for (t = source.scanToken(context); t instanceof OtherToken; //
+                        t = source.scanToken(context)) {
+                            no = t.getChar().getCodePoint() - '0';
+                            if (no < 0 || no > 7) {
+                                break;
+                            }
+                            n = n * 8 + no;
+                        }
+
+                        while (t instanceof SpaceToken) {
+                            t = source.getToken(context);
+                        }
+                        source.push(t);
+                        return n;
+
+                    case '"':
+
+                        for (t = source.scanToken(context); //
+                        t instanceof OtherToken || t instanceof LetterToken; //
+                        t = source.scanToken(context)) {
+                            no = t.getChar().getCodePoint();
+                            switch (no) {
+                                case '0':
+                                case '1':
+                                case '2':
+                                case '3':
+                                case '4':
+                                case '5':
+                                case '6':
+                                case '7':
+                                case '8':
+                                case '9':
+                                    n = n * 16 + no - '0';
+                                    break;
+                                case 'a':
+                                case 'b':
+                                case 'c':
+                                case 'd':
+                                case 'e':
+                                case 'f':
+                                    n = n * 16 + no - 'a' + 10;
+                                    break;
+                                case 'A':
+                                case 'B':
+                                case 'C':
+                                case 'D':
+                                case 'E':
+                                case 'F':
+                                    n = n * 16 + no - 'A' + 10;
+                                    break;
+                                default:
+                                    source.push(t);
+                                    source.skipSpace();
+                                    return n;
+                            }
+                        }
+
+                        while (t instanceof SpaceToken) {
+                            t = source.getToken(context);
+                        }
+                        source.push(t);
+                        return n;
+
+                    default:
+                }
+                break;
+            } else if (t instanceof CodeToken) {
+                Code code = context.getCode((CodeToken) t);
+                if (code == null) {
+
+                    break;
+
+                } else if (code instanceof CountConvertible) {
+                    return ((CountConvertible) code).convertCount(context,
+                            source, typesetter);
+                } else if (code instanceof ExpandableCode) {
+                    ((ExpandableCode) code).expand(Flags.NONE, context, source,
+                            typesetter);
+                    t = source.getToken(context);
+                } else {
+
+                    break;
+                }
+            } else {
+
+                break;
+            }
+        }
+
+        throw new MissingNumberException();
+    }
+
+    /**
      * parse a token stream for a count value.
      *
      * @param context the processor context
@@ -81,33 +497,7 @@ public class Count implements Serializable, FixedCount {
     public static Count parse(final Context context, final TokenSource source,
             final Typesetter typesetter) throws InterpreterException {
 
-        return new Count(scanCount(context, source, typesetter));
-    }
-
-    /**
-     * Scan the input stream for a count value.
-     *
-     * @param context the processor context
-     * @param source the source for new tokens
-     * @param typesetter the typesetter to use for conversion
-     *
-     * @return the value of the count
-     *
-     * @throws InterpreterException in case of an error
-     */
-    public static long scanCount(final Context context,
-            final TokenSource source, final Typesetter typesetter)
-            throws InterpreterException {
-
-        Token t = source.getNonSpace(context);
-
-        if (t == null) {
-            throw new EofException();
-        }
-
-        source.push(t);
-
-        return source.scanInteger(context, typesetter);
+        return new Count(scanInteger(context, source, typesetter));
     }
 
     /**
