8941dc9b90b04f4ec667f681f43896926d173415
gene <gene>
2006-06-26 12:59:18 +0000

    imports fixed

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/register/muskip/Gluetomu.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/register/muskip/Gluetomu.java
@@ -25,9 +25,11 @@ import de.dante.extex.interpreter.context.Context;
 import de.dante.extex.interpreter.exception.InterpreterException;
 import de.dante.extex.interpreter.exception.helping.CantUseInException;
 import de.dante.extex.interpreter.type.AbstractCode;
+import de.dante.extex.interpreter.type.Showable;
 import de.dante.extex.interpreter.type.glue.Glue;
 import de.dante.extex.interpreter.type.muskip.Muskip;
 import de.dante.extex.interpreter.type.muskip.MuskipConvertible;
+import de.dante.extex.interpreter.type.tokens.Tokens;
 import de.dante.extex.typesetter.Typesetter;
 
 /**
@@ -56,7 +58,7 @@ import de.dante.extex.typesetter.Typesetter;
  *
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class Gluetomu extends AbstractCode implements MuskipConvertible {
 
