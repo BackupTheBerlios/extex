63246fb6672d321155b2a668eb62892e3be55d5a
gene <gene>
2006-09-01 14:26:43 +0000

    new test cases for the command line interface

---
+++ ExTeX/src/test/de/dante/extex/main/TeXTest.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright (C) 2006 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.main;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+import de.dante.extex.ExTeX;
+import de.dante.extex.interpreter.exception.InterpreterException;
+
+/**
+ * TODO gene: missing JavaDoc.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public class TeXTest extends TestCase {
+
+    /**
+     * The field <tt>BANNER</tt> contains the ...
+     */
+    public static String BANNER = "This is ExTeX, Version "
+            + ExTeX.getVersion() + " (" + System.getProperty("java.version")
+            + ")\n";
+
+    /**
+     * The command line interface.
+     *
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
+
+        junit.textui.TestRunner.run(TeXTest.class);
+    }
+
+    /**
+     * TODO gene: missing JavaDoc
+     *
+     * @param args the array of command line arguments
+     * @param properties the properties to use
+     * @param result
+     *
+     * @throws InterpreterException in case of an interpreter error
+     * @throws IOException in case of an io error
+     */
+    public static void runTest(final String[] args,
+            final Properties properties, final String result, int exit)
+            throws InterpreterException,
+                IOException {
+
+        TeX tex;
+        ByteArrayOutputStream outBuffer = new ByteArrayOutputStream();
+        ByteArrayOutputStream errBuffer = new ByteArrayOutputStream();
+        PrintStream sout = System.out;
+        PrintStream serr = System.err;
+        try {
+            System.setOut(new PrintStream(outBuffer));
+            System.setErr(new PrintStream(errBuffer));
+
+            tex = new TeX(properties, null);
+            int status = tex.run(args);
+
+            assertEquals("", outBuffer.toString());
+            String err = errBuffer.toString();
+            assertEquals(result, err);
+            assertEquals(exit, status);
+
+        } finally {
+            outBuffer.close();
+            System.setOut(sout);
+            errBuffer.close();
+            System.setErr(serr);
+        }
+    }
+
+    /**
+     * <testcase>
+     *  This test case validates that ...
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test__undefined() throws Exception {
+
+        runTest(new String[]{"--undefined"}, System.getProperties(), BANNER
+                + "Missing argument on command line", -10);
+    }
+
+    /**
+     * <testcase>
+     *  This test case validates that <tt>-version</tt> prints the version
+     *  number and exists with code 0.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test_version() throws Exception {
+
+        Properties properties = new Properties();
+        properties.put("java.version", System.getProperty("java.version"));
+        runTest(new String[]{"-version"}, properties, BANNER, 0);
+    }
+
+    /**
+     * <testcase>
+     *  This test case validates that <tt>-ver</tt> prints the version
+     *  number and exists with code 0.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test_ver() throws Exception {
+
+        Properties properties = new Properties();
+        properties.put("java.version", System.getProperty("java.version"));
+        runTest(new String[]{"-ver"}, properties, BANNER, 0);
+    }
+
+    /**
+     * <testcase>
+     *  This test case validates that ...
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test_progname() throws Exception {
+
+        Properties properties = new Properties();
+        properties.put("java.version", System.getProperty("java.version"));
+        runTest(new String[]{"-progname", "abc", "-version"}, properties,
+                "This is abc, Version " + ExTeX.getVersion() + " ("
+                        + System.getProperty("java.version") + ")\n", 0);
+    }
+
+    /**
+     * <testcase>
+     *  This test case validates that ...
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test0() throws Exception {
+
+        System.setIn(new ByteArrayInputStream("\\relax\n\\end\\n".getBytes()));
+        runTest(new String[]{}, new Properties(), "This is ExTeX, Version "
+                + ExTeX.getVersion() + " (ExTeX mode)\n"
+                + "**\n*\nNo pages of output.\nTranscript written on "
+                + (new File(".", "texput.log")).toString() + ".\n", 0);
+    }
+
+    /**
+     * <testcase>
+     *  This test case validates that ...
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void testFmt() throws Exception {
+
+        System.setIn(new ByteArrayInputStream("\\relax\n\\end\\n".getBytes()));
+        runTest(
+                new String[]{"&xyzzy"},
+                new Properties(),
+                "This is ExTeX, Version "
+                        + ExTeX.getVersion()
+                        + " (ExTeX mode)\n"
+                        + "**\nSorry, I can't find the format `xyzzy.fmt'; will try `tex.fmt'.\n"
+                        + "Sorry, I can't find the format `tex.fmt'!\n"
+                        + "Transcript written on "
+                        + (new File(".", "texput.log")).toString() + ".\n", -1);
+    }
+
+    //TODO add more test cases
+}
