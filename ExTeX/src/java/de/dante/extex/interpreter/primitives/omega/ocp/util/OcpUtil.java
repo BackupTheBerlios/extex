20208d61844d74aeb162fe22856f86bf9f40f18a
gene <gene>
2006-09-26 15:58:54 +0000

    new

---
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/omega/ocp/util/OcpUtil.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2006 The ExTeX Group and individual authors listed below
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package de.dante.extex.interpreter.primitives.omega.ocp.util;
+
+import de.dante.extex.interpreter.TokenSource;
+import de.dante.extex.interpreter.context.Context;
+import de.dante.extex.interpreter.exception.InterpreterException;
+import de.dante.extex.scanner.type.token.SpaceToken;
+import de.dante.extex.scanner.type.token.Token;
+
+/**
+ * This class contains utility methods.
+ *
+ * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
+ * @version $Revision: 1.1 $
+ */
+public final class OcpUtil {
+
+    /**
+     * Creates a new object.
+     */
+    private OcpUtil() {
+
+    }
+
+    /**
+     * Get an ocp file name.
+     *
+     * @param source the source for new tokens
+     * @param context the interpreter context
+     *
+     * @return the ocp file name
+     *
+     * @throws InterpreterException in case of an error
+     */
+    public static String scanOcpFileName(final TokenSource source,
+            final Context context) throws InterpreterException {
+
+        StringBuffer sb = new StringBuffer();
+
+        for (Token t = source.getToken(context); //
+        t != null && !(t instanceof SpaceToken); //
+        t = source.getToken(context)) {
+            sb.append(t.toText());
+        }
+
+        return sb.toString();
+    }
+
+}
