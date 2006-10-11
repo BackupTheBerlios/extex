3816d901b4249bf930326a52fdc53e2ef874098a
gene <gene>
2006-10-11 12:11:23 +0000

    cleanup at end added

--- ExTeX/util/Build/build.sh
+++ ExTeX/util/Build/build.sh
@@ -38,7 +38,6 @@ date >$LOG/installer.log
 sh build installer >>$LOG/installer.log 2>&1
 if [ -e target/ExTeX-setup.jar ]
 then
-#    scp -q target/ExTeX-setup.jar $INSTALLDIR/snapshot
     cp target/ExTeX-setup.jar $INSTALLDIR/snapshot
 else
     cat $LOG/installer.log
@@ -50,9 +49,7 @@ fi
 cd $LOCALDIR/ExTeX
 date >$LOG/javadoc.log
 sh build javadoc >>$LOG/javadoc.log 2>&1
-#scp -q -r target/javadoc $INSTALLDIR/snapshot
 cp -r target/javadoc $INSTALLDIR/snapshot
-#cp -r target/javadoc ~/public_html/
 
 
 #--------------------------------------------------------------------
@@ -61,7 +58,6 @@ cd $LOCALDIR/ExTeX
 date >$LOG/test.log
 sh build clean junitreport >> $LOG/test.log 2>&1
 cp -r target/www/reports $INSTALLDIR/
-#cp -r target/www/reports ~/public_html/
 
 
 #--------------------------------------------------------------------
@@ -78,8 +74,8 @@ make >> $LOG/doc.log 2>&1
 date >$LOG/www.log
 cd $LOCALDIR/ExTeX/www
 make >>$LOG/www.log 2>&1
-#scp -q -r ../target/www/* $INSTALLDIR/
 cp -r ../target/www/* $INSTALLDIR/
 
 
+rm -r $LOCALDIR/ExTeX/target
 #
