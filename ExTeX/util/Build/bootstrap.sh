7399c5bffc1ce2c541fcda4325b3ec307386469c
gene <gene>
2006-10-11 10:59:40 +0000

    Configuration parameter INSTALLDIR added

--- ExTeX/util/Build/bootstrap.sh
+++ ExTeX/util/Build/bootstrap.sh
@@ -5,7 +5,10 @@
 #--------------------------------------------------------------------
 # LOCALDIR contains the location where the build resides.
 # Here all temporary files are created.
-LOCALDIR="$HOME/build"
+LOCALDIR="/serv/extex-project/build"
+#--------------------------------------------------------------------
+# INSTALLDIR contains the directory to store the web site in.
+INSTALLDIR=/serv/extex-project/www.extex.org
 #--------------------------------------------------------------------
 # Point to the Java SDK
 export JAVA_HOME=$HOME/local/share/j2sdk1.4.2_09/
@@ -13,10 +16,10 @@ export JAVA_HOME=$HOME/local/share/j2sdk1.4.2_09/
 #
 CVSDIR=":pserver:anonymous@cvs.extex.berlios.de:/cvsroot/extex"
 #--------------------------------------------------------------------
-#
+# MODULES contains the list of CVS modelues to check-out.
 MODULES="ExTeX"
 #--------------------------------------------------------------------
-#
+# LOG contains the directory where the logs are stored.
 LOG=$LOCALDIR/log
 #--------------------------------------------------------------------
 # 
