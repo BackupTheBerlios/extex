6cb610f4f432f0f98859d41c262539be22bfacab
gene <gene>
2006-10-24 10:11:38 +0000

    test case added

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/register/CatcodeTest.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/register/CatcodeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2005-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -25,7 +25,7 @@ import de.dante.test.NoFlagsButGlobalPrimitiveTester;
  * This is a test suite for the primitive <tt>\catcode</tt>.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class CatcodeTest extends NoFlagsButGlobalPrimitiveTester {
 
@@ -189,6 +189,23 @@ public class CatcodeTest extends NoFlagsButGlobalPrimitiveTester {
                 + "\\the\\catcode`A\\end",
                 //--- output channel ---
                 "10" + TERM);
+    }
+
+    /**
+     * <testcase primitive="\catcode">
+     *  Test case checking that <tt>\catcode</tt> is an assignment.
+     * </testcase>
+     *
+     * @throws Exception in case of an error
+     */
+    public void test30() throws Exception {
+
+        assertSuccess(//--- input code ---
+                "\\afterassignment abc"
+                + "\\catcode`A=10 "
+                + "\\the\\catcode`A\\end",
+                //--- output channel ---
+                "bca10" + TERM);
     }
 
 }
