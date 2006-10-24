d8060ffce9d568a8fc1103a3d995e1597756e94c
gene <gene>
2006-10-24 10:05:33 +0000

    test case renamed

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/info/ErrmessageTest.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/info/ErrmessageTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2004-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -25,12 +25,12 @@ import de.dante.test.ExTeXLauncher;
  * This is a test suite for the primitive <tt>\errmessage</tt>.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class ErrmessageTest extends ExTeXLauncher {
 
     /**
-     * Constructor for JobnameTest.
+     * Creates a new object.
      *
      * @param arg the name
      */
@@ -46,7 +46,7 @@ public class ErrmessageTest extends ExTeXLauncher {
      *
      * @throws Exception in case of an error
      */
-    public void testJobname1() throws Exception {
+    public void test1() throws Exception {
 
         assertFailure(//--- input code ---
                 DEFINE_BRACES + "\\errmessage{abc}",
