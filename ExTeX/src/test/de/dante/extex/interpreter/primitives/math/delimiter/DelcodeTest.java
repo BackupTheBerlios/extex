af85c1baba5f74de49c1bf03ba4096d67347cd80
gene <gene>
2006-10-29 19:13:29 +0000

    todo added

--- ExTeX/src/test/de/dante/extex/interpreter/primitives/math/delimiter/DelcodeTest.java
+++ ExTeX/src/test/de/dante/extex/interpreter/primitives/math/delimiter/DelcodeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 The ExTeX Group and individual authors listed below
+ * Copyright (C) 2005-2006 The ExTeX Group and individual authors listed below
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -25,7 +25,7 @@ import de.dante.test.NoFlagsButGlobalPrimitiveTester;
  * This is a test suite for the primitive <tt>\delcode</tt>.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class DelcodeTest extends NoFlagsButGlobalPrimitiveTester {
 
@@ -47,5 +47,7 @@ public class DelcodeTest extends NoFlagsButGlobalPrimitiveTester {
 
         super(arg, "delcode", "23 32");
     }
+
+    //TODO implement more primitive specific test cases
 
 }
