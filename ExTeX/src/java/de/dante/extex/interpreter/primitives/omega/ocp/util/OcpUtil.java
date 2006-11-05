4d70139b20d98db888c22a5a8a17b09be7e3fbad
gene <gene>
2006-11-05 19:51:41 +0000

    scanOcp() added

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/omega/ocp/util/OcpUtil.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/omega/ocp/util/OcpUtil.java
@@ -22,14 +22,22 @@ package de.dante.extex.interpreter.primitives.omega.ocp.util;
 import de.dante.extex.interpreter.TokenSource;
 import de.dante.extex.interpreter.context.Context;
 import de.dante.extex.interpreter.exception.InterpreterException;
+import de.dante.extex.interpreter.exception.helping.EofException;
+import de.dante.extex.interpreter.exception.helping.HelpingException;
+import de.dante.extex.interpreter.exception.helping.UndefinedControlSequenceException;
+import de.dante.extex.interpreter.type.Code;
+import de.dante.extex.scanner.type.token.CodeToken;
 import de.dante.extex.scanner.type.token.SpaceToken;
 import de.dante.extex.scanner.type.token.Token;
+import de.dante.extex.typesetter.Typesetter;
+import de.dante.util.framework.i18n.Localizer;
+import de.dante.util.framework.i18n.LocalizerFactory;
 
 /**
  * This class contains utility methods.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public final class OcpUtil {
 
@@ -62,6 +70,43 @@ public final class OcpUtil {
         }
 
         return sb.toString();
+    }
+
+    /**
+     * Get an ocp.
+     *
+     * @param source the source for new tokens
+     * @param context the interpreter context
+     * @param typesetter the typesetter
+     *
+     * @return the ocp file name
+     *
+     * @throws InterpreterException in case of an error
+     */
+    public static Ocp scanOcp(final TokenSource source, final Context context,
+            final Typesetter typesetter) throws InterpreterException {
+
+        Token t = source.getToken(context);
+        if (t == null) {
+            throw new EofException();
+        } else if (t instanceof CodeToken) {
+            Code code = context.getCode((CodeToken) t);
+            if (code == null) {
+                throw new UndefinedControlSequenceException(t.toString());
+            } else if (code instanceof OcpConvertible) {
+                return ((OcpConvertible) code).convertOcp(context, source,
+                        typesetter);
+            }
+
+        } else {
+            source.push(t);
+            return new Ocp(scanOcpFileName(source, context));
+        }
+
+        source.push(t);
+        throw new HelpingException(
+                LocalizerFactory.getLocalizer(OcpUtil.class),
+                "Omega.MissingOcp");
     }
 
 }
