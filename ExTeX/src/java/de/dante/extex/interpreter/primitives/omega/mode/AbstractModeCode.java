9219536e8a425d55c12777f20532856b3ab20786
gene <gene>
2006-11-05 19:52:02 +0000

    additional constants for translations

--- ExTeX/src/java/de/dante/extex/interpreter/primitives/omega/mode/AbstractModeCode.java
+++ ExTeX/src/java/de/dante/extex/interpreter/primitives/omega/mode/AbstractModeCode.java
@@ -22,14 +22,16 @@ package de.dante.extex.interpreter.primitives.omega.mode;
 import de.dante.extex.interpreter.TokenSource;
 import de.dante.extex.interpreter.context.Context;
 import de.dante.extex.interpreter.exception.InterpreterException;
+import de.dante.extex.interpreter.exception.helping.EofException;
 import de.dante.extex.interpreter.type.AbstractCode;
+import de.dante.extex.scanner.type.token.Token;
 
 /**
  * This is the abstract base class for primitives dealing with an input or
  * output mode as defined by Omega.
  *
  * @author <a href="mailto:gene@gerd-neugebauer.de">Gerd Neugebauer</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public abstract class AbstractModeCode extends AbstractCode {
 
@@ -39,9 +41,21 @@ public abstract class AbstractModeCode extends AbstractCode {
     protected static final String DEFAULT_INPUT_MODE = "defaultInputMode";
 
     /**
+     * The constant <tt>DEFAULT_INPUT_TRANSLATION</tt> contains the key for the
+     * default input translation.
+     */
+    protected static final String DEFAULT_INPUT_TRANSLATION = "defaultInputTranslation.";
+
+    /**
      * The constant <tt>OUTPUT_MODE</tt> contains the key for the output mode.
      */
     protected static final String DEFAULT_OUTPUT_MODE = "defaultOutputMode";
+
+    /**
+     * The constant <tt>DEFAULT_OUTPUT_TRANSLATION</tt> contains the key for the
+     * default output translation.
+     */
+    protected static final String DEFAULT_OUTPUT_TRANSLATION = "defaultOutputTranslation.";
 
     /**
      * The constant <tt>INPUT_MODE</tt> contains the key for the input mode.
@@ -49,9 +63,21 @@ public abstract class AbstractModeCode extends AbstractCode {
     protected static final String INPUT_MODE = "inputMode";
 
     /**
+     * The constant <tt>INPUT_TRANSLATION</tt> contains the key for the
+     * input translation.
+     */
+    protected static final String INPUT_TRANSLATION = "inputTranslation";
+
+    /**
      * The constant <tt>OUTPUT_MODE</tt> contains the key for the output mode.
      */
     protected static final String OUTPUT_MODE = "outputMode";
+
+    /**
+     * The constant <tt>OUTPUT_TRANSLATION</tt> contains the key for the
+     * output translation.
+     */
+    protected static final String OUTPUT_TRANSLATION = "outputTranslation";
 
     /**
      * Creates a new object.
@@ -79,6 +105,12 @@ public abstract class AbstractModeCode extends AbstractCode {
         OmegaMode mode = scanMode(context, source);
 
         if (mode == null) {
+            Token token = source.getToken(context);
+            if (token == null) {
+                throw new EofException();
+            }
+            source.push(token);
+
             throw new BadInputModeException();
         }
 
